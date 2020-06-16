8f6523e6c31570b4f86dcf65b1a6792edc6cde83
adammmertel <mertel.adam@gmail.com>
2020-06-16 13:52:25 +0200

    GUI: Implement Tailwind

--- gui/src/app.tsx
+++ gui/src/app.tsx
@@ -1,7 +1,16 @@
 import * as React from "react";
 
+import { Button } from "./components";
+
+import "./app.css";
+
 interface AppProps {}
 
 export const App: React.FC<AppProps> = () => {
-  return <h1>app works</h1>;
+  return (
+    <div>
+      <h1>app works</h1>
+      <Button />
+    </div>
+  );
 };
---
+++ gui/src/components/button/button.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+
+interface AppProps {}
+
+export const Button: React.FC<AppProps> = () => {
+  return <button className="btn">button</button>;
+};
