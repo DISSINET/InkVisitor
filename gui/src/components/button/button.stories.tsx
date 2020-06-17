b177df32b35e9e6b9ac6d19214fc9836c6d00d0e
adammmertel <mertel.adam@gmail.com>
2020-06-17 11:16:42 +0200

    GUI: Create storybook for button

--- gui/src/components/button/button.stories.tsx
+++ gui/src/components/button/button.stories.tsx
@@ -11,3 +11,35 @@ export default {
 export const DefaultButton = () => {
   return <Button label="default" />;
 };
+
+export const PrimaryButton = () => {
+  return <Button label="primary" color="primary" />;
+};
+
+export const WarningButton = () => {
+  return <Button label="warning" color="warning" />;
+};
+
+export const DangerButton = () => {
+  return <Button label="danger" color="danger" />;
+};
+
+export const InfoButton = () => {
+  return <Button label="info" color="info" />;
+};
+
+export const PrimaryInvertedButton = () => {
+  return <Button label="primary" inverted color="primary" />;
+};
+
+export const WarningInvertedButton = () => {
+  return <Button label="warning" inverted color="warning" />;
+};
+
+export const DangerInvertedButton = () => {
+  return <Button label="danger" inverted color="danger" />;
+};
+
+export const InfoInvertedButton = () => {
+  return <Button label="info" inverted color="info" />;
+};
--- gui/src/components/button/button.tsx
+++ gui/src/components/button/button.tsx
@@ -1,9 +1,50 @@
-import * as React from "react";
+import React, { MouseEventHandler } from "react";
+import classNames from "classnames";
+
+import { Colors } from "components/types";
 
 interface AppProps {
-  label: string;
+  label?: string;
+  inverted?: Boolean;
+  color?: typeof Colors[number];
+  onClick?: MouseEventHandler<HTMLElement>;
 }
 
-export const Button: React.FC<AppProps> = ({ label }) => {
-  return <button className="btn">{label}</button>;
+export const Button: React.FC<AppProps> = ({
+  label,
+  inverted,
+  color,
+  onClick,
+}) => {
+  const classes = [
+    "component",
+    "button",
+    "px-4",
+    "py-2",
+    "font-bold",
+    "border-2",
+  ];
+  if (inverted) {
+    classes.push("bg-white");
+    classes.push(`text-${color}`);
+    classes.push(`border-${color}`);
+  } else {
+    classes.push(`bg-${color}`);
+    classes.push("text-white");
+    classes.push(`border-${color}`);
+  }
+  return (
+    <button onClick={onClick} className={classNames(classes)}>
+      {label}
+    </button>
+  );
+};
+
+Button.defaultProps = {
+  onClick: () => {
+    // do nothing
+  },
+  color: "primary",
+  inverted: false,
+  label: "",
 };
