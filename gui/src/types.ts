5f5c52c47db0515ffe7030474f7e61d73f473bf8
adammmertel <mertel.adam@gmail.com>
2020-06-17 14:21:15 +0200

    GUI: Add list of entities

---
+++ gui/src/types.ts
@@ -0,0 +1,73 @@
+export const Colors = [
+  "black",
+  "white",
+  "primary",
+  "warning",
+  "danger",
+  "info",
+  "text",
+  "grey",
+  "entityT",
+  "entityR",
+  "entityA",
+  "entityC",
+  "entityE",
+  "entityG",
+  "entityL",
+  "entityO",
+  "entityP",
+  "entityV",
+];
+
+export const Entities = [
+  {
+    id: "T",
+    label: "Territory",
+    color: "entityT",
+  },
+  {
+    id: "R",
+    label: "Territory",
+    color: "entityR",
+  },
+  {
+    id: "A",
+    label: "Action",
+    color: "entityA",
+  },
+  {
+    id: "C",
+    label: "Concept",
+    color: "entityC",
+  },
+  {
+    id: "E",
+    label: "Event",
+    color: "entityE",
+  },
+  {
+    id: "G",
+    label: "Group",
+    color: "entityG",
+  },
+  {
+    id: "L",
+    label: "Location",
+    color: "entityL",
+  },
+  {
+    id: "O",
+    label: "Object",
+    color: "entityO",
+  },
+  {
+    id: "P",
+    label: "Person",
+    color: "entityP",
+  },
+  {
+    id: "V",
+    label: "Value",
+    color: "entityV",
+  },
+];
