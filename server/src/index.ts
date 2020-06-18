1c1f40540cb541d3a0bddd5ad14b5c63ff4d8bce
David Landa <david.landa@protonmail.com>
2020-06-18 16:58:00 +0200

    Add server code

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,57 +1,10 @@
-var express = require("express");
-import { replaceAll } from "../../shared/util/base";
-import { close, runQuery } from "../../shared/util/db";
+import './LoadEnv'; // Must be the first import
 
-const app = express();
-const port = 3000;
+import app from '@server';
+import logger from '@shared/Logger';
 
-app.get("/text/:textId", async (req, res) => {
-  console.log(req.params.textId);
-  const actions_actants = await runQuery(
-    "MATCH (text:T{id: '" +
-      req.params.textId +
-      "'})-[r_origin:ORIGINATES_IN]-(action:A)-[r_actant:HAS_ACTANT]-(actant) RETURN r_origin,action.id,r_actant,actant.id"
-  );
-  const ations_resources = await runQuery(
-    "MATCH (text:T{id: '" +
-      req.params.textId +
-      "'})-[:ORIGINATES_IN]-(action:A)-[r_reference:HAS_REFERENCE_IN]-(resource:R) RETURN action.id,r_reference,resource.id"
-  );
-  const actions = await runQuery(
-    "MATCH (text:T{id: '" +
-      req.params.textId +
-      "'})-[:ORIGINATES_IN]-(action:A) RETURN action"
-  );
-  const actants = await runQuery(
-    "MERGE (text:T{id: '" +
-      req.params.textId +
-      "'})-[:ORIGINATES_IN]-(:A)-[:HAS_ACTANT]-(actant) RETURN actant"
-  );
-  const texts = {
-    down: await runQuery(
-      "MATCH (:T{id: '" +
-        req.params.textId +
-        "'})-[r_part:IS_PART_OF]->(text:T) RETURN text, r_part"
-    ),
-    up: await runQuery(
-      "MATCH (:T{id: '" +
-        req.params.textId +
-        "'})<-[r_part:IS_PART_OF]-(text:T) RETURN text, r_part"
-    ),
-  };
-
-  close();
-  res.send({
-    actants,
-    texts,
-    actions,
-    ations_resources,
-    actions_actants,
-  });
+// Start the server
+const port = Number(process.env.PORT || 3000);
+app.listen(port, () => {
+    logger.info('Express server started on port: ' + port);
 });
-
-app.get("/", (req, res) => res.send("Hello!"));
-
-app.listen(port, () =>
-  console.log(`Example app listening at http://localhost:${port}`)
-);
