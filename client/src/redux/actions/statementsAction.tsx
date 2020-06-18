5adc66ff1faad861db5869e8ba0f2f17d9d2bf8d
Petr Hanak <petr.nahak@gmail.com>
2020-06-18 19:04:48 +0200

    finish redux statement fetch, add rest api implementation

---
+++ client/src/redux/actions/statementsAction.tsx
@@ -0,0 +1,17 @@
+import { Dispatch } from "redux";
+
+import { getStatements } from "api/getStatements";
+import { FETCH_STATEMENTS, StatementsAction } from "redux/types";
+import { Statement } from "types";
+
+export const fetchPersons = () => (
+  dispatch: Dispatch<StatementsAction>
+): Promise<void> => {
+  getStatements().then((data: Statement[]) =>
+    dispatch({
+      type: FETCH_STATEMENTS,
+      payload: data,
+    })
+  );
+  return Promise.resolve();
+};
