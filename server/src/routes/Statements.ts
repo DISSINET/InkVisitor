4af686395b2fcf8910df31bdb1eb35887d853823
David Landa <david.landa@protonmail.com>
2020-06-19 12:00:14 +0200

    Update statement routes

--- server/src/routes/Statements.ts
+++ server/src/routes/Statements.ts
@@ -6,19 +6,74 @@ import { paramMissingError } from '@shared/constants';
 import { stat } from 'fs';
 
 class Statement {
-    public id : string;
+    
+    public id : string; //uuid
     public tree: object;
+    public territoryId: string; //uuid
+    public resources : Array<string>; //uuids
+    public actionId : string; //uuid
+    public modality : number; //int
+    public certainty : number; //int
+    public epistemicLevel : number; //int
+    public tags: Array<string> //uuids;
+    public note: string;
+    public meta: string /*or object?*/;
+    public text: string;
 
-    constructor(id: string, tree: object) {
+    constructor(
+        id: string, 
+        tree: object,
+        territoryId: string, 
+        resources: Array<string>, 
+        actionId: string, 
+        modality: number, 
+        certainty: number,
+        epistemicLevel: number,
+        tags: Array<string>, 
+        note: string, 
+        meta: string, 
+        text: string
+    ) {
         this.id = id;
-        this.tree = tree;
+        this.tree = tree;    
+        this.territoryId = territoryId;
+        this.resources = resources;
+        this.actionId = actionId; 
+        this.modality = modality;
+        this.certainty = certainty;
+        this.epistemicLevel = epistemicLevel
+        this.tags = tags;
+        this.note = note;
+        this.meta = meta;
+        this.text = text
+    }
+
+    /**
+     * Create a mockup statement.
+     */
+    static createMockup(territoryId: string) : Statement {
+        return new Statement(
+            uuid4(),
+            {},    
+            territoryId,
+            [uuid4(), uuid4(), uuid4()],
+            uuid4(), 
+            1,
+            2,
+            3,
+            [uuid4(), uuid4(), uuid4()],
+            "note",
+            "meta",
+            "text"
+        )
     }
 }
 
 interface Repository<T> {
-    saveOne(entity: T) : Promise<void>;
-    findAll() : Promise<T[]>;
+    findAll(limit: number, offset: number, filters : object) : Promise<T[]>;
     findOne(id: string) : Promise<T | undefined>;
+    insertOne(entity: T) : Promise<void>;
+    removeOne(id: string) : Promise<void>;
 }
 
 interface IStatementRepository extends Repository<Statement> {
@@ -29,31 +84,58 @@ interface IStatementRepository extends Repository<Statement> {
  */
 class StatementRepository implements IStatementRepository {
 
-    // Mockups
-    private statements = [
-        new Statement('11111111-1111-1111-1111-111111111111', {}),
-        new Statement('22222222-2222-2222-2222-222222222222', {}),
-        new Statement('33333333-3333-3333-3333-333333333333', {}),
-    ]
+    private statements : Map<string, Statement>;
 
-    async saveOne(statement: Statement) :Promise<void> { 
+    constructor() {
+        // Mockups
+        this.statements = new Map([
+            Statement.createMockup("1"),
+            Statement.createMockup("1"),
+            Statement.createMockup("2"),
+            Statement.createMockup("2"),
+            Statement.createMockup("3"),
+        ].map(statement => [statement.id, statement]))
+    
+    }
+
+    /**
+     * 
+     * @param  
+     */
+    async insertOne(statement: Statement) :Promise<void> { 
         // if statement in statements raise exception.
-        this.statements.push(statement);
+        this.statements.set(statement.id, statement);
+    }
+
+    /**
+     * Remove one statetement.
+     * @param statementId
+     */
+    async removeOne(statementId: string) : Promise<void> {
+        this.statements.delete(statementId)
     }
 
     /**
      * Get the all statements.
      */
-    async findAll() : Promise<Statement[]> {
-        return [...this.statements];
+    async findAll(limit: number, offset: number, filters = {}) : Promise<Statement[]> {
+        const result = [...this.statements.values()]
+        
+        console.log(result)
+
+        if ("territoryId" in filters) {
+            // Only territoryId implemented.
+            return result.filter( _ => filters["territoryId"] === _.territoryId);
+        }
+        return result
     }
     
     /**
      * Get the one statement.
      * @param id 
      */
-    async findOne(statement_id: string /*UUID*/ ) : Promise<Statement | undefined> {
-        return this.statements.find( ({id}) => id === statement_id);
+    async findOne(statementId: string /*UUID*/ ) : Promise<Statement | undefined> {
+        return [...this.statements.values()].find( ({id}) => id === statementId )
     }
 }
 
@@ -64,7 +146,9 @@ const repository = new StatementRepository()
  * Get the statements `GET /api/statements`.
  */
 router.get("/", async (request: Request, response: Response) => {
-    const statements = await repository.findAll();
+    const filters = request.query //.filter( (x) => !(x in ["offset", "limit"]) )
+    //console.log(filters)
+    const statements = await repository.findAll(100, 0, filters);
     return response.status(OK).json(statements);
 })
 
@@ -77,7 +161,9 @@ router.get("/:uuid", async (request: Request, response: Response) => {
     return response.status(OK).json(statement);
 })
 
-// Create the statement `POST /api/statement/`.
+/**
+ * Create the statement `POST /api/statement`.
+ */
 router.post('/', async (request: Request, response: Response) => {
 
     const { statement } = request.body;
@@ -88,7 +174,7 @@ router.post('/', async (request: Request, response: Response) => {
         });
     }
 
-    await repository.saveOne(statement);
+    await repository.insertOne(statement);
 
     return response.status(CREATED).end();
 });
