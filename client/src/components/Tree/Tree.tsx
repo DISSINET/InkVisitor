6bae76d89cd545ddfe3b6f9bcf508c2748ec5efe
Petr Hanak <petr.nahak@gmail.com>
2020-06-20 16:53:42 +0200

    basic implementation of UI tree, typescript for tree recursion

--- client/src/components/Tree/Tree.tsx
+++ client/src/components/Tree/Tree.tsx
@@ -1,6 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
+import { Tag, Button } from "components";
+import { Entities, Node } from "types";
 
-interface Tree {}
-export const Tree: React.FC<Tree> = () => {
-  return <div>Tree</div>;
+interface Tree {
+  treeObject: Node;
+  onNodeSelect?: Function;
+  onNodeExpand?: Function;
+  selectedTreeId?: string;
+  expandedTreeId?: string;
+}
+
+export const Tree: React.FC<Tree> = ({
+  treeObject,
+  onNodeSelect,
+  onNodeExpand,
+  selectedTreeId,
+  expandedTreeId,
+}) => {
+  const [expandedTreeNode, setExpandedTreeNode] = useState(treeObject);
+
+  return (
+    <div>
+      <Tag
+        entity={Entities.T}
+        label={expandedTreeNode && expandedTreeNode.label}
+      />
+      <div className="flex flex-col">
+        {expandedTreeNode.children.map((child: Node, key) => {
+          return (
+            <div className="flex my-1 ml-2" key={key}>
+              <Tag
+                entity={Entities.T}
+                label={child && child.label}
+                mode={child.id === selectedTreeId && "selected"}
+                button={
+                  <>
+                    <Button
+                      onClick={() => onNodeExpand && onNodeExpand(child.id)}
+                      label=">"
+                    />
+                    <Button
+                      onClick={() => onNodeSelect && onNodeSelect(child.id)}
+                      label="O"
+                    />
+                  </>
+                }
+              />
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
--- client/src/redux/actions/statementsActions.tsx
+++ client/src/redux/actions/statementsActions.tsx
@@ -1,8 +1,14 @@
 import { Dispatch } from "redux";
 
 import { getStatements } from "api/getStatements";
-import { FETCH_STATEMENTS, StatementsAction } from "redux/types";
+import {
+  FETCH_STATEMENTS,
+  StatementsAction,
+  StatementAction,
+  FETCH_STATEMENT,
+} from "redux/types";
 import { Statement } from "types";
+import { getStatement } from "api/getStatement";
 
 export const fetchStatements = () => (
   dispatch: Dispatch<StatementsAction>
@@ -10,6 +16,18 @@ export const fetchStatements = () => (
   getStatements().then((data: Statement[]) =>
     dispatch({
       type: FETCH_STATEMENTS,
+      payload: data,
+    })
+  );
+  return Promise.resolve();
+};
+
+export const fetchStatement = (id: string) => (
+  dispatch: Dispatch<StatementAction>
+): Promise<void> => {
+  getStatement(id).then((data: Statement) =>
+    dispatch({
+      type: FETCH_STATEMENT,
       payload: data,
     })
   );
