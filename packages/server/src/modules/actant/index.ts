aa6213ffde18350db5249168184fb5a09f2b72ba
adammmertel <mertel.adam@gmail.com>
2020-08-27 09:15:40 +0200

    Create basic functions for actants route

--- packages/server/src/modules/actant/index.ts
+++ packages/server/src/modules/actant/index.ts
@@ -1,8 +1,164 @@
+import { getOneActant, Result } from "../index";
 
-type UUID = string;
+import { rethinkConfig } from "@service/RethinkDB";
+import { paramMissingError } from "@shared/constants";
+import { Router, Response, Request, NextFunction } from "express";
+import { BAD_REQUEST, CREATED, OK, NOT_FOUND } from "http-status-codes";
 
-export default interface Actant {
-    id: UUID;
-    label: string;
-    class: "T" | "R" | "E" | "S"; // defines the model and the conntent of data object
+import { r } from "rethinkdb-ts";
+
+//-----------------------------------------------------------------------------
+// Repository functions
+//-----------------------------------------------------------------------------
+const TABLE_NAME = "actants";
+const CLASS_NAME = "E";
+
+/**
+ * RethinkDB actant load function.
+ */
+async function findOne(actantId: string): Promise<any> {
+  return await getOneActant(actantId, CLASS_NAME);
 }
+
+/**
+ * RethinkDB actant load function.
+ */
+async function findAll(limit = 100, offset = 0): Promise<any> {
+  let conn = await r.connect(rethinkConfig);
+
+  let result = await r.table(TABLE_NAME).limit(limit).run(conn);
+
+  conn.close();
+
+  return result;
+}
+
+/**
+ * RethinkDB actant save function.
+ * @param actant
+ */
+async function saveOne(actant: any): Promise<any> {
+  let conn = await r.connect(rethinkConfig);
+
+  await r.table(TABLE_NAME).insert(actant).run(conn);
+
+  conn.close();
+}
+
+/**
+ * Update actant.
+ */
+async function updateOne(actant: any): Promise<any> {
+  let conn = await r.connect(rethinkConfig);
+
+  let result = await r.table(TABLE_NAME).get(actant.id).update(actant);
+  conn.close();
+
+  return result;
+}
+
+/**
+ * RethinkDB actant save function.
+ * @param actantId
+ */
+async function deleteOne(actantId: string): Promise<any> {
+  let conn = await r.connect(rethinkConfig);
+
+  console.log(actantId);
+
+  const result = await r.table(TABLE_NAME).get(actantId).delete().run(conn);
+
+  conn.close();
+
+  return result;
+}
+
+//-----------------------------------------------------------------------------
+// Controller functions
+//-----------------------------------------------------------------------------
+
+//-----------------------------------------------------------------------------
+// Router
+//-----------------------------------------------------------------------------
+
+export default Router()
+  /**
+   * Create the actant.
+   */
+  .post(
+    "/",
+    async (request: Request, response: Response, next: NextFunction) => {
+      const actant = request.body;
+
+      if (!actant) {
+        return response.status(BAD_REQUEST).json({
+          error: paramMissingError,
+        });
+      }
+
+      await saveOne(actant);
+
+      return response.status(CREATED).json(actant).end();
+    }
+  )
+
+  /**
+   * Update the actant.
+   */
+  .put(
+    "/:uuid",
+    async (request: Request, response: Response, next: NextFunction) => {
+      const actant = request.body;
+
+      if (!actant) {
+        return response.status(BAD_REQUEST).json({
+          error: paramMissingError,
+        });
+      }
+
+      const result_ = await saveOne(actant);
+
+      console.log(`UPDATE ${result_}`);
+
+      return result_
+        ? response.status(OK).json(actant).end()
+        : Result(response, NOT_FOUND, `Actant ${actant.id} not found`);
+    }
+  )
+
+  /**
+   * Delete the actant.
+   */
+  .delete("/:uuid", async (request: Request, response: Response) => {
+    const uuid: string = request.params.uuid;
+
+    const result_ = await deleteOne(uuid);
+
+    return result_
+      ? Result(response, OK, `Actant ${uuid} was deleted.`)
+      : Result(response, NOT_FOUND, `Actant ${uuid} not found.`);
+  })
+  /**
+   * Retrieve the actants collection.
+   */
+  .get("/", async (request: Request, response: Response) => {
+    const filters = request.query; //.filter( (x) => !(x in ["offset", "limit"]) )
+    const actants = await findAll(100, 0);
+
+    return Result(response, OK, actants);
+  })
+
+  /**
+   * Retrieve the actant individual.
+   */
+  .get("/:uuid", async (request: Request, response: Response) => {
+    const uuid: string = request.params.uuid;
+
+    console.log(uuid);
+
+    const actant: any | null = await findOne(uuid);
+
+    return actant
+      ? Result(response, OK, actant)
+      : Result(response, NOT_FOUND, `Actant ${uuid} was not found.`);
+  });
