22497b7eb2c1b6a4999b958b26fe07b8ee08cdc0
adammmertel <mertel.adam@gmail.com>
2020-08-28 09:05:42 +0200

    Apply filters to findAll and territory route to actant router

--- packages/server/src/modules/actant/index.ts
+++ packages/server/src/modules/actant/index.ts
@@ -23,10 +23,47 @@ async function findOne(actantId: string): Promise<any> {
 /**
  * RethinkDB actant load function.
  */
-async function findAll(limit = 100, offset = 0): Promise<any> {
+async function findAll(
+  limit = 100,
+  offset = 0,
+  filters: any = {}
+): Promise<any> {
   let conn = await r.connect(rethinkConfig);
 
-  let result = await r.table(TABLE_NAME).limit(limit).run(conn);
+  console.log(filters);
+
+  const baseFilterKeys = ["label", "class"];
+
+  const dataFilterKeys = [
+    "certainty",
+    "type",
+    "elvl",
+    "territory",
+    "action",
+    "language",
+    "link",
+    "parent",
+  ];
+
+  // construct the filterObject
+  const filterObject: any = { data: {} };
+
+  baseFilterKeys.forEach((baseFilterKey) => {
+    if (baseFilterKey in filters) {
+      filterObject[baseFilterKey] = filters[baseFilterKey];
+    }
+  });
+  dataFilterKeys.forEach((dataFilterKey) => {
+    if (dataFilterKey in filters) {
+      filterObject["data"][dataFilterKey] = filters[dataFilterKey];
+    }
+  });
+
+  let result = await r
+    .table(TABLE_NAME)
+    .filter(filterObject)
+    .limit(limit)
+    .run(conn);
 
   conn.close();
 
@@ -138,13 +175,112 @@ export default Router()
       ? Result(response, OK, `Actant ${uuid} was deleted.`)
       : Result(response, NOT_FOUND, `Actant ${uuid} not found.`);
   })
-  /**
-   * Retrieve the actants collection.
-   */
-  .get("/", async (request: Request, response: Response) => {
-    const filters = request.query; //.filter( (x) => !(x in ["offset", "limit"]) )
-    const actants = await findAll(100, 0);
 
+  /**
+   * Retrieve data for a given territory id
+   */
+  .get(
+    "/territory/:territoryId",
+    async (request: Request, response: Response) => {
+      let conn = await r.connect(rethinkConfig);
+      const territoryId: string = request.params.territoryId;
+      // 1. find territory in actants
+      const territory = await r.table(TABLE_NAME).get(territoryId).run(conn);
+      if (territory) {
+        // 2. find all child territories
+        const childTerritories = await r
+          .table(TABLE_NAME)
+          .filter({ data: { parent: territoryId } })
+          .run(conn);
+        territory.children = childTerritories;
+        console.log(childTerritories);
+
+        // 3. find parent territory
+        territory.parent = territory.data.parent
+          ? await r.table(TABLE_NAME).get(territory.data.parent).run(conn)
+          : false;
+
+        // 4. find all statements in this territory
+        const statements = await r
+          .table(TABLE_NAME)
+          .filter({ data: { territory: territoryId } })
+          .run(conn);
+
+        for (var si = 0; si < statements.length; si++) {
+          const statement = statements[si];
+
+          // 4.1 find action for all statements
+          statement.data.action = await r
+            .table("actions")
+            .get(statement.data.action)
+            .run(conn);
+
+          // 4.2 find references for all statements
+          for (var ri = 0; ri < statement.data.references.length; ri++) {
+            const resourceId = statement.data.references[ri].resource;
+            statement.data.references[ri].resource = await r
+              .table(TABLE_NAME)
+              .get(resourceId)
+              .run(conn);
+          }
+
+          // 4.3 find tags for all statements
+          const tags = [];
+          for (var ti = 0; ti < statement.data.tags.length; ti++) {
+            const tagId = statement.data.tags[ti];
+            const tag = await r.table(TABLE_NAME).get(tagId).run(conn);
+            tags.push(tag);
+          }
+          statement.data.tags = tags;
+
+          // 4.4 find actants for all statements
+          for (var ai = 0; ai < statement.data.actants.length; ai++) {
+            const actantId = statement.data.actants[ai].actant;
+            statement.data.actants[ai].actant = await r
+              .table(TABLE_NAME)
+              .get(actantId)
+              .run(conn);
+          }
+
+          // 4.5 find props and actants for all statements
+          for (var pi = 0; pi < statement.data.props.length; pi++) {
+            statement.data.props[pi].actant1 = await r
+              .table(TABLE_NAME)
+              .get(statement.data.props[pi].actant1)
+              .run(conn);
+            statement.data.props[pi].actant2 = await r
+              .table(TABLE_NAME)
+              .get(statement.data.props[pi].actant2)
+              .run(conn);
+            statement.data.props[pi].subject = await r
+              .table(TABLE_NAME)
+              .get(statement.data.props[pi].subject)
+              .run(conn);
+          }
+        }
+        territory.statements = statements;
+
+        return Result(response, OK, territory);
+      } else {
+        return Result(
+          response,
+          NOT_FOUND,
+          `Territory ${territoryId} not found.`
+        );
+      }
+
+      conn.close();
+    }
+  )
+
+  /**
+   * Retrieve the actants collection based on filters
+   */
+  .get("/actants", async (request: Request, response: Response) => {
+    const filters = request.query; //.filter( (x) => !(x in ["offset", "limit"]) )
+    const actants = await findAll(100, 0, filters);
+
+    console.log("getting actants");
     return Result(response, OK, actants);
   })
 
