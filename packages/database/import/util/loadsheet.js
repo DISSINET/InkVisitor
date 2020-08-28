34e5fb180019abe21b0c44b2fe57e1c029a2a038
adammmertel <mertel.adam@gmail.com>
2020-08-28 14:06:29 +0200

    Update loadsheet function

--- packages/database/import/util/loadsheet.js
+++ packages/database/import/util/loadsheet.js
@@ -5,31 +5,33 @@
 const fs = require("fs");
 const readline = require("readline");
 const { google } = require("googleapis");
+var readlineSync = require("readline-sync");
 
 const SCOPES = ["https://www.googleapis.com/auth/spreadsheets.readonly"];
-const TOKEN_PATH = "./config/token.json";
-const CREDENTIALS_PATH = "./config/credentials.json";
+const TOKEN_PATH = "import/util/token.json";
+const CREDENTIALS_PATH = "import/util/credentials.json";
 
 // Load client secrets from a local file.
 module.exports.loadSheet = async ({ spread, sheet }) => {
-  const credentials = fs.readFileSync(CREDENTIALS_PATH);
+  const credentials = JSON.parse(fs.readFileSync(CREDENTIALS_PATH));
 
-  const auth = authorize(JSON.parse(credentials));
+  const auth = await authorize(credentials);
+
   const sheets = google.sheets({ version: "v4", auth });
 
   let res = false;
   try {
     res = await sheets.spreadsheets.values.get({
       spreadsheetId: spread,
-      range: sheet + "!A:ZZ"
+      range: sheet + "!A:ZZ",
     });
   } catch (e) {
     console.error("error loading table", spread, sheet);
     console.error(e);
   }
 
-  const data = await res.data.values.filter(
-    row => row.filter(value => value && value !== "#N/A").length > 1
+  const data = res.data.values.filter(
+    (row) => row.filter((value) => value && value !== "#N/A").length > 1
   );
 
   // divide data to rows and header
@@ -38,7 +40,7 @@ module.exports.loadSheet = async ({ spread, sheet }) => {
 
   // change rows to objects
   const records = rows
-    .map(row => {
+    .map((row) => {
       const record = {};
       row.map((val, vi) => {
         record[header[vi]] = val;
@@ -49,8 +51,9 @@ module.exports.loadSheet = async ({ spread, sheet }) => {
      * filter out "empty" rows
      * TODO: should be changed after tables will refactor
      */
-    .filter(row => row.id_action_or_relation || row.label);
+    .filter((row) => row.id_action_or_relation || row.label);
 
+  console.log(rows);
   return records;
 };
 
@@ -60,8 +63,8 @@ module.exports.loadSheet = async ({ spread, sheet }) => {
  * @param {Object} credentials The authorization client credentials.
  * @param {function} callback The callback to call with the authorized client.
  */
-function authorize(credentials) {
-  const { client_secret, client_id, redirect_uris } = credentials.installed;
+const authorize = async (credentials) => {
+  const { client_secret, client_id, redirect_uris } = credentials.web;
   const oAuth2Client = new google.auth.OAuth2(
     client_id,
     client_secret,
@@ -69,10 +72,15 @@ function authorize(credentials) {
   );
 
   // Check if we have previously stored a token.
-  const token = fs.readFileSync(TOKEN_PATH);
-  oAuth2Client.setCredentials(JSON.parse(token));
-  return oAuth2Client;
-}
+  try {
+    const tokenFile = fs.readFileSync(TOKEN_PATH);
+    oAuth2Client.setCredentials(JSON.parse(tokenFile));
+    return oAuth2Client;
+  } catch (e) {
+    const newToken = await getNewToken(oAuth2Client);
+    return newToken;
+  }
+};
 
 /**
  * Get and store new token after prompting for user authorization, and then
@@ -80,31 +88,26 @@ function authorize(credentials) {
  * @param {google.auth.OAuth2} oAuth2Client The OAuth2 client to get token for.
  * @param {getEventsCallback} callback The callback for the authorized client.
  */
-function getNewToken(oAuth2Client, callback) {
+var getNewToken = async (oAuth2Client) => {
   const authUrl = oAuth2Client.generateAuthUrl({
     access_type: "offline",
-    scope: SCOPES
+    scope: SCOPES,
   });
   console.log("Authorize this app by visiting this url:", authUrl);
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
+
+  var code = readlineSync.question("Enter the code from that page here: ", {
+    hideEchoBack: true,
   });
-  rl.question("Enter the code from that page here: ", code => {
-    rl.close();
-    oAuth2Client.getToken(code, (err, token) => {
-      if (err)
-        return console.error(
-          "Error while trying to retrieve access token",
-          err
-        );
-      oAuth2Client.setCredentials(token);
-      // Store the token to disk for later program executions
-      fs.writeFile(TOKEN_PATH, JSON.stringify(token), err => {
-        if (err) return console.error(err);
-        console.log("Token stored to", TOKEN_PATH);
-      });
-      callback(oAuth2Client);
-    });
+
+  const { err, token } = await this.oAuth2Client.getToken(code);
+
+  if (err)
+    return console.error("Error while trying to retrieve access token", err);
+  oAuth2Client.setCredentials(token);
+  // Store the token to disk for later program executions
+  fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
+    if (err) return console.error(err);
+    console.log("Token stored to", TOKEN_PATH);
   });
-}
+  return oAuth2Client;
+};
