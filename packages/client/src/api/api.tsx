659caaf9e238930cc06f387c8c79ce75b7df0b70
adammmertel <mertel.adam@gmail.com>
2020-08-28 10:57:46 +0200

    Add getTerritory api route

--- packages/client/src/api/api.tsx
+++ packages/client/src/api/api.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:3000/api/",
+  baseURL: "http://localhost:3000/api/v1",
   timeout: 5000,
   responseType: "json",
   headers: {
