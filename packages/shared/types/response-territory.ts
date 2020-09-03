2a5744070bda3acc67316cfde35436c3cc9eac9c
adammmertel <mertel.adam@gmail.com>
2020-09-03 16:06:19 +0200

    Fix actions from response-territory interface removed

--- packages/shared/types/response-territory.ts
+++ packages/shared/types/response-territory.ts
@@ -8,6 +8,5 @@ export interface ResponseTerritoryI extends TerritoryI {
   children: TerritoryI[];
   parent: false | TerritoryI;
   statements: StatementI[];
-  actions: ActionI[];
   actants: ActantI[];
 }
