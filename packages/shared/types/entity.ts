50a7d52baed98d0133de0433cb3a89d59eb25062
Adam Mertel <mertel.adam@gmail.com>
2020-09-16 16:24:27 +0200

    Fix/actant classes (#23)
    * Fix sellan parsing
    
    * Fix models of shared types
    
    * Reparse Sellan data
    
    * Fix Arrow export

--- packages/shared/types/entity.ts
+++ packages/shared/types/entity.ts
@@ -1,6 +1,6 @@
 import { ActantI } from "./actant";
 
 export interface EntityI extends ActantI {
-  class: "E";
-  data: { type: "P" | "G" | "O" | "C" | "L" | "V" | "E" };
+  class: "P" | "G" | "O" | "C" | "L" | "V" | "E";
+  data: {};
 }
