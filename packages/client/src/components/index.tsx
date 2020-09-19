e66fdbc70c7d00f08d129e85666e3cf4d87e731d
Adam Mertel <mertel.adam@gmail.com>
2020-09-19 20:45:34 +0200

    Merge feat/header-component (#25)
    * Create Header component
    
    * Add stories for Header component
    
    * Add Header to index file
    
    * Fix header padding axis
    
    * Add Header component to MainPage

--- packages/client/src/components/index.tsx
+++ packages/client/src/components/index.tsx
@@ -2,8 +2,9 @@ import { Arrow } from "components/Arrow/Arrow";
 import { Box } from "components/Box/Box";
 import { Button } from "components/Button/Button";
 import { ButtonSet } from "components/Buttonset/Buttonset";
+import { Header } from "components/Header/Header";
 import { Input } from "components/Input/Input";
 import { Tag } from "components/Tag/Tag";
 import { Suggester } from "components/Suggester/Suggester";
 
-export { Arrow, Box, Button, ButtonSet, Input, Tag, Suggester };
+export { Arrow, Box, Button, ButtonSet, Header, Input, Tag, Suggester };
