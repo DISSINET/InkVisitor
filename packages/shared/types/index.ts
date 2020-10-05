da84512b86a4aa105bf8462ec546e7ae931e079f
adammmertel <mertel.adam@gmail.com>
2020-10-05 14:37:44 +0200

    Add dictionary to the models

--- packages/shared/types/index.ts
+++ packages/shared/types/index.ts
@@ -1,5 +1,6 @@
 import { ActionI } from "./action";
 import { ActantI } from "./actant";
+import { DictionaryEntryI } from "./dictionaryentry";
 import { EntityI } from "./entity";
 import { ResourceI } from "./resource";
 import { ResponseTerritoryI } from "./response-territory";
@@ -7,10 +8,14 @@ import { ResponseMetaI } from "./response-meta";
 import { StatementI } from "./statement";
 import { TerritoryI } from "./territory";
 
+import { OptionI } from "./option";
+
 export type {
   ActionI,
   ActantI,
+  DictionaryEntryI,
   EntityI,
+  OptionI,
   ResourceI,
   ResponseTerritoryI,
   ResponseMetaI,
