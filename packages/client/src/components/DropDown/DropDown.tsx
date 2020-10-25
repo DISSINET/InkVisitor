ee2ff1297e7e52f8d96d7da52b1bed5d33d40a4f
Ptrhnk <petr.nahak@gmail.com>
2020-10-25 14:17:51 +0100

    Feature/select with search (#45)
    * add react select lib and create dropdown component
    
    * add select to editor
    
    * add classnames to dropdown and restyle
    
    * restyle select component

---
+++ packages/client/src/components/DropDown/DropDown.tsx
@@ -0,0 +1,37 @@
+import React, { ReactNode } from "react";
+import Select, {
+  OptionsType,
+  OptionTypeBase,
+  GroupedOptionsType,
+  ValueType,
+  Theme,
+} from "react-select";
+
+interface DropDown {
+  options?: OptionsType<OptionTypeBase> | GroupedOptionsType<OptionTypeBase>;
+  value?: ValueType<OptionTypeBase>;
+  onChange: (selectedOption: ValueType<OptionTypeBase>) => void;
+  ref?: React.RefObject<ReactNode>;
+}
+export const DropDown: React.FC<DropDown> = ({ options, value, onChange }) => {
+  return (
+    <Select
+      className="react-select-container"
+      classNamePrefix="react-select"
+      value={value}
+      onChange={onChange}
+      options={options}
+      theme={(theme: Theme) => ({
+        ...theme,
+        borderRadius: 0,
+        colors: {
+          ...theme.colors,
+          text: "white",
+          primary25: "grey",
+          primary50: "grey",
+          primary: "black",
+        },
+      })}
+    />
+  );
+};
