9692a90e1164af40242d08de2cdeda8a1e13e4b2
adammmertel <mertel.adam@gmail.com>
2020-11-21 09:56:15 +0100

    Update models

--- packages/shared/types/index.ts
+++ packages/shared/types/index.ts
@@ -2,7 +2,6 @@ import { ActionI } from "./action";
 import { ActantI } from "./actant";
 import { DictionaryEntryI } from "./dictionaryentry";
 import { EntityI } from "./entity";
-import { ResourceI } from "./resource";
 import { ResponseTerritoryI } from "./response-territory";
 import { ResponseMetaI } from "./response-meta";
 import { StatementI } from "./statement";
@@ -16,7 +15,6 @@ export type {
   DictionaryEntryI,
   EntityI,
   OptionI,
-  ResourceI,
   ResponseTerritoryI,
   ResponseMetaI,
   StatementI,
--- packages/shared/types/territory.ts
+++ packages/shared/types/territory.ts
@@ -4,15 +4,7 @@ export interface TerritoryI extends ActantI {
   class: "T";
   data: {
     label: string;
-    content: string;
-    language: string;
     parent: string | false;
-    type: string;
   };
-  meta: {
-    created: {
-      user: string;
-      time: string;
-    };
-  };
+  meta: {};
 }
