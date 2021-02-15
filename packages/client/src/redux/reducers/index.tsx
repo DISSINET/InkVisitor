e21505ec2bfb1d82999caf3a88ef8ed696395678
Petr Hanak <petr.nahak@gmail.com>
2021-02-15 17:20:32 +0100

    migrate activeStatementId action/reducer

--- packages/client/src/redux/reducers/index.tsx
+++ packages/client/src/redux/reducers/index.tsx
@@ -2,13 +2,14 @@ import { combineReducers } from "redux";
 
 import meta from "./metaReducer";
 import { territory } from "./territoryTreeReducer";
-import activeStatementId from "./statementReducer";
+// import activeStatementId from "./statementReducer";
 import token from "./authTokenReducer";
+import activeStatementIdSlice from "redux/features/statementIdSlice";
 
 const rootReducer = combineReducers({
   meta,
   territory,
-  activeStatementId,
+  activeStatementId: activeStatementIdSlice,
   token,
 });
 
--- packages/client/src/redux/store.tsx
+++ packages/client/src/redux/store.tsx
@@ -2,6 +2,7 @@ import { applyMiddleware, createStore, compose, CombinedState } from "redux";
 import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
 import { Store } from "redux";
+import { configureStore } from "@reduxjs/toolkit";
 
 import rootReducer from "./reducers";
 import { ResponseTerritoryI } from "@shared/types/response-territory";
@@ -17,6 +18,19 @@ const initialState = {};
 
 const middleWare = [thunk];
 
+// const store: Store<
+//   CombinedState<{
+//     meta: ResponseMetaI;
+//     territory: ResponseTerritoryI;
+//     activeStatementId: string;
+//     token: string;
+//   }>,
+//   MetaAction | TerritoryAction | ActiveStatementIdAction | AuthTokenAction
+// > = createStore(
+//   rootReducer,
+//   initialState,
+//   composeWithDevTools(applyMiddleware(...middleWare))
+// );
 const store: Store<
   CombinedState<{
     meta: ResponseMetaI;
@@ -25,10 +39,10 @@ const store: Store<
     token: string;
   }>,
   MetaAction | TerritoryAction | ActiveStatementIdAction | AuthTokenAction
-> = createStore(
-  rootReducer,
-  initialState,
-  composeWithDevTools(applyMiddleware(...middleWare))
-);
+> = configureStore({
+  reducer: rootReducer,
+  // initialState,
+  // composeWithDevTools(applyMiddleware(...middleWare))
+});
 
 export default store;
