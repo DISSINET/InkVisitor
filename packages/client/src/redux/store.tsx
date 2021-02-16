d62de9ab6f97fcea2fc34c0f4cbe2d415d43649b
Petr Hanak <petr.nahak@gmail.com>
2021-02-16 16:42:12 +0100

    add redux toolkit auth slice

--- packages/client/src/redux/store.tsx
+++ packages/client/src/redux/store.tsx
@@ -1,22 +1,9 @@
-import { applyMiddleware, createStore, compose, CombinedState } from "redux";
-import thunk from "redux-thunk";
-import { composeWithDevTools } from "redux-devtools-extension";
 import { Store } from "redux";
 import { configureStore } from "@reduxjs/toolkit";
 
 import rootReducer from "./reducers";
-import { ResponseTerritoryI } from "@shared/types/response-territory";
-import { ResponseMetaI } from "@shared/types/response-meta";
-import {
-  MetaAction,
-  TerritoryAction,
-  ActiveStatementIdAction,
-  AuthTokenAction,
-} from "./types";
 
-const initialState = {};
-
-const middleWare = [thunk];
+// const middleWare = [thunk];
 
 // const store: Store<
 //   CombinedState<{
@@ -31,18 +18,8 @@ const middleWare = [thunk];
 //   initialState,
 //   composeWithDevTools(applyMiddleware(...middleWare))
 // );
-const store: Store<
-  CombinedState<{
-    meta: ResponseMetaI;
-    territory: ResponseTerritoryI;
-    activeStatementId: string;
-    token: string;
-  }>,
-  MetaAction | TerritoryAction | ActiveStatementIdAction | AuthTokenAction
-> = configureStore({
+const store: Store = configureStore({
   reducer: rootReducer,
-  // initialState,
-  // composeWithDevTools(applyMiddleware(...middleWare))
 });
 
 export default store;
