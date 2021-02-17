3022104a8deb201d7e6ea0cdbdf9435504b55948
adammertel <mertel.adam@gmail.com>
2021-02-17 14:56:48 +0100

    Update User and ResponseUser models

--- packages/shared/types/user.ts
+++ packages/shared/types/user.ts
@@ -1,25 +1,38 @@
-import { ActantI } from "./actant";
+import { ResponseActantI } from "./";
+import { userRoleDict } from "./../dictionaries";
 
-export interface TerritoryI extends ActantI {
-  class: "T";
-  data: {
-    label: string;
-    parent: string | false;
-  };
-  meta: {};
+const userRoleValues = userRoleDict.map((i) => i.value);
+export interface UserI {
+    id: string;
+    email: string;
+    name: string;
+    password?: string;
+    role: typeof userRoleValues[number];
+    bookmarks: BookmarkFolderI[];
+    storedTerritories: StoredTerritoryI[];
 }
 
 export interface UserOptions {
-  defaultTerritory: string;
-  defaultLanguage: string;
-  searchLanguages: string[];
+    defaultTerritory: string;
+    defaultLanguage: string;
+    searchLanguages: string[];
 }
 
-export interface User {
-  id: string;
-  email: string;
-  name: string;
-  password?: string;
-  options: UserOptions;
-  bookmarks: ActantI[];
+interface StoredTerritoryI {
+    id: string;
+    order: number;
+    territory: string;
+}
+
+interface BookmarkFolderI {
+    id: string;
+    name: string;
+    order: number;
+    actants: BookmarkActantI[];
+}
+
+interface BookmarkActantI {
+    id: string;
+    order: number;
+    actant: string;
 }
