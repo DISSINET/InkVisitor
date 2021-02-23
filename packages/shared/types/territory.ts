ab198f66ddd52a35d7025d8314c353d6a5c27c0a
adammertel <mertel.adam@gmail.com>
2021-02-23 19:37:05 +0100

    Apply new models to mock

--- packages/shared/types/territory.ts
+++ packages/shared/types/territory.ts
@@ -1,10 +1,14 @@
 import { IActant } from "./actant";
+import { languageDict } from "./../dictionaries";
 
+const languageValues = languageDict.map((i) => i.value);
 export interface ITerritory extends IActant {
     class: "T";
     data: {
-        parent: ParentTerritoryI;
+        parent: ParentTerritoryI | false;
         type: string; // territoryTypeDict
+        content: string;
+        lang: typeof languageValues[number];
     };
 }
 
