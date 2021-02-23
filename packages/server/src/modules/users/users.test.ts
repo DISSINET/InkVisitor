4e76ea07d17ae7b766fd1b97ad9a8e1c25ba0d97
jancimertel <johnny.mert@gmail.com>
2021-02-23 12:13:18 +0100

    add another tests for users/signin

--- packages/server/src/modules/users/users.test.ts
+++ packages/server/src/modules/users/users.test.ts
@@ -1,4 +1,4 @@
-import { BadParams } from "@common/errors";
+import { BadParams, UserDoesNotExits } from "@common/errors";
 import * as chai from "chai";
 import "mocha";
 import request from "supertest";
@@ -14,6 +14,31 @@ describe("Users signin", function () {
         .expect("Content-Type", /json/)
         .expect({ error: new BadParams("whatever").toString() })
         .expect(400, done);
+    });
+  });
+  describe("Ok body with faulty params ", () => {
+    it("should return a 200 code with successful response", (done) => {
+      return request(app)
+        .post(`${apiPath}/users/signin`)
+        .send({ username: "fake", password: "fake" })
+        .expect("Content-Type", /json/)
+        .expect({ error: new UserDoesNotExits("whatever").toString() })
+        .expect(400, done);
+    });
+  });
+  describe("Ok body with ok user", () => {
+    it("should return a 200 code with successful response", (done) => {
+      return request(app)
+        .post(`${apiPath}/users/signin`)
+        .send({ username: "admin", password: "admin" })
+        .expect("Content-Type", /json/)
+        .expect((res) => {
+          res.body.should.not.empty;
+          res.body.should.be.a("object");
+          res.body.should.have.keys("token");
+          res.body.token.should.not.empty;
+        })
+        .expect(200, done);
     });
   });
 });
