469205620acc3b1022dd14ea077ff9b35a648032
adammertel <mertel.adam@gmail.com>
2021-02-23 18:48:13 +0100

    Recreate data wrapper for actants

--- packages/shared/types/entity.ts
+++ packages/shared/types/entity.ts
@@ -4,5 +4,7 @@ import { entityLogicalTypeDict } from "./../dictionaries";
 const entityLogicalTypeValues = entityLogicalTypeDict.map((i) => i.value);
 export interface IEntity extends IActant {
     class: "P" | "G" | "O" | "C" | "L" | "V" | "E";
-    logicalType: typeof entityLogicalTypeValues[number];
+    data: {
+        logicalType: typeof entityLogicalTypeValues[number];
+    };
 }
--- packages/shared/types/statement.ts
+++ packages/shared/types/statement.ts
@@ -2,38 +2,40 @@ import { IActant } from "./";
 
 export interface IStatement extends IActant {
     class: "S";
-    action: string;
-    territory: {
-        id: string;
-        order: number;
+    data: {
+        action: string;
+        territory: {
+            id: string;
+            order: number;
+        };
+        references: {
+            id: string;
+            resource: string;
+            part: string;
+            type: string;
+        }[];
+        tags: string[];
+        certainty: string;
+        elvl: string;
+        modality: string;
+        text: string;
+        note: string;
+        props: {
+            id: string;
+            order: number;
+            origin: string;
+            type: string;
+            value: string;
+            elvl: string;
+            certainty: string;
+        }[];
+        actants: {
+            id: string;
+            order: number;
+            actant: string;
+            position: string;
+            elvl: string;
+            certainty: string;
+        }[];
     };
-    references: {
-        id: string;
-        resource: string;
-        part: string;
-        type: string;
-    }[];
-    tags: string[];
-    certainty: string;
-    elvl: string;
-    modality: string;
-    text: string;
-    note: string;
-    props: {
-        id: string;
-        order: number;
-        origin: string;
-        type: string;
-        value: string;
-        elvl: string;
-        certainty: string;
-    }[];
-    actants: {
-        id: string;
-        order: number;
-        actant: string;
-        position: string;
-        elvl: string;
-        certainty: string;
-    }[];
 }
