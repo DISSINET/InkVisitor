30141a228f25daaf5be3c084fa41e45070a5b942
jancimertel <johnny.mert@gmail.com>
2021-02-25 21:28:05 +0100

    add actions/create + tests

--- packages/server/src/modules/actions/index.ts
+++ packages/server/src/modules/actions/index.ts
@@ -1,7 +1,11 @@
 import { NextFunction, raw, Request, Response } from "express";
 import { Router } from "express";
 import { IAction } from "@shared/types";
-import { findActionById, findActionsByLabel } from "@service/shorthands";
+import {
+  findActionById,
+  findActionsByLabel,
+  createAction,
+} from "@service/shorthands";
 import {
   BadCredentialsError,
   BadParams,
@@ -39,5 +43,44 @@ export default Router()
       const actions = await findActionsByLabel(request.db, label);
 
       response.json(actions);
+    })
+  )
+  .post(
+    "/create",
+    asyncRouteHandler(async (request: Request, response: Response) => {
+      const actionData = request.body as IAction;
+
+      if (!actionData || !Object.keys(actionData).length) {
+        throw new BadParams("action data have to be set");
+      }
+
+      const allowedKeys = [
+        "id",
+        "parent",
+        "note",
+        "labels",
+        "types",
+        "valencies",
+        "rulesActants",
+        "rulesProperties",
+      ];
+      for (const key of Object.keys(actionData)) {
+        if (allowedKeys.indexOf(key) === -1) {
+          throw new BadParams("actant data have unsupported keys");
+        }
+      }
+
+      const result = await createAction(request.db, actionData);
+
+      if (result.inserted === 1) {
+        response.json({
+          success: true,
+        });
+      } else {
+        response.json({
+          success: false,
+          errors: result.errors,
+        });
+      }
     })
   );
