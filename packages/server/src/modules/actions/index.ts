b99c945fcb6586e5c0d36125e6d8269479c6cae1
jancimertel <johnny.mert@gmail.com>
2021-02-25 21:19:41 +0100

    add actions/getMore + tests

--- packages/server/src/modules/actions/index.ts
+++ packages/server/src/modules/actions/index.ts
@@ -1,7 +1,7 @@
 import { NextFunction, raw, Request, Response } from "express";
 import { Router } from "express";
 import { IAction } from "@shared/types";
-import { findActionById } from "@service/shorthands";
+import { findActionById, findActionsByLabel } from "@service/shorthands";
 import {
   BadCredentialsError,
   BadParams,
@@ -9,20 +9,35 @@ import {
 } from "@common/errors";
 import { asyncRouteHandler } from "..";
 
-export default Router().get(
-  "/get/:actionId?",
-  asyncRouteHandler(async (request: Request, response: Response) => {
-    const actionId = request.params.actionId;
+export default Router()
+  .get(
+    "/get/:actionId?",
+    asyncRouteHandler(async (request: Request, response: Response) => {
+      const actionId = request.params.actionId;
 
-    if (!actionId) {
-      throw new BadParams("action has to be set");
-    }
+      if (!actionId) {
+        throw new BadParams("action has to be set");
+      }
 
-    const action = await findActionById(request.db, actionId);
-    if (!action) {
-      throw new ActionDoesNotExits(`action ${actionId} was not found`);
-    }
+      const action = await findActionById(request.db, actionId);
+      if (!action) {
+        throw new ActionDoesNotExits(`action ${actionId} was not found`);
+      }
 
-    response.json(action);
-  })
-);
+      response.json(action);
+    })
+  )
+  .post(
+    "/getMore",
+    asyncRouteHandler(async (request: Request, response: Response) => {
+      const label = request.body.label;
+
+      if (!label) {
+        throw new BadParams("label has to be set");
+      }
+
+      const actions = await findActionsByLabel(request.db, label);
+
+      response.json(actions);
+    })
+  );
