6b7ad0f253e3da98a2394d545efc8ff2747a08a2
Petr Hanak <petr.nahak@gmail.com>
2021-02-25 13:21:42 +0100

    migrate modal style

--- packages/client/src/components/Modal/Modal.tsx
+++ packages/client/src/components/Modal/Modal.tsx
@@ -1,5 +1,14 @@
 import React, { ReactNode } from "react";
-import classNames from "classnames";
+
+import {
+  ModalWrap,
+  Background,
+  Card,
+  CardHeader,
+  CardTitle,
+  CardBody,
+  Footer,
+} from "./ModalStyles";
 
 interface Modal {
   children?: ReactNode;
@@ -13,37 +22,13 @@ export const Modal: React.FC<Modal> = ({
   showModal,
   disableBgClick,
 }) => {
-  const modalWrapClasses = classNames(
-    showModal ? "flex" : "hidden",
-    "flex-col",
-    "justify-center",
-    "items-center",
-    "overflow-hidden",
-    "fixed",
-    "top-0",
-    "right-0",
-    "bottom-0",
-    "left-0",
-    "z-40"
-  );
-
-  const backgroundClasses = classNames(
-    "absolute",
-    "top-0",
-    "right-0",
-    "bottom-0",
-    "left-0",
-    "bg-black",
-    "bg-opacity-75"
-  );
   return (
-    <div className={modalWrapClasses}>
-      <div
-        className={backgroundClasses}
+    <ModalWrap showModal={showModal}>
+      <Background
         onClick={(): void => (disableBgClick ? {} : onClose())}
-      ></div>
+      ></Background>
       {children}
-    </div>
+    </ModalWrap>
   );
 };
 
@@ -52,15 +37,7 @@ interface ModalCard {
   fullwidth?: boolean;
 }
 export const ModalCard: React.FC<ModalCard> = ({ children, fullwidth }) => {
-  const cardClasses = classNames(
-    "flex",
-    "flex-col",
-    "overflow-hidden",
-    "z-50",
-    "my-4",
-    fullwidth ? "w-4/5" : "w-2/4"
-  );
-  return <div className={cardClasses}>{children}</div>;
+  return <Card fullwidth={fullwidth}>{children}</Card>;
 };
 
 interface ModalHeader {
@@ -68,28 +45,15 @@ interface ModalHeader {
   onClose: Function;
 }
 export const ModalHeader: React.FC<ModalHeader> = ({ title, onClose }) => {
-  const cardHeaderClasses = classNames(
-    "flex",
-    "justify-start",
-    "items-center",
-    "flex-shrink-0",
-    "p-3",
-    "relative",
-    "bg-white",
-    "border-b-2",
-    "border-gray-200",
-    "rounded-t-sm"
-  );
-  const cardTitleClasses = classNames("flex-grow", "flex-shrink-0", "text-2xl");
   return (
-    <header className={cardHeaderClasses}>
-      <h2 className={cardTitleClasses}>{title}</h2>
-      <button
+    <CardHeader>
+      <CardTitle>{title}</CardTitle>
+      {/* <button
         type="button"
         onClick={(): void => onClose()}
         aria-label="close"
-      ></button>
-    </header>
+      ></button> */}
+    </CardHeader>
   );
 };
 
@@ -97,31 +61,12 @@ interface ModalContent {
   children?: ReactNode;
 }
 export const ModalContent: React.FC<ModalContent> = ({ children }) => {
-  const modalContentClasses = classNames(
-    "flex-grow",
-    "flex-shrink",
-    "overflow-auto",
-    "p-8",
-    "bg-white"
-  );
-  return <section className={modalContentClasses}>{children}</section>;
+  return <CardBody>{children}</CardBody>;
 };
 
 interface ModalFooter {
   children?: ReactNode;
 }
 export const ModalFooter: React.FC<ModalFooter> = ({ children }) => {
-  const modalFooterClasses = classNames(
-    "border-t-2",
-    "border-gray-200",
-    "flex",
-    "items-center",
-    "bg-white",
-    "flex-shrink-0",
-    "justify-end",
-    "relative",
-    "p-3",
-    "rounded-b-sm"
-  );
-  return <div className={modalFooterClasses}>{children}</div>;
+  return <Footer>{children}</Footer>;
 };
---
+++ packages/client/src/components/Modal/ModalStyles.tsx
@@ -0,0 +1,77 @@
+import styled from "styled-components";
+import { space3, space8 } from "Theme/constants";
+
+interface ModalWrap {
+  showModal: boolean;
+}
+export const ModalWrap = styled.div<ModalWrap>`
+  display: ${({ showModal }) => (showModal ? "flex" : "none")};
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  overflow: hidden;
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  top: 0;
+  z-index: 40;
+`;
+export const Background = styled.div`
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  top: 0;
+  background-color: rgba(0, 0, 0, 0.86);
+`;
+
+interface Card {
+  fullwidth?: boolean;
+}
+export const Card = styled.div<Card>`
+  width: ${({ fullwidth }) => (fullwidth ? "calc(100vw - 40px)" : "50%")};
+  display: flex;
+  flex-direction: column;
+  max-height: calc(100vh - 40px);
+  overflow: hidden;
+  z-index: 50;
+`;
+
+export const CardHeader = styled.header`
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  flex-shrink: 0;
+  padding: ${space3};
+  position: relative;
+  background-color: #ffffff;
+  border-bottom: 1px solid #dbdbdb;
+  border-top-left-radius: 6px;
+  border-top-right-radius: 6px;
+`;
+export const CardTitle = styled.h2`
+  flex-grow: 1;
+  flex-shrink: 0;
+  font-size: ${({ theme }) => theme.fontSizes["2xl"]};
+`;
+export const CardBody = styled.section`
+  -webkit-overflow-scrolling: touch;
+  flex-grow: 1;
+  flex-shrink: 1;
+  overflow: auto;
+  padding: ${space8};
+  background-color: #ffffff;
+`;
+export const Footer = styled.div`
+  border-bottom-left-radius: 6px;
+  border-bottom-right-radius: 6px;
+  border-top: 1px solid #dbdbdb;
+  align-items: center;
+  background-color: #ffffff;
+  display: flex;
+  flex-shrink: 0;
+  justify-content: flex-end;
+  padding: ${space3};
+  position: relative;
+`;
