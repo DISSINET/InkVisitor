c58625449e765dddb9dcace85ea61ae942055d64
jancimertel <johnny.mert@gmail.com>
2021-03-09 09:03:19 +0100

    add comments for statement type & response-territory

--- packages/shared/types/response-territory.ts
+++ packages/shared/types/response-territory.ts
@@ -6,6 +6,6 @@ import { IActant, ITerritory, IStatement } from "./";
 
 // to discuss
 export interface IResponseTerritory extends ITerritory {
-    statements: IStatement[]; // sorted statements with same territoryId
-    actants: IActant[]; // all actants in the
+  statements: IStatement[]; // sorted statements with same territoryId
+  actants: IActant[]; // all actants in the statements (actants.actanrt & tags & props.value.id & props.type.id & props.origin)
 }
