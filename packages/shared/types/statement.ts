faba7f8202bf47e91bfc088fa7f8550f82a189cd
jancimertel <johnny.mert@gmail.com>
2021-03-09 09:03:19 +0100

    add comments for statement type & response-territory

--- packages/shared/types/statement.ts
+++ packages/shared/types/statement.ts
@@ -1,32 +1,32 @@
 import { IActant, IProp } from "./";
 
 export interface IStatement extends IActant {
-    class: "S";
-    data: {
-        action: string;
-        territory: {
-            id: string;
-            order: number;
-        };
-        references: {
-            id: string;
-            resource: string;
-            part: string;
-            type: string;
-        }[];
-        tags: string[];
-        certainty: string;
-        elvl: string;
-        modality: string;
-        text: string;
-        note: string;
-        props: IProp[];
-        actants: {
-            id: string;
-            actant: string;
-            position: string;
-            elvl: string;
-            certainty: string;
-        }[];
+  class: "S";
+  data: {
+    action: string;
+    territory: {
+      id: string;
+      order: number;
     };
+    references: {
+      id: string;
+      resource: string;
+      part: string;
+      type: string;
+    }[];
+    tags: string[]; // ids of IActant
+    certainty: string;
+    elvl: string;
+    modality: string;
+    text: string;
+    note: string;
+    props: IProp[]; // this
+    actants: {
+      id: string;
+      actant: string; //  this
+      position: string;
+      elvl: string;
+      certainty: string;
+    }[];
+  };
 }
