dadbc9b23c08b3453af315e74654d03261c41003
adammertel <mertel.adam@gmail.com>
2021-03-10 20:16:59 +0100

    Move old files to legacy folder

--- packages/client/src/components/Suggester/Suggester.tsx
+++ packages/client/src/components/Suggester/Suggester.tsx
@@ -3,125 +3,129 @@ import { useDrop } from "react-dnd";
 import { FaPlus } from "react-icons/fa";
 
 import { Button, Input, Tag } from "components";
-import { OptionI } from "@shared/types";
+import { IOption } from "@shared/types";
 import { ItemTypes } from "types";
 import {
-  StyledSuggester,
-  InputWrapper,
-  SuggesterButton,
-  SuggesterList,
-  SuggestionLine,
+    StyledSuggester,
+    InputWrapper,
+    SuggesterButton,
+    SuggesterList,
+    SuggestionLine,
 } from "./SuggesterStyles";
 
 export interface SuggestionI {
-  id: string;
-  label: string;
-  category: string;
-  color: string;
+    id: string;
+    label: string;
+    category: string;
+    color: string;
 }
 
 interface SuggesterProps {
-  marginTop?: boolean;
-  suggestions: SuggestionI[];
-  placeholder?: string; // text to display when typed === ""
-  typed: string; // input value
-  category: string; // selected category
-  categories: OptionI[]; // all possible categories
-  suggestionListPosition?: string; // todo not implemented yet
-  disabled?: boolean; // todo not implemented yet
+    marginTop?: boolean;
+    suggestions: SuggestionI[];
+    placeholder?: string; // text to display when typed === ""
+    typed: string; // input value
+    category: string; // selected category
+    categories: IOption[]; // all possible categories
+    suggestionListPosition?: string; // todo not implemented yet
+    disabled?: boolean; // todo not implemented yet
 
-  // events
-  onType: Function;
-  onChangeCategory: Function;
-  onCreate: Function;
-  onPick: Function;
-  onDrop: Function;
+    // events
+    onType: Function;
+    onChangeCategory: Function;
+    onCreate: Function;
+    onPick: Function;
+    onDrop: Function;
 }
 
 const MAXSUGGESTIONDISPLAYED = 10;
 
 export const Suggester: React.FC<SuggesterProps> = ({
-  marginTop,
-  suggestions = [],
-  placeholder,
-  typed,
-  category,
-  categories,
-  suggestionListPosition,
-  disabled,
+    marginTop,
+    suggestions = [],
+    placeholder,
+    typed,
+    category,
+    categories,
+    suggestionListPosition,
+    disabled,
 
-  // events
-  onType,
-  onChangeCategory,
-  onCreate,
-  onPick,
-  onDrop,
+    // events
+    onType,
+    onChangeCategory,
+    onCreate,
+    onPick,
+    onDrop,
 }) => {
-  const [{ isOver }, dropRef] = useDrop({
-    accept: ItemTypes.TAG,
-    drop: (item) => {
-      onDrop(item);
-      console.log(item);
-    },
-    collect: (monitor) => ({
-      isOver: !!monitor.isOver(),
-    }),
-  });
+    const [{ isOver }, dropRef] = useDrop({
+        accept: ItemTypes.TAG,
+        drop: (item) => {
+            onDrop(item);
+            console.log(item);
+        },
+        collect: (monitor) => ({
+            isOver: !!monitor.isOver(),
+        }),
+    });
 
-  return (
-    <StyledSuggester marginTop={marginTop}>
-      <InputWrapper ref={dropRef} isOver={isOver}>
-        <Input
-          type="select"
-          value={category}
-          options={categories}
-          inverted
-          onChangeFn={onChangeCategory}
-        />
-        <Input
-          type="text"
-          value={typed}
-          onChangeFn={onType}
-          placeholder={placeholder}
-        />
-        <SuggesterButton>
-          <Button
-            icon={<FaPlus style={{ fontSize: "16px", padding: "2px" }} />}
-            color="primary"
-            onClick={() => {
-              onCreate({
-                label: typed,
-                category: category,
-              });
-            }}
-          />
-        </SuggesterButton>
-      </InputWrapper>
-      {suggestions.length ? (
-        <SuggesterList>
-          {suggestions
-            .filter((s, si) => si < MAXSUGGESTIONDISPLAYED)
-            .map((suggestion, si) => (
-              <SuggestionLine key={si}>
-                <Tag
-                  propId={suggestion.id}
-                  label={suggestion.label}
-                  category={suggestion.category}
-                  color={suggestion.color}
-                  button={
-                    <Button
-                      label=">"
-                      color="primary"
-                      onClick={() => {
-                        onPick(suggestion);
-                      }}
-                    />
-                  }
+    return (
+        <StyledSuggester marginTop={marginTop}>
+            <InputWrapper ref={dropRef} isOver={isOver}>
+                <Input
+                    type="select"
+                    value={category}
+                    options={categories}
+                    inverted
+                    onChangeFn={onChangeCategory}
                 />
-              </SuggestionLine>
-            ))}
-        </SuggesterList>
-      ) : null}
-    </StyledSuggester>
-  );
+                <Input
+                    type="text"
+                    value={typed}
+                    onChangeFn={onType}
+                    placeholder={placeholder}
+                />
+                <SuggesterButton>
+                    <Button
+                        icon={
+                            <FaPlus
+                                style={{ fontSize: "16px", padding: "2px" }}
+                            />
+                        }
+                        color="primary"
+                        onClick={() => {
+                            onCreate({
+                                label: typed,
+                                category: category,
+                            });
+                        }}
+                    />
+                </SuggesterButton>
+            </InputWrapper>
+            {suggestions.length ? (
+                <SuggesterList>
+                    {suggestions
+                        .filter((s, si) => si < MAXSUGGESTIONDISPLAYED)
+                        .map((suggestion, si) => (
+                            <SuggestionLine key={si}>
+                                <Tag
+                                    propId={suggestion.id}
+                                    label={suggestion.label}
+                                    category={suggestion.category}
+                                    color={suggestion.color}
+                                    button={
+                                        <Button
+                                            label=">"
+                                            color="primary"
+                                            onClick={() => {
+                                                onPick(suggestion);
+                                            }}
+                                        />
+                                    }
+                                />
+                            </SuggestionLine>
+                        ))}
+                </SuggesterList>
+            ) : null}
+        </StyledSuggester>
+    );
 };
