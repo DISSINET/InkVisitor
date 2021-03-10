f8fff21215a80e14e865cfe140b246ed5a5057af
adammertel <mertel.adam@gmail.com>
2021-03-10 18:35:51 +0100

    Add ActantDetailBox

--- packages/client/src/pages/MainPage/index.tsx
+++ packages/client/src/pages/MainPage/index.tsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from "react";
 import { connect } from "react-redux";
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
-import { useAuth0 } from "@auth0/auth0-react";
 
 import { Entities } from "types";
 
@@ -10,189 +9,37 @@ import { toast } from "react-toastify";
 
 import { Box, Button, Footer, Header, Toast } from "components";
 
-import { fetchMeta } from "redux/actions/metaActions";
-import { fetchTerritory } from "redux/actions/territoryTreeActions";
-import { setActiveStatementId } from "redux/actions/statementActions";
-import { setAuthToken } from "redux/actions/authActions";
-import { StatementsTable } from "./StatementsTable/StatementsTable";
-import { StatementEditor } from "./StatementEditor/StatementEditor";
+import {
+    ActantSearchBox,
+    ActantDetailBox,
+    ActantSuggester,
+    ActionModal,
+    ActantBookmarkBox,
+    StatementEditorBox,
+    StatementListBox,
+    TerritoryTreeBox,
+    UserAdministrationModal,
+    UserOptionsModal,
+} from "./containers";
 import { useHistory, useParams } from "react-router-dom";
-import { v4 as uuidv4 } from "uuid";
 import api from "../../api/api";
-import { deleteActant } from "api/deleteActant";
-
-import { updateActant } from "api/updateActant";
-import { createActant } from "api/createActant";
 
 interface MainPage {
-    fetchMeta: () => void;
-    meta: ResponseMetaI;
-    fetchTerritory: (id: string) => void;
-    territory: ResponseTerritoryI;
-    setActiveStatementId: (id: string) => void;
-    activeStatementId: string;
-    setAuthToken: (token: string) => void;
     size: number[];
-    token: string;
 }
 
 const initTerritory = "T40-02-07";
 
-const MainPage: React.FC<MainPage> = ({
-    meta,
-    fetchMeta,
-    fetchTerritory,
-    territory,
-    setActiveStatementId,
-    activeStatementId,
-    setAuthToken,
-    size,
-    token,
-}) => {
-    const usernameRef = React.createRef<any>();
-    const passwordRef = React.createRef<any>();
-    const handleLogin = async (event: any) => {
-        event.preventDefault();
-        await api.signIn(usernameRef.current.value, passwordRef.current.value);
-        setUser(api.getUser());
-    };
-
-    const [user, setUser] = useState(api.getUser());
-
+const MainPage: React.FC<MainPage> = ({ size }) => {
     const history = useHistory();
     const { territoryId, statementId } = useParams<{
         territoryId: string;
         statementId: string;
     }>();
 
-    /**
-     * handling GLOBAL STATE
-     */
-    const activeTerritoryChange = async (territoryId: string) => {
-        fetchTerritory(territoryId);
-        history.push(`/${territoryId}`);
-        setActiveStatementId("");
-    };
-
-    /**
-     * handling STATEMENTS
-     */
-    const statementCreateAndSet = async () => {
-        const entityClass: "S" = "S"; // wtf?
-        const newStatementId = uuidv4();
-
-        const newStatement = {
-            id: newStatementId,
-            class: entityClass,
-            data: {
-                label: "",
-                action: "A0093",
-                territory: territoryId,
-                references: [],
-                tags: [],
-                certainty: "1",
-                elvl: "1",
-                modality: "1",
-                text: "",
-                note: "",
-                props: [],
-                actants: [],
-            },
-            meta: {},
-        };
-
-        const createResponse = await createActant(newStatement);
-
-        if (createResponse && createResponse.id) {
-            await fetchTerritory(territoryId);
-            await setActiveStatementId(newStatementId);
-            history.push(`/${territoryId}/${newStatementId}`);
-        }
-        return !!createResponse;
-    };
-
-    /**
-     * handling TERRITORIES
-     */
-    const territoryCreate = async (territoryLabelToCreate: string) => {
-        const newTerritory: TerritoryI = {
-            id: uuidv4(),
-            class: "T",
-            data: {
-                label: territoryLabelToCreate,
-                parent: territoryId,
-            },
-            meta: {},
-        };
-        return actantCreate(newTerritory);
-    };
-
-    /**
-     * handling all actants
-     */
-    const actantDelete = async (actantId: string) => {
-        const response = deleteActant(actantId);
-
-        if (response) {
-            toast.dark("Statement deleted!");
-
-            // in case we are deleting the statement that is actually active
-            if (activeStatementId === actantId) {
-                setActiveStatementId("");
-            }
-
-            fetchTerritory(territoryId);
-        }
-        return !!response;
-    };
-
-    const actantUpdate = async (actant: ActantI) => {
-        const response = await updateActant(actant);
-        if (response) {
-            toast.dark("Actant updated!");
-            fetchTerritory(territoryId);
-        }
-        return !!response;
-    };
-
-    const actantCreate = async (actant: ActantI) => {
-        const response = await createActant(actant);
-        if (response) {
-            toast.dark("Actant created!");
-            fetchTerritory(territoryId);
-        }
-        return !!response;
-    };
-
-    useEffect(() => {
-        if (user) {
-        }
-    }, [user]);
-
-    useEffect(() => {
-        if (user) {
-            fetchMeta();
-        }
-    }, [fetchMeta, user, token]);
-
-    useEffect(() => {
-        if (user && token) {
-            if (territoryId) {
-                fetchTerritory(territoryId);
-            } else {
-                fetchTerritory(initTerritory);
-                history.push(`/${initTerritory}`);
-            }
-        }
-    }, [fetchTerritory, user, token, territoryId]);
-
     const heightHeader = 70;
     const heightFooter = 30;
     const heightContent = size[1] - heightHeader - heightFooter;
-
-    const activeStatement = statementId
-        ? territory.statements.find((statement) => statement.id === statementId)
-        : undefined;
 
     return (
         <>
@@ -203,7 +50,7 @@ const MainPage: React.FC<MainPage> = ({
                 left={<div className="text-4xl">InkVisitor</div>}
                 right={
                     <div className="flex">
-                        {user ? (
+                        {/* {user ? (
                             <>
                                 <div className="text-sm inline m-2">
                                     logged as {api.getUser().name}
@@ -220,124 +67,55 @@ const MainPage: React.FC<MainPage> = ({
                                 color="info"
                                 onClick={() => null}
                             />
-                        )}
+                        )} */}
                     </div>
                 }
             />
             <DndProvider backend={HTML5Backend}>
-                {user && meta ? (
-                    <div className="flex">
+                <div className="flex">
+                    <Box
+                        height={heightContent}
+                        width={200}
+                        label={"Territories"}
+                    >
+                        <TerritoryTreeBox />
+                    </Box>
+                    <Box
+                        height={heightContent}
+                        width={650}
+                        label={"Statements"}
+                    >
+                        <StatementListBox />
+                    </Box>
+                    <div className="flex flex-col">
                         <Box
-                            height={heightContent}
-                            width={200}
-                            label={"Territories"}
-                        >
-                            <TerritoryTree
-                                territory={territory}
-                                activeTerritoryChangeFn={activeTerritoryChange}
-                                territoryCreateFn={territoryCreate}
-                            />
-                        </Box>
-                        <Box
-                            height={heightContent}
-                            width={650}
-                            label={"Statements"}
-                        >
-                            <StatementsTable
-                                statements={territory.statements}
-                                meta={meta}
-                                actants={territory.actants}
-                                activeStatementId={statementId}
-                                setActiveStatementId={setActiveStatementId}
-                                statementCreateFn={statementCreateAndSet}
-                                actantDeleteFn={actantDelete}
-                            />
-                        </Box>
-                        <Box
-                            height={heightContent}
+                            height={heightContent - 400}
                             width={720}
                             label={"Editor"}
                         >
-                            {activeStatement ? (
-                                <StatementEditor
-                                    activeStatement={activeStatement}
-                                    meta={meta}
-                                    activeTerritoryActants={territory.actants}
-                                    actantDeleteFn={actantDelete}
-                                    actantCreateFn={actantCreate}
-                                    actantUpdateFn={actantUpdate}
-                                />
-                            ) : (
-                                <div>no statement selected</div>
-                            )}
+                            <StatementEditorBox />
                         </Box>
-                        <div className="flex flex-col">
-                            <Box
-                                height={400}
-                                width={350}
-                                label={"Search"}
-                            ></Box>
-                            <Box
-                                height={heightContent - 400}
-                                width={350}
-                                label={"Bookmarks"}
-                            ></Box>
-                        </div>
+                        <Box height={400} width={720} label={"Detail"}>
+                            <ActantDetailBox />
+                        </Box>
                     </div>
-                ) : (
-                    <div className="p-5">
-                        <form onSubmit={handleLogin}>
-                            <input
-                                type="text"
-                                placeholder="username"
-                                ref={usernameRef}
-                            />
-                            <input
-                                type="text"
-                                placeholder="username"
-                                ref={passwordRef}
-                            />
-                            <button type="submit">Submit</button>
-                        </form>
+                    <div className="flex flex-col">
+                        <Box height={400} width={350} label={"Search"}>
+                            <ActantSearchBox />
+                        </Box>
+                        <Box
+                            height={heightContent - 400}
+                            width={350}
+                            label={"Bookmarks"}
+                        >
+                            <ActantBookmarkBox />
+                        </Box>
                     </div>
-                )}
+                </div>
             </DndProvider>
 
             <Toast />
-            {user && <Footer height={heightFooter} />}
+            <Footer height={heightFooter} />
         </>
     );
 };
-
-const mapStateToProps = ({
-    meta,
-    territory,
-    activeStatementId,
-    token,
-}: StateFromProps): StateToProps => ({
-    meta,
-    territory,
-    activeStatementId,
-    token,
-});
-
-export default connect(mapStateToProps, {
-    fetchMeta,
-    fetchTerritory,
-    setActiveStatementId,
-    setAuthToken,
-})(MainPage);
-
-interface StateFromProps {
-    meta: ResponseMetaI;
-    territory: ResponseTerritoryI;
-    activeStatementId: string;
-    token: string;
-}
-
-interface StateToProps {
-    meta: ResponseMetaI;
-    territory: ResponseTerritoryI;
-    activeStatementId: string;
-    token: string;
-}
