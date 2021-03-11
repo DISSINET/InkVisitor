c5d4de2da2939e9263306f9940859277e0378558
adammertel <mertel.adam@gmail.com>
2021-03-11 14:26:46 +0100

    Change maxLvl to lvl in territoryTree model

--- packages/server/src/modules/tree/tree.get.test.ts
+++ packages/server/src/modules/tree/tree.get.test.ts
@@ -1,7 +1,7 @@
 import {
-  ActionDoesNotExits,
-  BadParams,
-  StatementDoesNotExits,
+    ActionDoesNotExits,
+    BadParams,
+    StatementDoesNotExits,
 } from "@common/errors";
 import * as chai from "chai";
 import "mocha";
@@ -16,37 +16,37 @@ import { createActant } from "@service/shorthands";
 const should = chai.should();
 
 const testValidTree = (res: any) => {
-  res.body.should.not.empty;
-  res.body.should.be.a("object");
-  const treeExample: IResponseTree = {
-    children: [],
-    maxLevels: 0,
-    statementsCount: 0,
-    territory: {
-      id: "",
-      class: "T",
-      data: {
-        content: "",
-        lang: "",
-        parent: false,
-        type: "",
-      },
-      labels: [],
-    },
-  };
-  res.body.should.have.keys(Object.keys(treeExample));
-  res.body.territory.should.have.keys(Object.keys(treeExample.territory));
+    res.body.should.not.empty;
+    res.body.should.be.a("object");
+    const treeExample: IResponseTree = {
+        children: [],
+        lvl: 0,
+        statementsCount: 0,
+        territory: {
+            id: "",
+            class: "T",
+            data: {
+                content: "",
+                lang: "",
+                parent: false,
+                type: "",
+            },
+            labels: [],
+        },
+    };
+    res.body.should.have.keys(Object.keys(treeExample));
+    res.body.territory.should.have.keys(Object.keys(treeExample.territory));
 };
 
 describe("Tree get", function () {
-  describe("Correct param", () => {
-    it("should return a 200 code with IResponseTree response", (done) => {
-      return request(app)
-        .get(`${apiPath}/tree/get`)
-        .set("authorization", "Bearer " + supertestConfig.token)
-        .expect(200)
-        .expect(testValidTree)
-        .then(() => done());
+    describe("Correct param", () => {
+        it("should return a 200 code with IResponseTree response", (done) => {
+            return request(app)
+                .get(`${apiPath}/tree/get`)
+                .set("authorization", "Bearer " + supertestConfig.token)
+                .expect(200)
+                .expect(testValidTree)
+                .then(() => done());
+        });
     });
-  });
 });
