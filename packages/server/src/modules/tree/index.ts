b0c2e7a1d167c7c8d9a22baf6046c2c93810e391
jancimertel <johnny.mert@gmail.com>
2021-03-12 11:21:12 +0100

    fix for tree/get - correct lvl value

--- packages/server/src/modules/tree/index.ts
+++ packages/server/src/modules/tree/index.ts
@@ -1,164 +1,150 @@
 import { Request, Response } from "express";
 import { Router } from "express";
 import {
-    findActantById,
-    getActants,
-    updateActant,
-    getTerritoryChilds,
+  findActantById,
+  getActants,
+  updateActant,
+  getTerritoryChilds,
 } from "@service/shorthands";
 import {
-    BadParams,
-    TerritoriesBrokenError,
-    TerritoryDoesNotExits,
-    TerrytoryInvalidMove,
+  BadParams,
+  TerritoriesBrokenError,
+  TerritoryDoesNotExits,
+  TerrytoryInvalidMove,
 } from "@common/errors";
 import { asyncRouteHandler } from "..";
 import { IResponseGeneric, IResponseTree, ITerritory } from "@shared/types";
 
 function populateTree(
-    root: ITerritory,
-    parentMap: Record<string, ITerritory[]>
+  root: ITerritory,
+  parentMap: Record<string, ITerritory[]>,
+  lvl: number
 ): IResponseTree {
-    const childs = parentMap[root.id]
-        ? parentMap[root.id].map((ter) => populateTree(ter, parentMap))
-        : [];
-    return {
-        territory: root,
-        statementsCount: 0, // for now
-        lvl: childs.length
-            ? Math.max(...childs.map((child) => child.lvl)) - 1
-            : 0,
-        children: childs,
-    };
+  const childs = parentMap[root.id]
+    ? parentMap[root.id].map((ter) => populateTree(ter, parentMap, lvl + 1))
+    : [];
+  return {
+    territory: root,
+    statementsCount: 0, // for now
+    lvl,
+    children: childs,
+  };
 }
 
 function insertTerritoryToChilds(
-    array: ITerritory[],
-    onIndex: number,
-    item: ITerritory
+  array: ITerritory[],
+  onIndex: number,
+  item: ITerritory
 ): ITerritory[] {
-    return [...array.slice(0, onIndex), item, ...array.slice(onIndex)];
+  return [...array.slice(0, onIndex), item, ...array.slice(onIndex)];
 }
 
 const sortTerritories = (terA: ITerritory, terB: ITerritory): number =>
-    (terA.data.parent ? terA.data.parent.order : 0) -
-    (terB.data.parent ? terB.data.parent.order : 0);
+  (terA.data.parent ? terA.data.parent.order : 0) -
+  (terB.data.parent ? terB.data.parent.order : 0);
 
 export default Router()
-    .get(
-        "/get",
-        asyncRouteHandler(async (request: Request, response: Response) => {
-            const territories = (
-                await getActants<ITerritory>(request.db, {
-                    class: "T",
-                })
-            ).sort(sortTerritories);
-
-            const parentMap: Record<string, ITerritory[]> = {};
-            for (const territory of territories) {
-                if (typeof territory.data.parent === "undefined") {
-                    continue;
-                }
-
-                const parentId: string = territory.data.parent
-                    ? territory.data.parent.id
-                    : "";
-                if (!parentMap[parentId]) {
-                    parentMap[parentId] = [];
-                }
-                parentMap[parentId].push(territory);
-            }
-
-            let root: ITerritory;
-            console.log(parentMap[""]);
-            if (parentMap[""].length != 1) {
-                throw new TerritoriesBrokenError("Territories tree is broken");
-            } else {
-                root = parentMap[""][0];
-            }
-
-            response.json(populateTree(root, parentMap));
+  .get(
+    "/get",
+    asyncRouteHandler(async (request: Request, response: Response) => {
+      const territories = (
+        await getActants<ITerritory>(request.db, {
+          class: "T",
         })
-    )
-    .post(
-        "/moveTerritory",
-        asyncRouteHandler(async (request: Request, response: Response) => {
-            const moveId = request.body.moveId;
-            const parentId = request.body.parentId;
-            const newIndex = request.body.newIndex;
+      ).sort(sortTerritories);
 
-            if (!moveId || !parentId || newIndex === undefined) {
-                throw new BadParams("moveId/parentId/newIndex has be set");
-            }
+      const parentMap: Record<string, ITerritory[]> = {};
+      for (const territory of territories) {
+        if (typeof territory.data.parent === "undefined") {
+          continue;
+        }
 
-            const territory = await findActantById<ITerritory>(
-                request.db,
-                moveId,
-                {
-                    class: "T",
-                }
-            );
-            if (!territory) {
-                throw new TerritoryDoesNotExits("territory does not exist");
-            }
+        const parentId: string = territory.data.parent
+          ? territory.data.parent.id
+          : "";
+        if (!parentMap[parentId]) {
+          parentMap[parentId] = [];
+        }
+        parentMap[parentId].push(territory);
+      }
 
-            const parent = await findActantById<ITerritory>(
-                request.db,
-                parentId,
-                {
-                    class: "T",
-                }
-            );
-            if (!parent) {
-                throw new TerritoryDoesNotExits("territory does not exist");
-            }
+      let root: ITerritory;
+      console.log(parentMap[""]);
+      if (parentMap[""].length != 1) {
+        throw new TerritoriesBrokenError("Territories tree is broken");
+      } else {
+        root = parentMap[""][0];
+      }
 
-            let childs = (await getTerritoryChilds(request.db, parentId)).sort(
-                sortTerritories
-            );
-            if (newIndex < 0 || newIndex > childs.length) {
-                throw new TerrytoryInvalidMove(
-                    "cannot move territory to invalid index"
-                );
-            }
+      response.json(populateTree(root, parentMap, 0));
+    })
+  )
+  .post(
+    "/moveTerritory",
+    asyncRouteHandler(async (request: Request, response: Response) => {
+      const moveId = request.body.moveId;
+      const parentId = request.body.parentId;
+      const newIndex = request.body.newIndex;
 
-            const out: IResponseGeneric = {
-                result: true,
-            };
+      if (!moveId || !parentId || newIndex === undefined) {
+        throw new BadParams("moveId/parentId/newIndex has be set");
+      }
 
-            if (
-                territory.data.parent &&
-                territory.data.parent.id !== parentId
-            ) {
-                territory.data.parent.id = parentId;
-            } else {
-                //  if the parent does not change -> remove the wanted child, so it can be added on specific position
-                const currentIndex = childs.findIndex(
-                    (ter) => ter.id === moveId
-                );
-                if (currentIndex === -1) {
-                    throw new TerrytoryInvalidMove(
-                        "cannot move territory- moveId not found in array of childs"
-                    );
-                }
-                if (currentIndex === newIndex) {
-                    out.result = false;
-                    out.message = "already on the new index";
-                    return response.json(out);
-                }
-                childs.splice(currentIndex, 1);
-            }
+      const territory = await findActantById<ITerritory>(request.db, moveId, {
+        class: "T",
+      });
+      if (!territory) {
+        throw new TerritoryDoesNotExits("territory does not exist");
+      }
 
-            childs = insertTerritoryToChilds(childs, newIndex, territory);
+      const parent = await findActantById<ITerritory>(request.db, parentId, {
+        class: "T",
+      });
+      if (!parent) {
+        throw new TerritoryDoesNotExits("territory does not exist");
+      }
 
-            for (let i = 0; i < childs.length; i++) {
-                const child = childs[i];
-                if (child.data.parent) {
-                    child.data.parent.order = i + 1;
-                }
-                await updateActant(request.db, child.id, child);
-            }
+      let childs = (await getTerritoryChilds(request.db, parentId)).sort(
+        sortTerritories
+      );
+      if (newIndex < 0 || newIndex > childs.length) {
+        throw new TerrytoryInvalidMove(
+          "cannot move territory to invalid index"
+        );
+      }
 
-            response.json(out);
-        })
-    );
+      const out: IResponseGeneric = {
+        result: true,
+      };
+
+      if (territory.data.parent && territory.data.parent.id !== parentId) {
+        territory.data.parent.id = parentId;
+      } else {
+        //  if the parent does not change -> remove the wanted child, so it can be added on specific position
+        const currentIndex = childs.findIndex((ter) => ter.id === moveId);
+        if (currentIndex === -1) {
+          throw new TerrytoryInvalidMove(
+            "cannot move territory- moveId not found in array of childs"
+          );
+        }
+        if (currentIndex === newIndex) {
+          out.result = false;
+          out.message = "already on the new index";
+          return response.json(out);
+        }
+        childs.splice(currentIndex, 1);
+      }
+
+      childs = insertTerritoryToChilds(childs, newIndex, territory);
+
+      for (let i = 0; i < childs.length; i++) {
+        const child = childs[i];
+        if (child.data.parent) {
+          child.data.parent.order = i + 1;
+        }
+        await updateActant(request.db, child.id, child);
+      }
+
+      response.json(out);
+    })
+  );
