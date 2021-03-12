80259c181f1603d127dceb8501f5e8254ca2e1fe
jancimertel <johnny.mert@gmail.com>
2021-03-12 12:00:29 +0100

    update territories/get so it returns statements/actants fields correctly + test

--- packages/server/src/modules/territories/territories.get.test.ts
+++ packages/server/src/modules/territories/territories.get.test.ts
@@ -1,14 +1,16 @@
 import { BadParams, TerritoryDoesNotExits } from "@common/errors";
 import { Db } from "@service/RethinkDB";
-import { createActant } from "@service/shorthands";
-import { IStatement, ITerritory } from "@shared/types";
+import { createActant, createAction, deleteActant } from "@service/shorthands";
+import { IActant, IStatement, ITerritory } from "@shared/types";
 import * as chai from "chai";
+import { link } from "fs";
 import "mocha";
 import request from "supertest";
 import { apiPath } from "../../common/constants";
 import app from "../../Server";
 
 const should = chai.should();
+const expect = chai.expect;
 
 describe("Territories get", function () {
   describe("Empty param", () => {
@@ -31,10 +33,26 @@ describe("Territories get", function () {
     it("should return a 200 code with IResponseTerritory response", async (done) => {
       const db = new Db();
       await db.initDb();
-      const testUserId = Math.random().toString();
+      const testTerritoryId = Math.random().toString();
+      const linkedStatementId = Math.random().toString();
+      const linkedActantId = Math.random().toString();
+
+      const territory: ITerritory = {
+        id: testTerritoryId,
+        class: "T",
+        data: {
+          content: "",
+          lang: "",
+          parent: false,
+          type: "",
+        },
+        labels: [],
+      };
+      await createActant(db, territory, true);
+
       const statement: IStatement = {
         class: "S",
-        id: testUserId,
+        id: linkedStatementId,
         labels: [],
         data: {
           actants: [],
@@ -45,29 +63,50 @@ describe("Territories get", function () {
           note: "",
           props: [],
           references: [],
-          tags: [],
+          tags: [linkedActantId],
           territory: {
-            id: "",
+            id: testTerritoryId,
             order: 1,
           },
           text: "",
         },
       };
 
+      const tagActant: IActant = {
+        class: "S",
+        labels: [],
+        data: {},
+        id: linkedActantId,
+      };
+      await createActant(db, tagActant, true);
       await createActant(db, statement, true);
+
       await request(app)
-        .get(`${apiPath}/territories/get/${testUserId}`)
+        .get(`${apiPath}/territories/get/${testTerritoryId}`)
         .expect(200)
         .expect((res) => {
           res.body.should.not.empty;
           res.body.should.be.a("object");
           res.body.should.have.keys([
-            ...Object.keys(statement),
+            ...Object.keys(territory),
             "statements",
             "actants",
           ]);
-          res.body.id.should.not.empty;
+
+          res.body.statements.should.be.a("array");
+          res.body.statements.should.have.length(1);
+          res.body.statements[0].should.have.keys([...Object.keys(statement)]);
+
+          res.body.actants.should.be.a("array");
+          res.body.actants.should.have.length(1);
+          res.body.actants[0].should.have.keys([...Object.keys(tagActant)]);
+
+          expect(res.body.id).to.be.eq(testTerritoryId);
         });
+
+      await deleteActant(db, testTerritoryId);
+      await deleteActant(db, linkedStatementId);
+      await deleteActant(db, linkedActantId);
       return done();
     });
   });
--- packages/shared/types/response-territory.ts
+++ packages/shared/types/response-territory.ts
@@ -7,5 +7,5 @@ import { IActant, ITerritory, IStatement } from "./";
 // to discuss
 export interface IResponseTerritory extends ITerritory {
   statements: IStatement[]; // sorted statements with same territoryId
-  actants: IActant[]; // all actants in the statements (actants.actanrt & tags & props.value.id & props.type.id & props.origin)
+  actants: IActant[]; // all actants in the statements (actants.actant & tags & props.value.id & props.type.id & props.origin)
 }
