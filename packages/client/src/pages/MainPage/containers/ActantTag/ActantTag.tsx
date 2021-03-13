f453718897e3be1e09494af0d594a72439214a59
adammertel <mertel.adam@gmail.com>
2021-03-13 13:03:14 +0100

    Create ActantTag component

---
+++ packages/client/src/pages/MainPage/containers/ActantTag/ActantTag.tsx
@@ -0,0 +1,113 @@
+import React, { ReactNode } from "react";
+import { Tag } from "components";
+import { IActant, IEntity } from "@shared/types";
+
+const classes = {
+  T: {
+    id: "T",
+    label: "Territory",
+    color: "entityT",
+  },
+  R: {
+    id: "R",
+    label: "Territory",
+    color: "entityR",
+  },
+  A: {
+    id: "A",
+    label: "Action",
+    color: "entityA",
+  },
+  S: {
+    id: "S",
+    label: "Statement",
+    color: "entityS",
+  },
+  C: {
+    id: "C",
+    label: "Concept",
+    color: "entityC",
+  },
+  E: {
+    id: "E",
+    label: "Event",
+    color: "entityE",
+  },
+  G: {
+    id: "G",
+    label: "Group",
+    color: "entityG",
+  },
+  L: {
+    id: "L",
+    label: "Location",
+    color: "entityL",
+  },
+  O: {
+    id: "O",
+    label: "Object",
+    color: "entityO",
+  },
+  P: {
+    id: "P",
+    label: "Person",
+    color: "entityP",
+  },
+  V: {
+    id: "V",
+    label: "Value",
+    color: "entityV",
+  },
+};
+
+const logicalTypes = {
+  definitive: "solid",
+  indefinitive: "dashed",
+  hypothetical: "dotted",
+};
+
+interface IActantTag {
+  actant: IActant | IEntity;
+  mode?: "selected" | "disabled" | "invalid" | false;
+  short?: boolean;
+  button?: ReactNode;
+  moveFn?: (dragIndex: number, hoverIndex: number) => void;
+}
+
+export const ActantTag: React.FC<IActantTag> = ({
+  actant,
+  short = false,
+  mode,
+  button,
+  moveFn,
+}) => {
+  const classId = actant.class;
+  const classObject = classes[classId];
+
+  // todo
+  const label = short ? "" : actant.labels[0].value;
+
+  // todo - clean
+  const borderStyle =
+    "logicalType" in actant.data
+      ? (logicalTypes[
+          actant.data?.logicalType as
+            | "definitive"
+            | "indefinitive"
+            | "hypothetical"
+        ] as "solid" | "dotted" | "dashed")
+      : "solid";
+
+  return (
+    <Tag
+      label={label}
+      short={short}
+      button={button}
+      moveFn={moveFn}
+      category={classId}
+      color={classObject.color}
+      mode={mode}
+      borderStyle={borderStyle}
+    />
+  );
+};
--- packages/client/src/pages/MainPage/containers/index.tsx
+++ packages/client/src/pages/MainPage/containers/index.tsx
@@ -1,5 +1,6 @@
 import { ActantSearchBox } from "./ActantSearchBox/ActantSearchBox";
 import { ActantDetailBox } from "./ActantDetailBox/ActantDetailBox";
+import { ActantTag } from "./ActantTag/ActantTag";
 import { ActantSuggester } from "./ActantSuggester/ActantSuggester";
 import { ActionModal } from "./ActionModal/ActionModal";
 import { ActantBookmarkBox } from "./ActantBookmarkBox/ActantBookmarkBox";
@@ -10,14 +11,15 @@ import { UserAdministrationModal } from "./UserAdmininstationModal/UserAdministr
 import { UserOptionsModal } from "./UserOptionsModal/UserOptionsModal";
 
 export {
-    ActantSearchBox,
-    ActantDetailBox,
-    ActantSuggester,
-    ActionModal,
-    ActantBookmarkBox,
-    StatementEditorBox,
-    StatementListBox,
-    TerritoryTreeBox,
-    UserAdministrationModal,
-    UserOptionsModal,
+  ActantSearchBox,
+  ActantDetailBox,
+  ActantSuggester,
+  ActantTag,
+  ActionModal,
+  ActantBookmarkBox,
+  StatementEditorBox,
+  StatementListBox,
+  TerritoryTreeBox,
+  UserAdministrationModal,
+  UserOptionsModal,
 };
