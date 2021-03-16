d8569df4efb7d7c828f9548ca11f03caad6071de
adammertel <mertel.adam@gmail.com>
2021-03-16 13:53:30 +0100

    Reorder parameters in statement model

--- packages/shared/types/statement.ts
+++ packages/shared/types/statement.ts
@@ -1,33 +1,33 @@
 import { IActant, IProp, ILabel } from "./";
 
 export interface IStatement extends IActant {
-    class: "S";
-    data: {
-        action: string;
-        territory: {
-            id: string;
-            order: number;
-        };
-        //labels: ILabel[];
-        references: {
-            id: string;
-            resource: string;
-            part: string;
-            type: string;
-        }[];
-        tags: string[]; // ids of IActant
-        certainty: string;
-        elvl: string;
-        modality: string;
-        text: string;
-        note: string;
-        props: IProp[]; // this
-        actants: {
-            id: string;
-            actant: string; //  this
-            position: string;
-            elvl: string;
-            certainty: string;
-        }[];
+  class: "S";
+  data: {
+    action: string;
+    certainty: string;
+    elvl: string;
+    modality: string;
+    text: string;
+    note: string;
+    territory: {
+      id: string;
+      order: number;
     };
+    actants: {
+      id: string;
+      actant: string; //  this
+      position: string;
+      elvl: string;
+      certainty: string;
+    }[];
+    //labels: ILabel[];
+    props: IProp[]; // this
+    references: {
+      id: string;
+      resource: string;
+      part: string;
+      type: string;
+    }[];
+    tags: string[]; // ids of IActant
+  };
 }
