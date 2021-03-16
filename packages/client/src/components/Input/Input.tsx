f5265cf9aa1451abd453a2815eeea1cb89476dbd
adammertel <mertel.adam@gmail.com>
2021-03-16 13:43:11 +0100

    Fix input component supports onBlur change

--- packages/client/src/components/Input/Input.tsx
+++ packages/client/src/components/Input/Input.tsx
@@ -1,81 +1,93 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import { IOption } from "@shared/types";
 import {
-    Wrapper,
-    Label,
-    StyledInput,
-    StyledSelect,
-    StyledTextArea,
+  Wrapper,
+  Label,
+  StyledInput,
+  StyledSelect,
+  StyledTextArea,
 } from "./InputStyles";
 
 interface InputProps {
-    label?: string;
-    value?: string;
-    inverted?: boolean;
-    type?: "text" | "textarea" | "select";
-    options?: IOption[];
-    rows?: number;
-    cols?: number;
-    onChangeFn: Function;
-    placeholder?: string;
+  label?: string;
+  value?: string;
+  inverted?: boolean;
+  type?: "text" | "textarea" | "select";
+  options?: IOption[];
+  rows?: number;
+  cols?: number;
+  onChangeFn: Function;
+  placeholder?: string;
 }
 
 export const Input: React.FC<InputProps> = ({
-    label = "",
-    inverted = false,
-    value = "",
-    type = "text",
-    options = [],
-    rows = 5,
-    cols = 50,
-    onChangeFn,
-    placeholder,
+  label = "",
+  inverted = false,
+  value = "",
+  type = "text",
+  options = [],
+  rows = 5,
+  cols = 50,
+  onChangeFn,
+  placeholder,
 }) => {
-    return (
-        <Wrapper>
-            {label && <Label className="label"> {label}</Label>}
-            {type === "text" && (
-                <StyledInput
-                    className="value"
-                    style={{ lineHeight: "16px" }}
-                    placeholder={placeholder}
-                    value={value}
-                    onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-                        onChangeFn(e.currentTarget.value);
-                    }}
-                    inverted={inverted}
-                />
-            )}
-            {type === "textarea" && (
-                <StyledTextArea
-                    className="value"
-                    placeholder={placeholder}
-                    value={value}
-                    rows={rows}
-                    cols={cols}
-                    onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) => {
-                        onChangeFn(e.target.value);
-                    }}
-                    inverted={inverted}
-                />
-            )}
-            {type === "select" && options && (
-                <StyledSelect
-                    className="value"
-                    value={value}
-                    onChange={(e: React.ChangeEvent<HTMLSelectElement>) => {
-                        onChangeFn(e.target.value);
-                    }}
-                    inverted={inverted}
-                >
-                    {options.map((option, oi) => (
-                        <option key={oi} value={option.value}>
-                            {option.label}
-                        </option>
-                    ))}
-                </StyledSelect>
-            )}
-        </Wrapper>
-    );
+  const [displayValue, setDisplayValue] = useState(value);
+
+  useEffect(() => {
+    setDisplayValue(value);
+  }, [value]);
+
+  return (
+    <Wrapper>
+      {label && <Label className="label"> {label}</Label>}
+      {type === "text" && (
+        <StyledInput
+          className="value"
+          style={{ lineHeight: "16px" }}
+          placeholder={placeholder}
+          value={displayValue}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+            setDisplayValue(e.currentTarget.value);
+          }}
+          onBlur={() => {
+            onChangeFn(displayValue);
+          }}
+          inverted={inverted}
+        />
+      )}
+      {type === "textarea" && (
+        <StyledTextArea
+          className="value"
+          placeholder={placeholder}
+          value={displayValue}
+          rows={rows}
+          cols={cols}
+          onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) => {
+            setDisplayValue(e.target.value);
+          }}
+          onBlur={() => {
+            onChangeFn(displayValue);
+          }}
+          inverted={inverted}
+        />
+      )}
+      {type === "select" && options && (
+        <StyledSelect
+          className="value"
+          value={value}
+          onChange={(e: React.ChangeEvent<HTMLSelectElement>) => {
+            onChangeFn(e.target.value);
+          }}
+          inverted={inverted}
+        >
+          {options.map((option, oi) => (
+            <option key={oi} value={option.value}>
+              {option.label}
+            </option>
+          ))}
+        </StyledSelect>
+      )}
+    </Wrapper>
+  );
 };
