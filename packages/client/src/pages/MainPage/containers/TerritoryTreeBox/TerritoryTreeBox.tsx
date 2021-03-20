498c5a147f3a6160bfb6e6844e3d94ce288cf801
Petr Hanak <petr.nahak@gmail.com>
2021-03-20 14:05:57 +0100

    code clean up

--- packages/client/src/pages/MainPage/containers/TerritoryTreeBox/TerritoryTreeBox.tsx
+++ packages/client/src/pages/MainPage/containers/TerritoryTreeBox/TerritoryTreeBox.tsx
@@ -1,34 +1,10 @@
 import React from "react";
 import { useQuery } from "react-query";
 import api from "api";
-const queryString = require("query-string");
 
-import { useLocation, useHistory } from "react-router";
-import {
-  ITerritory,
-  IResponseTreeTerritoryComponent,
-  IActant,
-} from "@shared/types";
-import { Arrow, Button, ButtonGroup, Tag } from "components";
-
-import { ActantTag } from "./../";
-import { FaCaretRight, FaCircle, FaDotCircle } from "react-icons/fa";
-import {
-  StyledChildrenWrap,
-  StyledIconWrap,
-  StyledTerritoryTagWrap,
-} from "./TerritoryTreeBoxStyle";
-import theme from "Theme/theme";
 import { TerritoryTreeNode } from "./TerritoryTreeNode";
 
 export const TerritoryTreeBox: React.FC = () => {
-  // let history = useHistory();
-  // let location = useLocation();
-
-  // var hashParams = queryString.parse(location.hash);
-  // const territoryId = hashParams.territory;
-  // const statementId = hashParams.statement;
-
   const { status, data, error, isFetching } = useQuery(
     ["statement", "territory", "tree"],
     async () => {
@@ -39,7 +15,7 @@ export const TerritoryTreeBox: React.FC = () => {
   );
 
   return (
-    <div>
+    <>
       {data && (
         <TerritoryTreeNode
           territory={data.territory}
@@ -47,6 +23,6 @@ export const TerritoryTreeBox: React.FC = () => {
           lvl={data.lvl}
         />
       )}
-    </div>
+    </>
   );
 };
