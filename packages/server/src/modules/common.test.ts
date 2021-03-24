8b275760d2b2c13884ef46f9291af09cd63fe236
jancimertel <johnny.mert@gmail.com>
2021-03-25 00:16:34 +0100

    update tree/get test + mocks

--- packages/server/src/modules/common.test.ts
+++ packages/server/src/modules/common.test.ts
@@ -1,7 +1,10 @@
-import { IResponseGeneric } from "@shared/types";
+import { IResponseGeneric, IStatement } from "@shared/types";
 import "mocha";
 import * as chai from "chai";
 import supertest from "supertest";
+import { ITerritory } from "@shared/types/index";
+import { Db } from "@service/RethinkDB";
+import { createActant, deleteActants } from "@service/shorthands";
 
 export const expect = chai.expect;
 export const should = chai.should();
@@ -24,3 +27,102 @@ export const testFaultyMessage = (res: supertest.Response): void => {
   res.body.result.should.be.false;
   res.body.errors.should.be.an("array");
 };
+
+function getRandomFromArray<T>(input: T[]): T {
+  return input[Math.floor(Math.random() * input.length)];
+}
+
+export async function createMockTree(db: Db): Promise<ITerritory[]> {
+  await deleteActants(db);
+  const randSuffix = Math.random();
+  const out: ITerritory[] = [
+    {
+      id: `root-${randSuffix}`,
+      class: "T",
+      labels: [],
+      data: {
+        content: "",
+        lang: "",
+        parent: false,
+        type: "",
+      },
+    },
+    {
+      id: `lvl1-1-${randSuffix}`,
+      class: "T",
+      labels: [],
+      data: {
+        content: "",
+        lang: "",
+        parent: {
+          id: `root-${randSuffix}`,
+          order: 1,
+        },
+        type: "",
+      },
+    },
+    {
+      id: `lvl1-2-${randSuffix}`,
+      class: "T",
+      labels: [],
+      data: {
+        content: "",
+        lang: "",
+        parent: {
+          id: `root-${randSuffix}`,
+          order: 2,
+        },
+        type: "",
+      },
+    },
+  ];
+
+  for (const ter of out) {
+    await createActant(db, ter, true);
+  }
+  return out;
+}
+
+export async function createMockStatements(
+  db: Db,
+  territories: ITerritory[]
+): Promise<IStatement[]> {
+  const randSuffix = Math.random();
+  const forStatement = getRandomFromArray<ITerritory>(territories).id;
+
+  const out: IStatement[] = [];
+
+  // create statements with territory id set
+  for (let i = 0; i < 3; i++) {
+    out.push({
+      id: `statement-${i}-${randSuffix}`,
+      class: "S",
+      labels: [],
+      data: {
+        territory: {
+          id: forStatement,
+          order: i + 1,
+        },
+        actants: [],
+        action: "",
+        certainty: "",
+        elvl: "",
+        modality: "",
+        note: "",
+        props: [],
+        references: [],
+        tags: [],
+        text: "",
+      },
+    });
+  }
+
+  for (const ter of out) {
+    await createActant(db, ter, true);
+  }
+  return out;
+}
+
+export async function clean(db: Db): Promise<void> {
+  await deleteActants(db);
+}
--- packages/server/src/modules/tree/tree.get.test.ts
+++ packages/server/src/modules/tree/tree.get.test.ts
@@ -1,43 +1,103 @@
-import "@modules/common.test";
+import {
+  expect,
+  createMockTree,
+  createMockStatements,
+  clean,
+} from "@modules/common.test";
 import request from "supertest";
 import { supertestConfig } from "..";
 import { apiPath } from "../../common/constants";
 import app from "../../Server";
-import { IResponseTree } from "@shared/types";
+import { IResponseTree, IStatement, ITerritory } from "@shared/types";
+import { Db } from "@service/RethinkDB";
 
-const testValidTree = (res: any) => {
-  res.body.should.not.empty;
-  res.body.should.be.a("object");
-  const treeExample: IResponseTree = {
-    children: [],
-    lvl: 0,
-    statementsCount: 0,
-    territory: {
-      id: "",
-      class: "T",
-      data: {
-        content: "",
-        lang: "",
-        parent: false,
-        type: "",
-      },
-      label: "",
-    },
-    path: [],
-  };
-  res.body.should.have.keys(Object.keys(treeExample));
-  res.body.territory.should.have.keys(Object.keys(treeExample.territory));
+const findSubtreeInTree = (
+  territories: IResponseTree,
+  wantedId: string
+): IResponseTree | null => {
+  if (territories.territory.id === wantedId) {
+    return territories;
+  }
+
+  for (const child of territories.children) {
+    const result = findSubtreeInTree(child, wantedId);
+    if (result) {
+      return result;
+    }
+  }
+
+  return null;
+};
+
+const testCorrectRootTerritory = (mockTerritories: ITerritory[], res: any) => {
+  expect(res.body.territory).to.be.deep.eq(mockTerritories[0]);
+};
+
+const testCorrectStatementsCount = (
+  statementsTerritoryId: string,
+  statements: IStatement[],
+  res: any
+) => {
+  const territoryWithStatements = findSubtreeInTree(
+    res.body,
+    statementsTerritoryId
+  );
+  if (!territoryWithStatements) {
+    expect(territoryWithStatements).to.be.not.eq(null);
+  } else {
+    expect(territoryWithStatements.statementsCount).to.be.eq(statements.length);
+  }
+};
+
+const hasParent = (
+  mockTerritories: ITerritory[],
+  childId: string,
+  parentId: string
+): boolean => {
+  const child = mockTerritories.find((t) => t.id === childId);
+  if (child) {
+    return child.data.parent && child.data.parent.id === parentId;
+  }
+  return false;
+};
+
+const testCorrectPaths = (mockTerritories: ITerritory[], res: any) => {
+  (function testPath(rootTree: IResponseTree) {
+    let currentId = rootTree.territory.id;
+    for (const parentId of rootTree.path) {
+      expect(hasParent(mockTerritories, currentId, parentId)).to.be.eq(true);
+      currentId = parentId;
+    }
+    for (const child of rootTree.children) {
+      testPath(child);
+    }
+  })(res.body);
 };
 
 describe("Tree get", function () {
-  describe("Correct param", () => {
-    it("should return a 200 code with IResponseTree response", (done) => {
-      return request(app)
-        .get(`${apiPath}/tree/get`)
-        .set("authorization", "Bearer " + supertestConfig.token)
-        .expect(200)
-        .expect(testValidTree)
-        .then(() => done());
-    });
+  it("should return a 200 code with IResponseTree response", async (done) => {
+    const db = new Db();
+    await db.initDb();
+
+    const territories = await createMockTree(db);
+    const statements = await createMockStatements(db, territories);
+    const statementsTerritoryId = statements[0].data.territory.id;
+
+    await request(app)
+      .get(`${apiPath}/tree/get`)
+      .set("authorization", "Bearer " + supertestConfig.token)
+      .expect(200)
+      .expect(testCorrectRootTerritory.bind(undefined, territories))
+      .expect(
+        testCorrectStatementsCount.bind(
+          undefined,
+          statementsTerritoryId,
+          statements
+        )
+      )
+      .expect(testCorrectPaths.bind(undefined, territories));
+
+    await clean(db);
+    return done();
   });
 });
