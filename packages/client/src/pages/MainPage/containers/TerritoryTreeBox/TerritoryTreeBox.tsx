a414e56270963ff78108ecf31781e7818b165230
Petr Hanak <petr.nahak@gmail.com>
2021-04-14 22:38:12 +0200

    fetch selected territory to tree box

--- packages/client/src/pages/MainPage/containers/TerritoryTreeBox/TerritoryTreeBox.tsx
+++ packages/client/src/pages/MainPage/containers/TerritoryTreeBox/TerritoryTreeBox.tsx
@@ -19,32 +19,20 @@ export const TerritoryTreeBox: React.FC = () => {
   );
   var hashParams = queryString.parse(location.hash);
   const territoryId = hashParams.territory;
+  const {
+    status: territoryStatus,
+    data: territory,
+    error: territoryError,
+    isFetching: territoryIsFetching,
+  } = useQuery(
+    ["territory"],
+    async () => {
+      const res = await api.territoryGet(territoryId);
+      return res.data;
+    },
+    {}
+  );
   const [selectedTerritory, setSelectedTerritory] = useState<IResponseTree>();
-
-  // const searchTree = (
-  //   element: IResponseTree,
-  //   matchingTitle: string
-  // ): IResponseTree | null => {
-  //   if (element.territory.id === matchingTitle) {
-  //     return element;
-  //   } else if (element.children != null) {
-  //     var i;
-  //     var result = null;
-  //     for (i = 0; result === null && i < element.children.length; i++) {
-  //       result = searchTree(element.children[i], matchingTitle);
-  //     }
-  //     return result;
-  //   }
-  //   return null;
-  // };
-  // useEffect(() => {
-  //   if (data) {
-  //     const foundTerritory = searchTree(data, territoryId);
-  //     if (foundTerritory) {
-  //       setSelectedTerritory(foundTerritory);
-  //     }
-  //   }
-  // }, [data]);
 
   if (isFetching) {
     return (
