e8e433ad725485100baf83d55f205e552de82c3f
Ptrhnk <petr.nahak@gmail.com>
2021-04-16 02:24:56 +0200

    Feature/login page (#131)
    * add login page and redux token handling with types
    
    * clean up

---
+++ packages/client/src/pages/LogInPage/index.tsx
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import { connect } from "react-redux";
+
+import { Button, Input } from "components";
+import { setAuthToken } from "redux/features/authTokenSlice";
+import { useAppDispatch, useAppSelector } from "redux/hooks";
+import { StyledLogInBox, StyledPage } from "./LogInPageStyles";
+import { api } from "api";
+
+interface LogInPage {
+  size: number[];
+}
+const LogInPage: React.FC<LogInPage> = ({}) => {
+  const dispatch = useAppDispatch();
+  const token = useAppSelector((state) => state.token);
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogIn = () => {
+    // const res = api.checkLogin();
+    dispatch(setAuthToken(Math.floor(Math.random() * 100).toString()));
+  };
+
+  return (
+    <StyledPage>
+      <StyledLogInBox>
+        <h2>InkVisitor</h2>
+        <Input
+          placeholder="username"
+          onChangeFn={(text: string) => setUsername(text)}
+          value={username}
+        />
+        <Input
+          placeholder="password"
+          onChangeFn={(text: string) => setPassword(text)}
+          value={password}
+        />
+        <h5>{token}</h5>
+        <Button onClick={() => handleLogIn()} label={"Log In"} color="danger" />
+      </StyledLogInBox>
+    </StyledPage>
+  );
+};
+
+export default LogInPage;
