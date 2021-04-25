99b5f06b3c27d2096f8c825965bf88b1382ce79d
jancimertel <johnny.mert@gmail.com>
2021-04-25 23:49:05 +0200

    add entity & resource implementations

---
+++ packages/server/src/models/entity.ts
@@ -0,0 +1,70 @@
+import { fillFlatObject, UnknownObject, IModel, IDbModel } from "./common";
+import { r as rethink, Connection, WriteResult } from "rethinkdb-ts";
+import { ActantType, EntityActantType } from "@shared/enums";
+import { IEntity, entityLogicalTypeValues } from "@shared/types/entity";
+
+class EntityData implements IModel {
+  logicalType: typeof entityLogicalTypeValues[number] = "s";
+
+  constructor(data: UnknownObject) {
+    if (!data) {
+      return;
+    }
+  }
+
+  isValid(): boolean {
+    return true;
+  }
+}
+
+class Entity implements IEntity, IDbModel {
+  static table = "actants";
+
+  id = "";
+  class: EntityActantType = ActantType.Concept; // just default
+  label = "";
+  data = new EntityData({});
+
+  constructor(data: UnknownObject) {
+    if (!data) {
+      return;
+    }
+
+    fillFlatObject(this, data);
+    this.data = new EntityData(data.data as UnknownObject);
+  }
+
+  isValid(): boolean {
+    const alloweedClasses = [
+      ActantType.Person,
+      ActantType.Group,
+      ActantType.Object,
+      ActantType.Concept,
+      ActantType.Location,
+      ActantType.Value,
+      ActantType.Event,
+    ];
+    if (alloweedClasses.indexOf(this.class) === -1) {
+      return false;
+    }
+
+    return this.data.isValid();
+  }
+
+  save(db: Connection | undefined): Promise<WriteResult> {
+    return rethink.table(Entity.table).insert(this).run(db);
+  }
+
+  update(
+    db: Connection | undefined,
+    updateData: Record<string, unknown>
+  ): Promise<WriteResult> {
+    return rethink.table(Entity.table).get(this.id).update(updateData).run(db);
+  }
+
+  delete(db: Connection | undefined): Promise<WriteResult> {
+    return rethink.table(Entity.table).get(this.id).delete().run(db);
+  }
+}
+
+export default Entity;
--- packages/server/src/models/factory.ts
+++ packages/server/src/models/factory.ts
@@ -2,6 +2,8 @@ import { IDbModel, UnknownObject } from "./common";
 import { ActantType } from "@shared/enums";
 import Territory from "./territory";
 import Statement from "./statement";
+import Entity from "./entity";
+import Resource from "./resource";
 
 export function getActantType(data: UnknownObject): IDbModel | null {
   if (!data || typeof data !== "object" || Object.keys(data).length === 0) {
@@ -13,6 +15,22 @@ export function getActantType(data: UnknownObject): IDbModel | null {
       return new Territory(data);
     case ActantType.Statement:
       return new Statement(data);
+    case ActantType.Person:
+      return new Entity(data);
+    case ActantType.Group:
+      return new Entity(data);
+    case ActantType.Object:
+      return new Entity(data);
+    case ActantType.Concept:
+      return new Entity(data);
+    case ActantType.Location:
+      return new Entity(data);
+    case ActantType.Value:
+      return new Entity(data);
+    case ActantType.Event:
+      return new Entity(data);
+    case ActantType.Resource:
+      return new Resource(data);
     default:
       return null;
   }
