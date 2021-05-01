f1e1710b98237d6cf14b52cba8f7719177b153c2
Ptrhnk <petr.nahak@gmail.com>
2021-05-02 00:07:00 +0200

    Feature/improvements (#164)
    * improve statement list loader
    
    * loader component
    
    * add loader to detail and editor

--- packages/client/src/pages/MainPage/containers/StatementsListBox/StatementListBox.tsx
+++ packages/client/src/pages/MainPage/containers/StatementsListBox/StatementListBox.tsx
@@ -3,11 +3,6 @@ import { Cell } from "react-table";
 import { useQuery, useQueryClient } from "react-query";
 import {
   FaInfo,
-  FaPencilAlt,
-  FaRegCheckSquare,
-  FaRegSquare,
-  FaSquare,
-  FaClone,
   FaTrashAlt,
   FaPlus,
   FaRegCircle,
@@ -15,18 +10,16 @@ import {
 } from "react-icons/fa";
 import { useLocation, useHistory } from "react-router";
 import { toast } from "react-toastify";
-import { DotLoader } from "react-spinners";
 const queryString = require("query-string");
 
-import { Button, ButtonGroup, TagGroup, Tooltip } from "components";
+import { Button, ButtonGroup, Loader, TagGroup, Tooltip } from "components";
 import { ActantTag } from "./../";
 import api from "api";
 import { IStatement, IActant, IAction } from "@shared/types";
 import { ActantType } from "@shared/enums";
 import { StatementListTable } from "./StatementListTable/StatementListTable";
-import { StyledDots, StyledLoaderWrap } from "./StatementLitBoxStyles";
+import { StyledDots } from "./StatementLitBoxStyles";
 import { CStatement } from "constructors";
-import theme from "Theme/theme";
 
 const initialData: {
   statements: IStatement[];
@@ -254,20 +247,16 @@ export const StatementListBox: React.FC = () => {
     }
   };
 
-  if (isFetching) {
-    return (
-      <StyledLoaderWrap>
-        <DotLoader color={theme.color["primary"]} />
-      </StyledLoaderWrap>
-    );
-  }
   return (
-    <StatementListTable
-      data={statements}
-      columns={columns}
-      handleRowClick={(rowId: string) => {
-        //console.log(rowId);
-      }}
-    />
+    <>
+      <StatementListTable
+        data={statements}
+        columns={columns}
+        handleRowClick={(rowId: string) => {
+          //console.log(rowId);
+        }}
+      />
+      <Loader show={isFetching} />
+    </>
   );
 };
--- packages/client/src/pages/MainPage/containers/StatementsListBox/StatementLitBoxStyles.tsx
+++ packages/client/src/pages/MainPage/containers/StatementsListBox/StatementLitBoxStyles.tsx
@@ -4,10 +4,3 @@ export const StyledDots = styled.div`
   display: flex;
   align-items: flex-end;
 `;
-export const StyledLoaderWrap = styled.div`
-  height: 100%;
-  width: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
