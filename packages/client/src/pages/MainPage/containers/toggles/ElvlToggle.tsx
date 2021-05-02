04cbcb21d65ba969049ee83d7edfcaee50bf1696
adammertel <mertel.adam@gmail.com>
2021-05-02 22:40:37 +0200

    Make Elvl Toggle as a dropdown

--- packages/client/src/pages/MainPage/containers/toggles/ElvlToggle.tsx
+++ packages/client/src/pages/MainPage/containers/toggles/ElvlToggle.tsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useMemo } from "react";
+import styled from "styled-components";
 import { FaBook } from "react-icons/fa";
 
-import { Toggle } from "components";
+import { Toggle, Dropdown } from "components";
 
 import { elvlDict } from "./../../../../../../shared/dictionaries";
 
@@ -14,19 +15,43 @@ export const ElvlToggle: React.FC<IElvlToggle> = ({ onChangeFn, value }) => {
   const items = elvlDict.map((i) => {
     return {
       value: i.value,
-      label: i.value,
+      label: i.label,
       tooltip: i.label,
     };
   });
+
+  const selectedItem = useMemo(() => {
+    return items.find((i) => i.value === value);
+  }, [value]);
+
   return (
-    <Toggle
-      icon={<FaBook />}
-      inverted
-      onChangeFn={(newValue) => {
+    <Dropdown
+      label={selectedItem?.label}
+      options={items}
+      value={selectedItem}
+      onChange={(newValue: any) => {
         onChangeFn(newValue.value);
       }}
-      optionList={items}
-      selectedValue={value}
+      formatOptionLabel={(option: any, optionProps: any) => {
+        return optionProps.context === "menu" ? (
+          <StyledModalityContentSpan>{option.label}</StyledModalityContentSpan>
+        ) : (
+          <StyledModalityContentSpan>
+            <FaBook />
+            {option.value}
+          </StyledModalityContentSpan>
+        );
+      }}
+      width={40}
+      menuWidth={100}
+      noDropDownIndicator
     />
   );
 };
+
+const StyledModalityContentSpan = styled.span`
+  svg {
+    vertical-align: middle;
+    margin-right: 4px;
+  }
+`;
