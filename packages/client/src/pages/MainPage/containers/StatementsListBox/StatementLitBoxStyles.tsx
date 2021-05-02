4da80ccbc7642a2a60f2b1c626fd73eb79f1a335
adammertel <mertel.adam@gmail.com>
2021-05-02 16:50:12 +0200

    Creating statement through the header button

--- packages/client/src/pages/MainPage/containers/StatementsListBox/StatementLitBoxStyles.tsx
+++ packages/client/src/pages/MainPage/containers/StatementsListBox/StatementLitBoxStyles.tsx
@@ -17,3 +17,12 @@ export const StyledSelectorCell = styled.div`
   cursor: pointer;
   margin: -0.5em 0em;
 `;
+
+export const StyledStatementListHeader = styled.div`
+  display: inline-flex;
+  align-items: center;
+`;
+export const StyledStatementListHeaderTitle = styled.h3``;
+export const StyledStatementListHeaderActions = styled.div`
+  margin-left: 10px;
+`;
