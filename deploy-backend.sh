508b72ee8a3bcf72ba2c4210fcadf2620b52661a
jancimertel <johnny.mert@gmail.com>
2021-05-03 00:46:23 +0200

    add package jsons to rsync command in deploy scripts

--- deploy-backend.sh
+++ deploy-backend.sh
@@ -2,5 +2,5 @@
 echo -n "Enter your name and press [ENTER]: "
 read username
 
-rsync -a ./packages/server/env ./packages/server/secret ./packages/server/dist  ${username}@10.16.30.211:/var/www/html/apps/inkvisitor/server 
+rsync -a ./packages/server/package* ./packages/server/env ./packages/server/secret ./packages/server/dist  ${username}@10.16.30.211:/var/www/html/apps/inkvisitor/server 
 ssh ${username}@10.16.30.211 'cd /var/www/html/apps/inkvisitor/server && npm install && npm run restart:prod'
--- deploy-dev-backend.sh
+++ deploy-dev-backend.sh
@@ -2,5 +2,5 @@
 echo -n "Enter your name and press [ENTER]: "
 read username
 
-rsync -a ./packages/server/env ./packages/server/secret ./packages/server/dist  ${username}@10.16.30.211:/var/www/html/apps/inkvisitor-dev/server 
+rsync -a ./packages/server/package* ./packages/server/env ./packages/server/secret ./packages/server/dist  ${username}@10.16.30.211:/var/www/html/apps/inkvisitor-dev/server 
 ssh ${username}@10.16.30.211 'cd /var/www/html/apps/inkvisitor-dev/server && npm install && npm run restart:prod'
