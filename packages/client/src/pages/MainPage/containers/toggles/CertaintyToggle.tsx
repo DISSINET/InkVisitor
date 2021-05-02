a75846f38df2503e4998ddc0bc45b07bd83064e1
adammertel <mertel.adam@gmail.com>
2021-05-02 22:36:15 +0200

    Make certainty toggle as dropdown

--- packages/client/src/pages/MainPage/containers/toggles/CertaintyToggle.tsx
+++ packages/client/src/pages/MainPage/containers/toggles/CertaintyToggle.tsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useMemo } from "react";
+import styled from "styled-components";
+
 import { FaQuestionCircle } from "react-icons/fa";
+import { IoMdChatbubbles } from "react-icons/io";
 
-import { Toggle } from "components";
-
+import { Toggle, Dropdown } from "components";
 import { certaintyDict } from "./../../../../../../shared/dictionaries";
 
 interface ICertaintyToggle {
@@ -17,19 +19,52 @@ export const CertaintyToggle: React.FC<ICertaintyToggle> = ({
   const items = certaintyDict.map((i) => {
     return {
       value: i.value,
-      label: i.value,
+      label: i.label,
       tooltip: i.label,
     };
   });
+
+  const selectedItem = useMemo(() => {
+    return items.find((i) => i.value === value);
+  }, [value]);
+
   return (
-    <Toggle
-      icon={<FaQuestionCircle />}
-      inverted
-      onChangeFn={(newValue) => {
+    <Dropdown
+      label={selectedItem?.label}
+      options={items}
+      value={selectedItem}
+      onChange={(newValue: any) => {
         onChangeFn(newValue.value);
       }}
-      selectedValue={value}
-      optionList={items}
+      formatOptionLabel={(option: any, optionProps: any) => {
+        return optionProps.context === "menu" ? (
+          <StyledModalityContentSpan>{option.label}</StyledModalityContentSpan>
+        ) : (
+          <StyledModalityContentSpan>
+            <IoMdChatbubbles />
+            {option.value}
+          </StyledModalityContentSpan>
+        );
+      }}
+      width={40}
+      menuWidth={100}
+      noDropDownIndicator
     />
+    // <Toggle
+    //   icon={<FaQuestionCircle />}
+    //   inverted
+    //   onChangeFn={(newValue) => {
+    //     onChangeFn(newValue.value);
+    //   }}
+    //   selectedValue={value}
+    //   optionList={items}
+    // />
   );
 };
+
+const StyledModalityContentSpan = styled.span`
+  svg {
+    vertical-align: middle;
+    margin-right: 4px;
+  }
+`;
