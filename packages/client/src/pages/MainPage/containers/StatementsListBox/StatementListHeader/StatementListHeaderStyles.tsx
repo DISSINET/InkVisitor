edaf0befe5ffb3c58aa8eb09092549fb9dc454f2
Petr Hanak <petr.nahak@gmail.com>
2021-05-05 21:49:44 +0200

    breadcrumb with id links

---
+++ packages/client/src/pages/MainPage/containers/StatementsListBox/StatementListHeader/StatementListHeaderStyles.tsx
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const StyledHeader = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+export const StyledHeaderBreadcrumbRow = styled.div`
+  display: inline-flex;
+  align-items: center;
+`;
+export const StyledHeaderRow = styled.div`
+  display: inline-flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+export const StyledTitle = styled.h4`
+  display: inline-flex;
+`;
