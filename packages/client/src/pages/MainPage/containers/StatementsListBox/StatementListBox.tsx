d565d461757a0fa19f0efa5fb3de89f74f7ff858
adammertel <mertel.adam@gmail.com>
2021-05-06 22:42:22 +0200

    Fix styling of props rows in editor

--- packages/client/src/pages/MainPage/containers/StatementsListBox/StatementListBox.tsx
+++ packages/client/src/pages/MainPage/containers/StatementsListBox/StatementListBox.tsx
@@ -46,6 +46,7 @@ export const StatementListBox: React.FC = () => {
     ["territory", "statement-list", territoryId],
     async () => {
       const res = await api.territoryGet(territoryId);
+      console.log("data", res.data);
       return res.data;
     },
     { initialData: initialData, enabled: !!territoryId && api.isLoggedIn() }
