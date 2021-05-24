45941528e7f3f3aa770db6e2389854df2466dca3
adammertel <mertel.adam@gmail.com>
2021-05-24 14:25:20 +0200

    Fix statement list action type styling

--- packages/client/src/pages/MainPage/containers/StatementsListBox/StatementLitBoxStyles.tsx
+++ packages/client/src/pages/MainPage/containers/StatementsListBox/StatementLitBoxStyles.tsx
@@ -17,3 +17,11 @@ export const StyledSelectorCell = styled.div`
   cursor: pointer;
   margin: -0.5em 0em;
 `;
+
+export const StyledActionLabel = styled.div`
+  font-size: 90%;
+  max-width: 12em;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+`;
