9101496e12aec8df6575c3f0e1ae0ef775e93adf
Ptrhnk <petr.nahak@gmail.com>
2021-05-25 23:52:55 +0200

    Feature/improvements (#221)
    * tooltips for T in tree
    
    * position tooltip to right
    
    * clean up

--- packages/client/src/pages/MainPage/containers/StatementEditorBox/StatementEditorActantListRow/StatementEditorActantListRow.tsx
+++ packages/client/src/pages/MainPage/containers/StatementEditorBox/StatementEditorActantListRow/StatementEditorActantListRow.tsx
@@ -28,162 +28,164 @@ interface StatementEditorActantListRow {
   moveFn: (dragIndex: number, hoverIndex: number) => void;
   updateOrderFn: () => void;
 }
-export const StatementEditorActantListRow: React.FC<StatementEditorActantListRow> = ({
-  index,
-  actant,
-  sActant,
-  statement,
-  statementId,
-  classEntitiesActant,
-  moveFn,
-  updateOrderFn,
-}) => {
-  const queryClient = useQueryClient();
-  const dropRef = useRef<HTMLDivElement>(null);
-  const dragRef = useRef<HTMLDivElement>(null);
+export const StatementEditorActantListRow: React.FC<StatementEditorActantListRow> =
+  ({
+    index,
+    actant,
+    sActant,
+    statement,
+    statementId,
+    classEntitiesActant,
+    moveFn,
+    updateOrderFn,
+  }) => {
+    const queryClient = useQueryClient();
+    const dropRef = useRef<HTMLDivElement>(null);
+    const dragRef = useRef<HTMLDivElement>(null);
 
-  const updateActant = (statementActantId: string, changes: any) => {
-    if (statement && statementActantId) {
-      const updatedActants = statement.data.actants.map((a) =>
-        a.id === statementActantId ? { ...a, ...changes } : a
-      );
-      const newData = { actants: updatedActants };
-      updateApiCall(newData);
-    }
-  };
-  const removeActant = (statementActantId: string) => {
-    if (statement) {
-      const updatedActants = statement.data.actants.filter(
-        (a) => a.id !== statementActantId
-      );
-      const newData = { actants: updatedActants };
-      updateApiCall(newData);
-    }
-  };
-  const updateApiCall = async (changes: object) => {
-    const res = await api.actantsUpdate(statementId, {
-      data: changes,
+    const updateActant = (statementActantId: string, changes: any) => {
+      if (statement && statementActantId) {
+        const updatedActants = statement.data.actants.map((a) =>
+          a.id === statementActantId ? { ...a, ...changes } : a
+        );
+        const newData = { actants: updatedActants };
+        updateApiCall(newData);
+      }
+    };
+    const removeActant = (statementActantId: string) => {
+      if (statement) {
+        const updatedActants = statement.data.actants.filter(
+          (a) => a.id !== statementActantId
+        );
+        const newData = { actants: updatedActants };
+        updateApiCall(newData);
+      }
+    };
+    const updateApiCall = async (changes: object) => {
+      const res = await api.actantsUpdate(statementId, {
+        data: changes,
+      });
+      queryClient.invalidateQueries(["statement"]);
+    };
+
+    const [, drop] = useDrop({
+      accept: ItemTypes.ROW,
+      hover(item: DragItem, monitor: DropTargetMonitor) {
+        if (!dropRef.current) {
+          return;
+        }
+        const dragIndex = item.index;
+        const hoverIndex = index;
+        if (dragIndex === hoverIndex) {
+          return;
+        }
+        const hoverBoundingRect = dropRef.current?.getBoundingClientRect();
+        const hoverMiddleY =
+          (hoverBoundingRect.bottom - hoverBoundingRect.top) / 2;
+        const clientOffset = monitor.getClientOffset();
+        const hoverClientY =
+          (clientOffset as XYCoord).y - hoverBoundingRect.top;
+        if (dragIndex < hoverIndex && hoverClientY < hoverMiddleY) {
+          return;
+        }
+        if (dragIndex > hoverIndex && hoverClientY > hoverMiddleY) {
+          return;
+        }
+        moveFn(dragIndex, hoverIndex);
+        item.index = hoverIndex;
+      },
     });
-    queryClient.invalidateQueries(["statement"]);
-  };
 
-  const [, drop] = useDrop({
-    accept: ItemTypes.ROW,
-    hover(item: DragItem, monitor: DropTargetMonitor) {
-      if (!dropRef.current) {
-        return;
-      }
-      const dragIndex = item.index;
-      const hoverIndex = index;
-      if (dragIndex === hoverIndex) {
-        return;
-      }
-      const hoverBoundingRect = dropRef.current?.getBoundingClientRect();
-      const hoverMiddleY =
-        (hoverBoundingRect.bottom - hoverBoundingRect.top) / 2;
-      const clientOffset = monitor.getClientOffset();
-      const hoverClientY = (clientOffset as XYCoord).y - hoverBoundingRect.top;
-      if (dragIndex < hoverIndex && hoverClientY < hoverMiddleY) {
-        return;
-      }
-      if (dragIndex > hoverIndex && hoverClientY > hoverMiddleY) {
-        return;
-      }
-      moveFn(dragIndex, hoverIndex);
-      item.index = hoverIndex;
-    },
-  });
+    const [{ isDragging }, drag, preview] = useDrag({
+      item: { type: ItemTypes.ROW, index, id: sActant.id },
+      collect: (monitor: DragSourceMonitor) => ({
+        isDragging: monitor.isDragging(),
+      }),
+      end: updateOrderFn,
+    });
 
-  const [{ isDragging }, drag, preview] = useDrag({
-    item: { type: ItemTypes.ROW, index, id: sActant.id },
-    collect: (monitor: DragSourceMonitor) => ({
-      isDragging: monitor.isDragging(),
-    }),
-    end: updateOrderFn,
-  });
+    drag(drop(dropRef));
 
-  drag(drop(dropRef));
+    // const opacity = isDragging ? 0.2 : 1;
+    const opacity = 1;
 
-  // const opacity = isDragging ? 0.2 : 1;
-  const opacity = 1;
+    return (
+      <>
+        <StyledActantListItem ref={dropRef}>
+          <FaGripVertical style={{ cursor: "move" }} />
+        </StyledActantListItem>
 
-  return (
-    <React.Fragment>
-      <StyledActantListItem ref={dropRef}>
-        <FaGripVertical style={{ cursor: "move" }} />
-      </StyledActantListItem>
-
-      <StyledActantListItem opacity={opacity}>
-        {actant ? (
-          <ActantTag
-            actant={actant}
-            short={false}
-            button={
-              <Button
-                key="d"
-                tooltip="unlink actant"
-                icon={<FaUnlink />}
-                color="danger"
-                onClick={() => {
-                  updateActant(sActant.id, {
-                    actant: "",
-                  });
-                }}
-              />
-            }
-          />
-        ) : (
-          <ActantSuggester
-            onSelected={(newSelectedId: string) => {
+        <StyledActantListItem opacity={opacity}>
+          {actant ? (
+            <ActantTag
+              actant={actant}
+              short={false}
+              button={
+                <Button
+                  key="d"
+                  tooltip="unlink actant"
+                  icon={<FaUnlink />}
+                  color="danger"
+                  onClick={() => {
+                    updateActant(sActant.id, {
+                      actant: "",
+                    });
+                  }}
+                />
+              }
+            />
+          ) : (
+            <ActantSuggester
+              onSelected={(newSelectedId: string) => {
+                updateActant(sActant.id, {
+                  actant: newSelectedId,
+                });
+              }}
+              categoryIds={classEntitiesActant}
+            />
+          )}
+        </StyledActantListItem>
+        <StyledActantListItem opacity={opacity}>
+          <Input
+            type="select"
+            value={sActant.position}
+            options={actantPositionDict}
+            onChangeFn={(newPosition: any) => {
               updateActant(sActant.id, {
-                actant: newSelectedId,
+                position: newPosition,
               });
             }}
-            categoryIds={classEntitiesActant}
+          ></Input>
+        </StyledActantListItem>
+        <StyledActantListItem opacity={opacity}>
+          <ElvlToggle
+            value={sActant.elvl}
+            onChangeFn={(newValue: string) => {
+              updateActant(sActant.id, {
+                elvl: newValue,
+              });
+            }}
           />
-        )}
-      </StyledActantListItem>
-      <StyledActantListItem opacity={opacity}>
-        <Input
-          type="select"
-          value={sActant.position}
-          options={actantPositionDict}
-          onChangeFn={(newPosition: any) => {
-            updateActant(sActant.id, {
-              position: newPosition,
-            });
-          }}
-        ></Input>
-      </StyledActantListItem>
-      <StyledActantListItem opacity={opacity}>
-        <ElvlToggle
-          value={sActant.elvl}
-          onChangeFn={(newValue: string) => {
-            updateActant(sActant.id, {
-              elvl: newValue,
-            });
-          }}
-        />
-        <CertaintyToggle
-          value={sActant.certainty}
-          onChangeFn={(newValue: string) => {
-            updateActant(sActant.id, {
-              certainty: newValue,
-            });
-          }}
-        />
-      </StyledActantListItem>
-      <StyledActantListItem opacity={opacity}>
-        <Button
-          key="d"
-          icon={<FaTrashAlt />}
-          color="danger"
-          tooltip="remove actant row"
-          onClick={() => {
-            removeActant(sActant.id);
-          }}
-        />
-      </StyledActantListItem>
-    </React.Fragment>
-  );
-};
+          <CertaintyToggle
+            value={sActant.certainty}
+            onChangeFn={(newValue: string) => {
+              updateActant(sActant.id, {
+                certainty: newValue,
+              });
+            }}
+          />
+        </StyledActantListItem>
+        <StyledActantListItem opacity={opacity}>
+          <Button
+            key="d"
+            icon={<FaTrashAlt />}
+            color="danger"
+            tooltip="remove actant row"
+            onClick={() => {
+              removeActant(sActant.id);
+            }}
+          />
+        </StyledActantListItem>
+      </>
+    );
+  };
