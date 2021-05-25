debf1d5d52f8416cdece3a3af77b777352c6a39e
Ptrhnk <petr.nahak@gmail.com>
2021-05-25 22:35:27 +0200

    Feature/improvements (#220)
    * add react spring animation lib instead of react transition group
    
    * animate modal mount
    
    * clean up logger
    
    * close context menu on click
    
    * change animation to Transition for correct unmount
    
    * indicate context menu open
    
    * improve state set
    
    * fix animations

--- packages/client/src/pages/MainPage/containers/TerritoryTreeBox/ContextMenuSubmitDelete/ContextMenuSubmitDelete.tsx
+++ packages/client/src/pages/MainPage/containers/TerritoryTreeBox/ContextMenuSubmitDelete/ContextMenuSubmitDelete.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useQueryClient } from "react-query";
 import { useHistory, useLocation } from "react-router-dom";
 import { toast } from "react-toastify";
@@ -17,6 +17,12 @@ export const ContextMenuSubmitDelete: React.FC<ContextMenuSubmitDelete> = ({
   onClose,
   territoryActant,
 }) => {
+  const [showModal, setShowModal] = useState(false);
+
+  useEffect(() => {
+    setShowModal(true);
+  }, []);
+
   const queryClient = useQueryClient();
   let history = useHistory();
   let location = useLocation();
@@ -46,8 +52,8 @@ export const ContextMenuSubmitDelete: React.FC<ContextMenuSubmitDelete> = ({
   return (
     <Submit
       title={"Delete Territory"}
-      text={`Do you really want do delete Territory with ID [${territoryActant.id}]?`}
-      show={true}
+      text={`Do you really want do delete Territory [${territoryActant.label}]?`}
+      show={showModal}
       onSubmit={() => onSubmitDelete()}
       onCancel={() => onClose()}
     />
