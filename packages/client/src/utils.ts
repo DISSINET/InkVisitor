aeda9076cff3490b0c550a61d3b306c75e7345c6
Petr Hanak <petr.nahak@gmail.com>
2021-07-11 12:10:22 +0200

    add debounce utils function, fix panel separator resizing bug

--- packages/client/src/utils.ts
+++ packages/client/src/utils.ts
@@ -40,3 +40,17 @@ export const findPositionInStatement = (
     return "reference";
   }
 };
+
+export function debounce<T extends unknown[], U>(
+  callback: (...args: T) => PromiseLike<U> | U,
+  wait: number
+) {
+  let timer: NodeJS.Timeout;
+
+  return (...args: T): Promise<U> => {
+    clearTimeout(timer);
+    return new Promise((resolve) => {
+      timer = setTimeout(() => resolve(callback(...args)), wait);
+    });
+  };
+}
