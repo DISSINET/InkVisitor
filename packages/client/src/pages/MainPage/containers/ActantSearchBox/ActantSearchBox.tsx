fe449853d635ab6c22987216e003b3e2f0406a70
Petr Hanak <petr.nahak@gmail.com>
2021-07-28 16:04:51 +0200

    search first part

--- packages/client/src/pages/MainPage/containers/ActantSearchBox/ActantSearchBox.tsx
+++ packages/client/src/pages/MainPage/containers/ActantSearchBox/ActantSearchBox.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { OptionTypeBase, ValueType } from "react-select";
 
 import { Dropdown, Input, Suggester } from "components";
@@ -8,11 +8,49 @@ import {
   StyledRowHeader,
 } from "./ActantSearchBoxStyles";
 import { ActantSuggester } from "..";
+import { useMutation } from "react-query";
+import api from "api";
+import { IRequestSearch } from "types";
 
 const classes = ["C", "P", "G", "O", "L", "V", "E", "T", "R"];
 const classesActants = ["P", "G", "O", "C", "L", "V", "E", "S", "T", "R"];
 
+const initValues: IRequestSearch = {
+  class: null,
+  actantId: "",
+  label: "",
+};
+
 export const ActantSearchBox: React.FC = () => {
+  const [data, setData] = useState<IRequestSearch>(initValues);
+
+  const handleChange = (
+    key: string,
+    value: string | ValueType<OptionTypeBase>
+  ) => {
+    setData({
+      ...data,
+      [key]: value,
+    });
+  };
+
+  useEffect(() => {
+    if (data.actantId || data.label || data.class) {
+      searchActantsMutation.mutate(data);
+    } else {
+      // Clear search results
+    }
+  }, [data]);
+
+  const searchActantsMutation = useMutation(
+    async (searchData: IRequestSearch) => api.actantsSearch(searchData),
+    {
+      onSuccess: (data) => {
+        console.log(data.data);
+      },
+    }
+  );
+
   const options = classes.map((c: string) => {
     return { label: c, value: c };
   });
@@ -21,30 +59,21 @@ export const ActantSearchBox: React.FC = () => {
       <StyledRow>
         <StyledRowHeader>class</StyledRowHeader>
         <Dropdown
-          onChange={(option: ValueType<OptionTypeBase>) => console.log(option)}
-          options={options}
-          width={80}
           placeholder={""}
+          width={80}
           isClearable
+          options={options}
+          onChange={(option: ValueType<OptionTypeBase>) => {
+            handleChange("class", option);
+          }}
         />
       </StyledRow>
       <StyledRow>
         <StyledRowHeader>label</StyledRowHeader>
         <Input
-          changeOnType
-          onChangeFn={(value: string) => console.log(value)}
           placeholder="search"
-        />
-      </StyledRow>
-      <StyledRow>
-        <StyledRowHeader>used with action</StyledRowHeader>
-        <ActantSuggester
-          categoryIds={classesActants}
-          onSelected={(newSelectedId: string) => {
-            console.log(newSelectedId);
-          }}
-          placeholder={"actant"}
-          allowCreate={false}
+          changeOnType
+          onChangeFn={(value: string) => handleChange("label", value)}
         />
       </StyledRow>
       <StyledRow>
@@ -52,7 +81,7 @@ export const ActantSearchBox: React.FC = () => {
         <ActantSuggester
           categoryIds={classesActants}
           onSelected={(newSelectedId: string) => {
-            console.log(newSelectedId);
+            handleChange("actantId", newSelectedId);
           }}
           placeholder={"actant"}
           allowCreate={false}
