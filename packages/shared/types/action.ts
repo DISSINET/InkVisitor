362f12057a8472d86572787bd5177ed0355c2d43
jancimertel <johnny.mert@gmail.com>
2021-08-05 20:20:37 +0200

    add Action model

--- packages/shared/types/action.ts
+++ packages/shared/types/action.ts
@@ -3,18 +3,20 @@ import { ILabel, IActant } from "./";
 // TODO
 export interface IAction extends IActant {
   data: {
-    valencies: {
-      s: string;
-      a1: string;
-      a2: string;
-    };
-
-    entities: {
-      s: string[];
-      a1: string[];
-      a2: string[];
-    };
-
-    properties: [];
+    valencies: ActionValency;
+    entities: ActionEntity;
+    properties: any[];
   };
+}
+
+export interface ActionValency {
+  s: string;
+  a1: string;
+  a2: string;
+}
+
+export interface ActionEntity {
+  s: string[];
+  a1: string[];
+  a2: string[];
 }
