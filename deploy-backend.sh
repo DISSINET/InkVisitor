464b244be00a668b987a0918f7808232788ee968
Ján Mertel <mertel@turing-technology.com>
2021-08-31 17:00:32 +0200

    update deploy scripts

--- deploy-backend.sh
+++ deploy-backend.sh
@@ -2,5 +2,8 @@
 echo -n "Enter your name and press [ENTER]: "
 read username
 
-rsync -a ./packages/server/package* ./packages/server/env ./packages/server/secret ./packages/server/dist  ${username}@10.16.30.211:/var/www/html/apps/inkvisitor/server 
-ssh ${username}@10.16.30.211 'cd /var/www/html/apps/inkvisitor/server && npm install && npm run restart:prod'
+echo -n "Enter host [ENTER]: "
+read host
+
+rsync -a ./packages/server/package* ./packages/server/env ./packages/server/secret ./packages/server/dist  ${username}@${host}:/var/www/html/apps/inkvisitor/server 
+ssh ${username}@${host} 'cd /var/www/html/apps/inkvisitor/server && npm install && npm run restart:prod'
