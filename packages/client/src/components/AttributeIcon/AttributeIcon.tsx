3476fddbaf3dd1d9f075ba9034bcb50906dda835
Petr Hanak <petr.nahak@gmail.com>
2021-09-01 19:09:34 +0200

    change icons for virtuality and operator

--- packages/client/src/components/AttributeIcon/AttributeIcon.tsx
+++ packages/client/src/components/AttributeIcon/AttributeIcon.tsx
@@ -1,10 +1,11 @@
-import { FaQuestionCircle, FaBook } from "react-icons/fa";
+import { FaQuestionCircle, FaBook, FaRegObjectGroup } from "react-icons/fa";
 import { TiPlus } from "react-icons/ti";
 import { MdMood } from "react-icons/md";
 import { SiRealm } from "react-icons/si";
 import { GrVirtualMachine, GrConnect } from "react-icons/gr";
 import { AiOutlineApartment } from "react-icons/ai";
 import { BiChevronLeft, BiChevronRight } from "react-icons/bi";
+import { VscDebugDisconnect } from "react-icons/vsc";
 
 import React from "react";
 
@@ -14,9 +15,11 @@ export const attributeIcons = {
   logic: <TiPlus />,
   mood: <MdMood />,
   moodvariant: <SiRealm />,
-  virtuality: <GrVirtualMachine />,
+  // virtuality: <GrVirtualMachine />,
+  virtuality: <FaRegObjectGroup />,
   partitivity: <AiOutlineApartment />,
-  operator: <GrConnect />,
+  // operator: <GrConnect  />,
+  operator: <VscDebugDisconnect />,
   bundleStart: <BiChevronLeft />,
   bundleEnd: <BiChevronRight />,
 };
