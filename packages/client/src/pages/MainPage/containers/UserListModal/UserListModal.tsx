ab322495c2664d9017e633e2242ffe217cdf6ba9
Peter Ondrejka <pondrejk@redhat.com>
2021-09-14 20:52:39 +0200

    basic user table

--- packages/client/src/pages/MainPage/containers/UserListModal/UserListModal.tsx
+++ packages/client/src/pages/MainPage/containers/UserListModal/UserListModal.tsx
@@ -1,8 +1,16 @@
-import React from "react";
+import React, { useCallback, useEffect, useMemo, useState } from "react";
 import { Column, useTable, useExpanded, Row } from "react-table";
 import { Modal, ModalHeader, ModalContent, ModalFooter, Button, ButtonGroup } from "components";
 import { useQuery } from "react-query";
 import api from "api";
+
+import {
+  StyledTable,
+  StyledTHead,
+  StyledTh,
+} from "./UserListTableStyles";
+
+import {UserListTableRow}from "./UserListTableRow/UserListTableRow"
 
 interface UserListModal {
   isOpen: boolean;
@@ -21,21 +29,93 @@ export const UserListModal: React.FC = (modVals: UserListModal) => {
    
    console.log(data)
 
+  const getRowId = useCallback((row) => {
+    return row.id;
+  }, []);
+
+  const columns: Column<{}>[] = useMemo(() => {
+    return [
+      {
+        Header: "Name",
+        id: "Name",
+        accessor: "name",
+      },
+      {
+        Header: "Email",
+        id: "email",
+        accessor: "email",
+      },
+      {
+        Header: "Role",
+        id: "role",
+        accessor: "role",
+      },
+      {
+        Header: "Territories",
+        id: "territories",
+        accessor: "territories",
+      },
+      {
+        Header: "Password",
+        id: "password",
+        accessor: "password",
+      },
+      {
+        Header: "Actions",
+        id: "actions",
+        accessor: "actions",
+      },
+     ]
+     }, [data]);
+
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow,
+    visibleColumns,
+  } = useTable({
+    columns,
+    data: data || [],
+    getRowId,
+    initialState: {
+      hiddenColumns: ["id"],
+    },
+  });
+
+
     return (
-    <Modal showModal={modVals.isOpen} onClose={() => modVals.handler()} >
+    <Modal showModal={modVals.isOpen} onClose={() => modVals.handler()} width="middle">
      <ModalHeader title={"Manage Users"} />
       <ModalContent>
-       <table>
-        <thead>
-        </thead>
-        <tbody>
+       <StyledTable {...getTableProps()}>
+       <StyledTHead>
+          {headerGroups.map((headerGroup, key) => (
+            <tr {...headerGroup.getHeaderGroupProps()} key={key}>
+              <th></th>
+              {headerGroup.headers.map((column, key) => (
+                <StyledTh {...column.getHeaderProps()} key={key}>
+                  {column.render("Header")}
+                </StyledTh>
+              ))}
+            </tr>
+          ))}
+       </StyledTHead>
+       <tbody {...getTableBodyProps()}>
+          {rows.map((row: Row, i: number) => {
+          prepareRow(row);
+            return ( 
+            <UserListTableRow
+                index={i}
+                row={row}
+                {...row.getRowProps()}
+              />
+            );
+          })}
         </tbody>
-        {data && (data.map((row, i) =>{
-           return(
-            <tr><td>{row.name}</td><td>{row.email}</td><td>{row.role}</td></tr> 
-           )
-        }))}
-       </table>
+
+       </StyledTable>
 
       </ModalContent>
       <ModalFooter>
