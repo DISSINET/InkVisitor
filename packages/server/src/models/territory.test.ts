77a66839b4fde92cff9535e98c725e2eaa57a5c0
jancimertel <johnny.mert@gmail.com>
2021-10-09 17:38:44 +0200

    fix some tests

--- packages/server/src/models/territory.test.ts
+++ packages/server/src/models/territory.test.ts
@@ -1,4 +1,4 @@
-import { ActantType } from "@shared/enums";
+import { ActantStatus, ActantType } from "@shared/enums";
 import "ts-jest";
 import Territory, { TerritoryData, TerritoryParent } from "./territory";
 import { Db } from "@service/RethinkDB";
@@ -9,16 +9,7 @@ import { ITerritory } from "@shared/types";
 describe("Territory constructor test", function () {
   describe("empty data", () => {
     const emptyData = {};
-    const emptyTerritory: Territory = Object.create(Territory.prototype);
-    emptyTerritory.id = "";
-    emptyTerritory.class = ActantType.Territory;
-    emptyTerritory.label = "";
-    emptyTerritory.data = Object.create(TerritoryData.prototype);
-    emptyTerritory.data.parent = false;
-    emptyTerritory.data.type = "";
-    emptyTerritory.data.content = "";
-    emptyTerritory.data.lang = "";
-
+    const emptyTerritory = new Territory({});
     it("should return empty territory", () => {
       const out = new Territory(emptyData);
       expect(JSON.stringify(out)).toEqual(JSON.stringify(emptyTerritory));
@@ -26,35 +17,25 @@ describe("Territory constructor test", function () {
   });
 
   describe("ok data", () => {
-    const fullData = {
+    const fullData: ITerritory = {
       id: "id",
-      class: "T",
+      detail: "detail",
+      language: ["lang"],
+      notes: [],
+      status: ActantStatus.Pending,
       label: "label",
       data: {
         parent: {
           id: "2",
           order: -1,
         },
-        type: "type",
-        content: "content",
-        lang: "lang",
       },
+      class: ActantType.Territory,
     };
-    const fullTerritory: Territory = Object.create(Territory.prototype);
-    fullTerritory.id = "id";
-    fullTerritory.class = ActantType.Territory;
-    fullTerritory.label = "label";
-    fullTerritory.data = Object.create(TerritoryData.prototype);
-    fullTerritory.data.parent = Object.create(TerritoryParent.prototype);
-    (fullTerritory.data.parent as TerritoryParent).id = "2";
-    (fullTerritory.data.parent as TerritoryParent).order = -1;
-    fullTerritory.data.type = "type";
-    fullTerritory.data.content = "content";
-    fullTerritory.data.lang = "lang";
+    const fullTerritory = new Territory({ ...fullData });
 
     it("should return full territory", () => {
-      const out = new Territory(fullData);
-      expect(JSON.stringify(out)).toEqual(JSON.stringify(fullTerritory));
+      expect(JSON.stringify(fullData)).toEqual(JSON.stringify(fullTerritory));
     });
   });
 });
