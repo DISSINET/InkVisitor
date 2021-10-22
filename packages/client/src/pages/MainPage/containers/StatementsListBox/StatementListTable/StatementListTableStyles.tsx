b184ceab1bbc032fe638d6431d71c5a928609d14
Peter Ondrejka <pondrejk@redhat.com>
2021-10-22 12:44:45 +0200

    table header fixed to top

--- packages/client/src/pages/MainPage/containers/StatementsListBox/StatementListTable/StatementListTableStyles.tsx
+++ packages/client/src/pages/MainPage/containers/StatementsListBox/StatementListTable/StatementListTableStyles.tsx
@@ -6,6 +6,7 @@ export const StyledTable = styled.table`
   border-collapse: collapse;
   border-width: ${({ theme }) => theme.borderWidth[1]};
   border-style: solid;
+  margin-top: 90px;
   border-color: ${({ theme }) => theme.color["gray"][500]};
   box-shadow: ${({ theme }) => theme.boxShadow["subtle"]};
 `;
