c8af78d756f2706efed3e08d8c889638502a25c5
jancimertel <johnny.mert@gmail.com>
2021-10-24 17:58:36 +0200

    replace closure with bind

--- packages/server/src/middlewares/request.ts
+++ packages/server/src/middlewares/request.ts
@@ -2,26 +2,27 @@ import { InternalServerError } from "@shared/types/errors";
 import { Response, Request, NextFunction } from "express";
 import User from "@models/user";
 
-// todo this can be optimized
+const getUserOrFail = function (user: User | null): User {
+  if (user) {
+    return user;
+  } else {
+    throw new InternalServerError(
+      "User is required for the action, but not set"
+    );
+  }
+};
+
 export default async function customizeRequest(
   req: Request,
   res: Response,
   next: NextFunction
 ): Promise<void> {
-  let user: User | null;
+  let user: User | null = null;
   if (req.user && req.user.user) {
     user = await User.getUser(req.db.connection, req.user.user.id);
   }
 
-  req.getUserOrFail = function (): User {
-    if (user) {
-      return user;
-    } else {
-      throw new InternalServerError(
-        "User is required for the action, but not set"
-      );
-    }
-  };
+  req.getUserOrFail = getUserOrFail.bind(undefined, user);
 
   next();
 }
