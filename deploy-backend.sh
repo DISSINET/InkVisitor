38e3312fd81e246eb19a7b82cceac4d981b7cb3f
Ján Mertel <mertel@turing-technology.com>
2021-11-08 23:48:05 +0100

    update deploy-backend to include emails

--- deploy-backend.sh
+++ deploy-backend.sh
@@ -5,5 +5,7 @@ read username
 echo -n "Enter host [ENTER]: "
 read host
 
-rsync -a ./packages/server/package* ./packages/server/env ./packages/server/secret ./packages/server/dist  ${username}@${host}:/var/www/html/apps/inkvisitor/server 
+rsync -a ./packages/server/package* ./packages/server/env ./packages/server/secret ./packages/server/dist ${username}@${host}:/var/www/html/apps/inkvisitor/server 
+rsync -a ./packages/server/src/service/emails ${username}@${host}:/var/www/html/apps/inkvisitor/server/dist/server/src/service
+
 ssh ${username}@${host} 'cd /var/www/html/apps/inkvisitor/server && npm install && npm run restart:prod'
