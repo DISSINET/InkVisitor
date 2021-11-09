a52052dbc475754b44319aa1a220c763c336954d
Ján Mertel <mertel@turing-technology.com>
2021-11-09 23:04:32 +0100

    fix sandbox server name

--- deploy-backend-sandbox.sh
+++ deploy-backend-sandbox.sh
@@ -8,4 +8,4 @@ read host
 rsync -a ./packages/server/package* ./packages/server/env ./packages/server/secret ./packages/server/dist ${username}@${host}:/var/www/html/apps/inkvisitor-sandbox/server 
 rsync -a ./packages/server/src/service/emails ${username}@${host}:/var/www/html/apps/inkvisitor-sandbox/server/dist/server/src/service
 
-ssh ${username}@${host} 'cd /var/www/html/apps/inkv isitor-sandbox/server && npm install && npm run restart:sandbox'
+ssh ${username}@${host} 'cd /var/www/html/apps/inkvisitor-sandbox/server && npm install && npm run restart:sandbox'
