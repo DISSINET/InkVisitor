af0da23c267f1bbac0a758c07e5b65b67d8dbd38
Peter Ondrejka <pondrejk@redhat.com>
2021-11-11 22:13:36 +0100

    less buttonlike breadcrumb

--- packages/client/src/pages/MainPage/containers/StatementsListBox/StatementListHeader/StatementListBreadcrumbItem/StatementListBreadcrumbItemStyles.tsx
+++ packages/client/src/pages/MainPage/containers/StatementsListBox/StatementListHeader/StatementListBreadcrumbItem/StatementListBreadcrumbItemStyles.tsx
@@ -2,6 +2,8 @@ import styled from "styled-components";
 
 export const StyledItemBox = styled.div`
   position: relative;
+  display: flex;
+  color: ${({ theme }) => theme.color["info"]};
   margin-bottom: ${({ theme }) => theme.space[2]};
-  margin-right: ${({ theme }) => theme.space[2]};
+  margin-right: ${({ theme }) => theme.space[1]};
 `;
