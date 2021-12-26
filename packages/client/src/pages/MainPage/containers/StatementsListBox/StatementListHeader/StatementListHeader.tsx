53a47c58a85f2b4e5e93b40e3e61acacb55dea19
Ptrhnk <petr.nahak@gmail.com>
2021-12-26 15:35:18 +0100

    star to list header, improve header layout (#680)

--- packages/client/src/pages/MainPage/containers/StatementsListBox/StatementListHeader/StatementListHeader.tsx
+++ packages/client/src/pages/MainPage/containers/StatementsListBox/StatementListHeader/StatementListHeader.tsx
@@ -3,18 +3,20 @@ import { UseMutationResult, useQueryClient } from "react-query";
 import { FaPlus, FaRecycle } from "react-icons/fa";
 
 import {
+  StyledButtons,
+  StyledFaStar,
   StyledHeader,
   StyledHeaderBreadcrumbRow,
-  StyledTitle,
 } from "./StatementListHeaderStyles";
 import { StyledHeaderRow } from "./StatementListHeaderStyles";
 import { StatementListBreadcrumbItem } from "./StatementListBreadcrumbItem/StatementListBreadcrumbItem";
-import { IActant, IResponseTerritory, IStatement } from "@shared/types";
+import { IResponseTerritory, IStatement } from "@shared/types";
 import { CStatement } from "constructors";
 import { Button, ButtonGroup } from "components";
 import { useAppSelector } from "redux/hooks";
 import { useSearchParams } from "hooks";
 import { UserRole, UserRoleMode } from "@shared/enums";
+import theme from "Theme/theme";
 
 interface StatementListHeader {
   data: IResponseTerritory;
@@ -24,10 +26,12 @@ interface StatementListHeader {
     IStatement,
     unknown
   >;
+  isFavorited?: boolean;
 }
 export const StatementListHeader: React.FC<StatementListHeader> = ({
   data,
   addStatementAtTheEndMutation,
+  isFavorited,
 }) => {
   const queryClient = useQueryClient();
   const { territoryId } = useSearchParams();
@@ -69,37 +73,40 @@ export const StatementListHeader: React.FC<StatementListHeader> = ({
           })}
       </StyledHeaderBreadcrumbRow>
       <StyledHeaderRow>
-        <StyledTitle>
-          {territoryId
-            ? `T: ${trimTerritoryLabel(data.label)}`
-            : "no territory selected"}
-        </StyledTitle>
+        {isFavorited && (
+          <StyledFaStar size={18} color={theme.color["warning"]} />
+        )}
+        {territoryId
+          ? `T: ${trimTerritoryLabel(data.label)}`
+          : "no territory selected"}
         {territoryId && (
-          <ButtonGroup>
-            {data.right !== UserRoleMode.Read && (
+          <StyledButtons>
+            <ButtonGroup>
+              {data.right !== UserRoleMode.Read && (
+                <Button
+                  key="add"
+                  icon={<FaPlus size={14} />}
+                  tooltip="add new statement at the end of the list"
+                  color="primary"
+                  label="new statement"
+                  onClick={() => {
+                    handleCreateStatement();
+                  }}
+                />
+              )}
               <Button
-                key="add"
-                icon={<FaPlus size={14} />}
-                tooltip="add new statement at the end of the list"
+                key="refresh"
+                icon={<FaRecycle size={14} />}
+                tooltip="refresh data"
+                inverted
                 color="primary"
-                label="new statement"
+                label="refresh"
                 onClick={() => {
-                  handleCreateStatement();
+                  queryClient.invalidateQueries(["territory"]);
                 }}
               />
-            )}
-            <Button
-              key="refresh"
-              icon={<FaRecycle size={14} />}
-              tooltip="refresh data"
-              inverted
-              color="primary"
-              label="refresh"
-              onClick={() => {
-                queryClient.invalidateQueries(["territory"]);
-              }}
-            />
-          </ButtonGroup>
+            </ButtonGroup>
+          </StyledButtons>
         )}
       </StyledHeaderRow>
     </StyledHeader>
