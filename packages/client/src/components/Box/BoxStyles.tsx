f07cad5850de5b22818a6846ed805195a5456152
Petr Hanak <petr.nahak@gmail.com>
2022-01-09 15:13:57 +0100

    fix collapsed box border, improve box styles

--- packages/client/src/components/Box/BoxStyles.tsx
+++ packages/client/src/components/Box/BoxStyles.tsx
@@ -1,27 +1,26 @@
 import { animated } from "react-spring";
 import styled from "styled-components";
-import { space2 } from "Theme/constants";
 
 interface StyledBox {
-  color: string;
   height?: number;
 }
 export const StyledBox = styled(animated.div)<StyledBox>`
   position: relative;
   display: flex;
-  width: 100%;
   flex-direction: column;
   height: ${({ height }) => (height ? `${height / 10}rem` : "100%")};
 `;
 
 interface StyledHead {
+  $noPadding: boolean;
   color: string;
 }
 export const StyledHead = styled(animated.div)<StyledHead>`
+  height: 3.2rem;
   background-color: ${({ theme, color }) =>
     color ? color : theme.color["gray"]["200"]};
   color: ${({ theme }) => theme.color["gray"]["600"]};
-  padding: ${space2};
+  padding: ${({ theme }) => theme.space[2]};
   font-size: ${({ theme }) => theme.fontSize["base"]};
   line-height: 2rem;
   font-family: Muni;
@@ -30,7 +29,13 @@ export const StyledHead = styled(animated.div)<StyledHead>`
   text-transform: uppercase;
   border-left-color: ${({ theme }) => theme.color["gray"][200]};
   border-left-style: solid;
-  border-left-width: ${({ theme }) => theme.borderWidth[4]};
+  border-width: ${({ theme, $noPadding }) =>
+    $noPadding ? theme.borderWidth[1] : theme.borderWidth[4]};
+`;
+export const StyledButtonWrap = styled.div`
+  position: absolute;
+  top: ${({ theme }) => theme.space[2]};
+  right: ${({ theme }) => theme.space[2]};
 `;
 interface StyledContent {
   $noPadding: boolean;
@@ -46,11 +51,11 @@ export const StyledContent = styled(animated.div)<StyledContent>`
   height: 100%;
   font-size: ${({ theme }) => theme.fontSize["base"]};
 
-  border-color: ${({ theme, color, $isExpanded }) =>
+  border-color: ${({ theme, $isExpanded }) =>
     $isExpanded ? theme.color["gray"]["200"] : theme.color["grey"]};
   border-style: ${({ $isExpanded }) => ($isExpanded ? "solid" : "dotted")};
-  border-width: ${({ theme, $isExpanded }) =>
-    $isExpanded ? theme.borderWidth[4] : theme.borderWidth[1]};
+  border-width: ${({ theme, $noPadding }) =>
+    $noPadding ? theme.borderWidth[1] : theme.borderWidth[4]};
   border-top: none;
 `;
 interface StyledContentAnimationWrap {
@@ -61,25 +66,21 @@ export const StyledContentAnimationWrap = styled(
 )<StyledContentAnimationWrap>`
   display: ${({ $hideContent }) => ($hideContent ? "none" : "inherit")};
   flex-direction: column;
+  height: 100%;
 `;
 interface StyledVerticalText {
   $showContentLabel: boolean;
 }
 export const StyledVerticalText = styled(animated.p)<StyledVerticalText>`
   position: absolute;
-  top: ${({ theme }) => theme.space[12]};
+  top: ${({ theme }) => theme.space[14]};
+  left: 0.4rem;
   display: ${({ $showContentLabel }) =>
     $showContentLabel ? "initial" : "none"};
-  left: ${({ theme }) => theme.space[1]};
   writing-mode: vertical-rl;
   text-orientation: mixed;
   font-family: Muni;
   font-weight: ${({ theme }) => theme.fontWeight["bold"]};
   font-size: ${({ theme }) => theme.fontSize["base"]};
   color: ${({ theme }) => theme.color["gray"]["600"]};
-`;
-export const StyledButtonWrap = styled.div`
-  position: absolute;
-  top: ${({ theme }) => theme.space[2]};
-  right: ${({ theme }) => theme.space[2]};
 `;
