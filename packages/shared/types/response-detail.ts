a3f1106adf4ad3dde1af4a0e3b8672193992d975
adammertel <mertel.adam@gmail.com>
2022-01-30 22:10:17 +0100

    Extend response detail interface

--- packages/shared/types/response-detail.ts
+++ packages/shared/types/response-detail.ts
@@ -2,10 +2,17 @@
  * Very extensive object showing all the details about one actant
  */
 
-import { IActant, IResponseActant, IStatement } from ".";
+import { IActant, IResponseActant, IResponseStatement, IStatement } from ".";
 
 export interface IResponseDetail extends IResponseActant {
   entities: { [key: string]: IActant }; // all entities (IActant) from props.type/value and from all statements from usedInStatement, and usedInStatementProps
-  usedInStatement: string[]; // find all statements, where the actant is used as action, actant, or tag
-  usedInStatementProps: string[]; // find all statements, where the actant is used anywhere in any statement prop
+  usedInStatementAction: { statement: IResponseStatement; position: "action" }; // find all statements, where the actant is used as action, actant, or tag
+  usedInStatementActant: {
+    statement: IResponseStatement;
+    position: "subject" | "actant1" | "actant2" | "pseudoactant";
+  }[]; // find all statements, where the actant is used as action, actant, or tag
+  usedInStatementProps: {
+    statement: IResponseStatement;
+    position: "value" | "type";
+  }[]; // find all statements, where the actant is used anywhere in any statement prop
 }
