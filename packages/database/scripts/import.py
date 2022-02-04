30e1699ad6654eda2bb89ffccd536155ad7d615d
tomas.hampejs <caerulus@gmail.com>
2022-02-04 18:02:36 +0100

    Update import.py
    working version

--- packages/database/scripts/import.py
+++ packages/database/scripts/import.py
@@ -1,90 +1,86 @@
-# rough immitaion of import.ts
+# rough immitation of import.ts
 # purpose: to test import to rethinkDB because of import.ts problems on windows platforms
-
-
-# OBSERVATIONS
-# 1) connection to db needs to be probably per request, it is closes automatically after each request
-# 2) no... fully true,  dropping and creating db was done ...
-#
-#
-#
 ##########################################################################################
 
-from typing import List, Tuple, Dict
-
-import sys
+import sys, bcrypt, json
 from rethinkdb import RethinkDB
 from dotenv import dotenv_values
-#import twisted / tornado [possible to immitate await syntax]
+
+# import twisted / tornado [possible to immitate await syntax]
 
 r = RethinkDB()
 
-datasets = { "all" : [
-    {
-      "name": "acl_permissions",
-      "data": "datasets/all/acl_permissions.json",
-    },
-    {
-      "name": "actants",
-      "data": "datasets/all/actants.json",
-      "indexes": [
-        r.table("actants").index_create("class"),
-        r.table("actants").index_create("label"),
-        r
-          .table("actants")
-          .index_create(
-            "data.actants.actant",
-            r.row["data"]["actants"]["actant"]
-          ),
-        r
-          .table("actants")
-          .index_create(
-            "data.actions.action",
-            r.row["data"]["actions"]["action"]
-          ),
-        r.table("actants").index_create("data.tags", r.row["data"]["tags"]),
-        r
-          .table("actants")
-          .index_create(
-            "data.props.type.id",
-            r.row["data"]["props"]["type"]["id"]
-          ),
-        r
-          .table("actants")
-          .index_create(
-            "data.props.value.id",
-            r.row["data"]["props"]["value"]["id"]
-          ),
-        r
-          .table("actants")
-          .index_create(
-            "data.references.resource",
-            r.row["data"]["references"]["resource"]
-          ),
-        r
-          .table("actants")
-          .index_create("data.props.origin", r.row["data"]["props"]["origin"]),
-        r
-          .table("actants")
-          .index_create("data.territory.id", r.row["data"]["territory"]["id"]),
-        r
-          .table("actants")
-          .index_create("data.parent.id", r.row["data"]["parent"]["id"]),
-      ],
-    },
-    {
-      "name": "users",
-      "data": "datasets/all/users.json",
-    },
-    {
-      "name": "audits",
-      "data": "datasets/all/audits.json",
-    },
-  ]
+datasets = {"all": [
+  {
+    "name": "acl_permissions",
+    "datafile": "datasets/all/acl_permissions.json",
+  },
+  {
+    "name": "actants",
+    "datafile": "datasets/all/actants.json",
+    "indexes": [
+      r.table("actants").index_create("class"),
+      r.table("actants").index_create("label"),
+      r
+        .table("actants")
+        .index_create(
+        "data.actants.actant",
+        r.row["data"]["actants"]["actant"]
+      ),
+      r
+        .table("actants")
+        .index_create(
+        "data.actions.action",
+        r.row["data"]["actions"]["action"]
+      ),
+      r.table("actants").index_create("data.tags", r.row["data"]["tags"]),
+      r
+        .table("actants")
+        .index_create(
+        "data.props.type.id",
+        r.row["data"]["props"]["type"]["id"]
+      ),
+      r
+        .table("actants")
+        .index_create(
+        "data.props.value.id",
+        r.row["data"]["props"]["value"]["id"]
+      ),
+      r
+        .table("actants")
+        .index_create(
+        "data.references.resource",
+        r.row["data"]["references"]["resource"]
+      ),
+      r
+        .table("actants")
+        .index_create("data.props.origin", r.row["data"]["props"]["origin"]),
+      r
+        .table("actants")
+        .index_create("data.territory.id", r.row["data"]["territory"]["id"]),
+      r
+        .table("actants")
+        .index_create("data.parent.id", r.row["data"]["parent"]["id"]),
+    ],
+  },
+  {
+    "name": "users",
+    "datafile": "datasets/all/users.json",
+  },
+  {
+    "name": "audits",
+    "datafile": "datasets/all/audits.json",
+  },
+]
 }
 
-datasetId:str = sys.argv[1]
-dbMode:str = sys.argv[2]
+
+def hash_password(raw_password: str) -> str:
+  return bcrypt.hashpw(str.encode(raw_password), bcrypt.gensalt(10)).decode()
+
+
+datasetId: str = sys.argv[1]
+dbMode: str = sys.argv[2]
 envData = dotenv_values(f"../env/.env.{dbMode}")
 tablesToImport = datasets[datasetId]
 
@@ -92,48 +88,65 @@ print(f"***importing dataset {datasetId}***")
 print("")
 
 config = {
-    "db": envData['DB_NAME'],
-    "host": envData['DB_HOST'],
-    "port": envData['DB_PORT'],
-    "password": envData['DB_AUTH']
-  }
+  "db": envData['DB_NAME'],
+  "host": envData['DB_HOST'],
+  "port": envData['DB_PORT'],
+  "password": envData['DB_AUTH'] or ""
+}
 
-
-def conn():
-    conn = r.connect(config["host"], config["port"])
-    # set default database
-    conn.use(config["db"])
-    return conn
+conn = None
 
 try:
-    # Drop the database.
-    try:
-        r.db_drop(config["db"]).run(conn())
-        print("database dropped")
-    except Exception as e:
-        print("database not dropped", e)
 
-    # Recreate the database
-    try:
-        r.db_create(config["db"]).run(conn())
-        print("database created")
-    except Exception as e:
-        print("database not created, reason:", e)
+  conn = r.connect(config["host"], config["port"])
+  # set default database
+  conn.use(config["db"])
 
-    for table in tablesToImport:
-      # recreate the table
-      try:
-        print(f"table {table['name']} will be created")
-        r.table_create(table["name"]).run(conn())
-        print(f"table{table['name']} created")
-      except Exception as e:
-        print(f"table {table['name']} was not created, reason: {e}")
+  # Drop the database.
+  try:
+    r.db_drop(config["db"]).run(conn)
+    print("database dropped")
+  except Exception as e:
+    print("database not dropped", e)
+
+  # Recreate the database
+  try:
+    r.db_create(config["db"]).run(conn)
+    print("database created")
+  except Exception as e:
+    print("database not created, reason:", e)
+
+  for table in tablesToImport:
+    # recreate the table
+    try:
+      print(f"table {table['name']} will be created")
+      r.table_create(table["name"]).run(conn)
+      print(f"table {table['name']} created")
+    except Exception as e:
+      print(f"table {table['name']} was not created, reason: {e}")
+
+    try:
+      fpath = "../" + table["datafile"]
+      print("reading ", fpath)
+      data = json.load(open(fpath, ))
+
+      if table["name"] == "users":
+        for user in data:
+          user["password"] = hash_password(user["password"]) if user["password"] else hash_password("")
+
+      if table["name"] == "audits":
+        pass
+
+      r.table(table["name"]).insert(data).run(conn)
+      print(f"data into the table {table['name']} inserted")
+
+    except Exception as e:
+      print(f"Inserting unsuccesfull, reason:{repr(e)}, type:{type(e)}")
 
 
 except Exception as e:
-    print(e)
+  print(e)
 finally:
-    pass
-    # print("closing connection")
-    # if (conn):
-    #   conn.close()
+  print("closing connection")
+  if conn:
+    conn.close()
