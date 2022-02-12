b016ac4ca61c27929f83d2aaa7f742240110f898
jancimertel <10438157+jancimertel@users.noreply.github.com>
2022-02-13 00:02:30 +0100

    change implementation in ResponseDetail according to interface changes

--- packages/shared/types/response-detail.ts
+++ packages/shared/types/response-detail.ts
@@ -6,13 +6,19 @@ import { IActant, IResponseActant, IStatement } from ".";
 import { Position, PositionContext } from "../enums";
 
 export interface IResponseDetail extends IResponseActant {
-  entities: { [key: string]: IActant }; // all entities (IActant) from props.type/value and from all statements from usedInStatement, and usedInStatementProps
-  usedInStatementEntities: IResponseUsedStatement<Position | "action">[]; // find all statements, where the actant is used as action, actant, or tag
-  usedInStatementProps: IResponseUsedStatement<PositionContext>[]; // find all statements, where the actant is used anywhere in any statement propp
-  usedInMetaProps: string[];
+  entities: { [key: string]: IActant }; // all entities from IStatement and entityIds...
+  usedInStatement: IResponseUsedInStatement<Position | "action">[]; // all statements, where the detail id is used as an actant, action, or tag
+  usedInStatementProps: IResponseUsedInStatement<PositionContext>[]; // all statements, where the detail id is used in props
+  usedInMetaProps: IResponseUsedInMetaProp<PositionContext>[]; // all entities, where the detail id is used in props
 }
 
-export interface IResponseUsedStatement<PositionEnum> {
+export interface IResponseUsedInStatement<PositionEnum> {
   statement: IStatement;
   position: PositionEnum;
+  originId?: string; // what entity is the detail id used for
+}
+
+export interface IResponseUsedInMetaProp<PositionEnum> {
+  entityId: string; // IActant(later IEntity) will be placed in IResponseDetail.entities
+  position: PositionEnum; // type | value
 }
