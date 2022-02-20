6b1abae93b1f1cd3fb0b0a4b7ccb96b8768d30dd
jancimertel <10438157+jancimertel@users.noreply.github.com>
2022-02-20 22:24:20 +0100

    add Statement.findUsedInDataProps with test rename Statement.findUsed -> findUsedInDataEntities fix null condition in statement.test file

--- packages/server/src/models/statement/statement.ts
+++ packages/server/src/models/statement/statement.ts
@@ -527,7 +527,7 @@ class Statement extends Entity implements IStatement {
    * @param entityId id of the entity
    * @returns list of statements data
    */
-  static async findUsed(
+  static async findUsedInDataEntities(
     db: Connection | undefined,
     entityId: string
   ): Promise<IStatement[]> {
@@ -545,6 +545,86 @@ class Statement extends Entity implements IStatement {
             entry("actant").eq(entityId)
           ),
           row("data")("tags").contains(entityId)
+        );
+      })
+      .run(db);
+
+    return statements.sort((a, b) => {
+      return a.data.territory.order - b.data.territory.order;
+    });
+  }
+
+  /**
+   * finds statements which are linked to different entity
+   * using statement.data.actions[].props or statement.data.actants[].props
+   * searches also in props.children to lvl3
+   * @param db db connection
+   * @param entityId id of the entity
+   * @returns list of statements data
+   */
+  static async findUsedInDataProps(
+    db: Connection | undefined,
+    entityId: string
+  ): Promise<IStatement[]> {
+    const statements = await rethink
+      .table(Entity.table)
+      .filter({
+        class: EntityClass.Statement,
+      })
+      .filter((row: RDatum) => {
+        return rethink.or(
+          row("data")("actions").contains((action: RDatum) =>
+            action("props").contains((entry: RDatum) =>
+              rethink.or(
+                entry("value")("id").eq(entityId),
+                entry("type")("id").eq(entityId),
+                entry("children").contains((ch1: RDatum) =>
+                  rethink.or(
+                    ch1("value")("id").eq(entityId),
+                    ch1("type")("id").eq(entityId),
+                    ch1("children").contains((ch2: RDatum) =>
+                      rethink.or(
+                        ch2("value")("id").eq(entityId),
+                        ch2("type")("id").eq(entityId),
+                        ch2("children").contains((ch3: RDatum) =>
+                          rethink.or(
+                            ch3("value")("id").eq(entityId),
+                            ch3("type")("id").eq(entityId)
+                          )
+                        )
+                      )
+                    )
+                  )
+                )
+              )
+            )
+          ),
+          row("data")("actants").contains((actant: RDatum) =>
+            actant("props").contains((prop: RDatum) =>
+              rethink.or(
+                prop("value")("id").eq(entityId),
+                prop("type")("id").eq(entityId),
+                prop("children").contains((ch1: RDatum) =>
+                  rethink.or(
+                    ch1("value")("id").eq(entityId),
+                    ch1("type")("id").eq(entityId),
+                    ch1("children").contains((ch2: RDatum) =>
+                      rethink.or(
+                        ch2("value")("id").eq(entityId),
+                        ch2("type")("id").eq(entityId),
+                        ch2("children").contains((ch3: RDatum) =>
+                          rethink.or(
+                            ch3("value")("id").eq(entityId),
+                            ch3("type")("id").eq(entityId)
+                          )
+                        )
+                      )
+                    )
+                  )
+                )
+              )
+            )
+          )
         );
       })
       .run(db);
