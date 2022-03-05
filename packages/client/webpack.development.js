76c12a2e8b2ac2e4b7a31a0ac42af74932e3049e
jancimertel <10438157+jancimertel@users.noreply.github.com>
2022-03-05 18:01:53 +0100

    unify env names (use development,production,staging)

--- packages/client/webpack.development.js
+++ packages/client/webpack.development.js
@@ -13,7 +13,7 @@ module.exports = merge(common, {
   },
   plugins: [
     new Dotenv({
-      path: "./env/.env.devel",
+      path: "./env/.env.development",
       systemvars: true,
     }),
   ],
