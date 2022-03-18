e58366e25cf32d4a9f15e0241e30d9630445d065
Adam Mertel <mertel.adam@gmail.com>
2022-03-18 20:39:17 +0100

    Add Italian, German and French languages to the dictionary (#828)

--- packages/shared/dictionaries/language.ts
+++ packages/shared/dictionaries/language.ts
@@ -3,8 +3,12 @@ import { Language } from "../enums";
 export const languageDict = [
   { label: "-", value: Language.Empty },
   { label: "Latin", value: Language.Latin },
+  { label: "Latin", value: Language.Latin },
   { label: "English", value: Language.English },
   { label: "Middle English", value: Language.MiddleEnglish },
   { label: "Occitan", value: Language.Occitan },
   { label: "Czech", value: Language.Czech },
+  { label: "Italian", value: Language.Italian },
+  { label: "French", value: Language.French },
+  { label: "German", value: Language.German },
 ];
