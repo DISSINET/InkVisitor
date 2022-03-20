955c54e50d665dff45a07f9fe2c0a3064afcbde5
jancimertel <10438157+jancimertel@users.noreply.github.com>
2022-03-20 11:30:32 +0100

    add more specific nonenumerable decorator used in ResponseEntity class

--- packages/server/src/common/decorators.ts
+++ packages/server/src/common/decorators.ts
@@ -1,10 +1,19 @@
-export function enumerable(value: boolean) {
-  return function (target: any, propertyKey: string) {
-    let descriptor = Object.getOwnPropertyDescriptor(target, propertyKey) || {};
-    if (descriptor.enumerable != value) {
-      descriptor.enumerable = value;
-      descriptor.writable = true;
-      Object.defineProperty(target, propertyKey, descriptor);
-    }
-  };
-}
+export const nonenumerable: {
+  (target: any, name: string): void;
+  (target: any, name: string, desc: PropertyDescriptor): PropertyDescriptor;
+} = (target: any, name: string, desc?: any) => {
+  if (desc) {
+    desc.enumerable = false;
+    return desc;
+  }
+  Object.defineProperty(target, name, {
+    set(value) {
+      Object.defineProperty(this, name, {
+        value,
+        writable: true,
+        configurable: true,
+      });
+    },
+    configurable: true,
+  });
+};
