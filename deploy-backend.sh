b5a3de57489fa8273fabe25ecfcf2cf2d96bc20c
Peter Ondrejka <pondrejk@redhat.com>
2022-03-28 23:15:01 +0200

    merge to master workflow added

--- deploy-backend.sh
+++ deploy-backend.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-echo -n "Enter your name and press [ENTER]: "
-read username
 
-echo -n "Enter host [ENTER]: "
-read host
+username=$1
+host=$2
 
 rsync -a ./packages/server/package* ./packages/server/env ./packages/server/secret ./packages/server/dist ${username}@${host}:/var/www/html/apps/inkvisitor/server 
 rsync -a ./packages/server/src/service/emails ${username}@${host}:/var/www/html/apps/inkvisitor/server/dist/server/src/service
