a4fa955ff4721163f85f35e90bcc912ab1352e68
Petr Hanak <petr.nahak@gmail.com>
2022-04-12 17:49:43 +0100

    location with keys useEffect

--- packages/client/src/hooks/useParamsContext.tsx
+++ packages/client/src/hooks/useParamsContext.tsx
@@ -1,3 +1,4 @@
+import { Listener } from "@storybook/addons";
 import React, {
   createContext,
   ReactElement,
@@ -5,7 +6,7 @@ import React, {
   useEffect,
   useState,
 } from "react";
-import { useHistory, useLocation } from "react-router-dom";
+import { RouteComponentProps, useHistory, useLocation } from "react-router-dom";
 
 const UNINITIALISED = (): void => {
   throw `function uninitialised`;
@@ -62,6 +63,29 @@ export const SearchParamsProvider = ({
       hash: `${params}`,
     });
   }, [territoryId, statementId, detailId]);
+
+  const [locationKeys, setLocationKeys] = useState<any>([]);
+
+  useEffect(() => {
+    console.log(locationKeys);
+    return history.listen((location: any) => {
+      if (history.action === "PUSH") {
+        setLocationKeys([location.key]);
+      }
+
+      if (history.action === "POP") {
+        if (locationKeys[1] === location.key) {
+          setLocationKeys(([_, ...keys]) => keys);
+
+          // Handle forward event
+        } else {
+          setLocationKeys((keys) => [location.key, ...keys]);
+
+          // Handle back event
+        }
+      }
+    });
+  }, [locationKeys]);
 
   return (
     <SearchParamsContext.Provider
