43db021e74d41b88d3936d159746beb3f6e422ef
Ptrhnk <petr.nahak@gmail.com>
2022-05-08 18:02:37 +0100

    Implement used in tables to detail box (#1030)
    * Add property entities to the list of response entities
    
    * Refactor usedInMetaProps in response model
    
    * Add entities in statement to the response detail call
    
    * universal table component
    
    * detail statements table
    
    * complete query data handling for statements table
    
    * basic table style
    
    * improve table styles
    
    * style table paging
    
    * improve pagination styles
    
    * general table component improvements, title and header conditional rendering
    
    * solve table td width handling
    
    * unify detail table wrapper component
    
    * use EntityDetailTable component
    
    * back to statements table wrapper component
    
    * table style improvements
    
    * create components for meta props and statement props tables
    
    * improvements
    
    * entity tags to statement props
    
    * meta props table entity tags
    
    * disable table heading prop
    
    * fullWidthColumn prop to table
    
    * fill subj., actions, object data to used in statements table
    
    * refactor folders / imports
    
    * clean up after box table
    
    * put back position enum to metaProps response detail
    
    * fullWidth tags to tables
    
    * fix short tags tooltip in tables
    
    * add statement tag and edit button to in statement table
    
    * first last minWidth prop to table component
    
    Co-authored-by: adammertel <mertel.adam@gmail.com>

--- packages/shared/types/response-detail.ts
+++ packages/shared/types/response-detail.ts
@@ -2,13 +2,13 @@
  * Very extensive object showing all the details about one actant
  */
 
-import { IEntity, IProp, IResponseEntity, IStatement } from ".";
+import { IEntity, IResponseEntity, IStatement } from ".";
 import { UsedInPosition } from "../enums";
 
 export interface IResponseDetail extends IResponseEntity {
   entities: { [key: string]: IEntity }; // all entities from IStatement and entityIds...
   usedInStatement: IResponseUsedInStatement<UsedInPosition>[]; // all statements, where the detail id is used as an actant, action, or tag
-  usedInStatementProps: IResponseUsedInStatement<UsedInPosition>[]; // all statements, where the detail id is used in props
+  usedInStatementProps: IResponseUsedInStatementProps[]; // all statements, where the detail id is used in props
   usedInMetaProps: IResponseUsedInMetaProp<UsedInPosition>[]; // all entities, where the detail id is used in props (entity.props[])
   usedAsTemplate?: string[];
 }
@@ -16,11 +16,17 @@ export interface IResponseDetail extends IResponseEntity {
 export interface IResponseUsedInStatement<PositionEnum> {
   statement: IStatement;
   position: PositionEnum;
-  originId?: string; // what entity is the detail id used for
 }
 
+export interface IResponseUsedInStatementProps {
+  statementId: string;
+  typeId: string;
+  valueId: string;
+  originId: string; // what entity is the detail id used for
+}
+// TODO: clear position
 export interface IResponseUsedInMetaProp<PositionEnum> {
-  entityId: string; // IEntity will be placed in IResponseDetail.entities to avoid making duplications
-  position: PositionEnum; // type | value
-  prop: IProp;
+  typeId: string;
+  valueId: string;
+  originId: string; // what entity is the detail id used for
 }
