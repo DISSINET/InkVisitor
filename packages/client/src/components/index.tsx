6158960d827baa4fd0255a6a1321e24b97a3a613
Petr Hanak <petr.nahak@gmail.com>
2022-07-18 17:52:48 +0100

    temp rename components folder

--- packages/client/src/components/index.tsx
+++ packages/client/src/components/index.tsx
@@ -1,11 +1,11 @@
-import { Box } from "./basic/Box/Box";
-import { Button } from "./basic/Button/Button";
-import { ButtonGroup } from "./basic/ButtonGroup/ButtonGroup";
-import { Dropdown } from "./basic/Dropdown/Dropdown";
-import { Footer, MemoizedFooter } from "./basic/Footer/Footer";
-import { Header } from "./basic/Header/Header";
-import { Input } from "./basic/Input/Input";
-import { Loader } from "./basic/Loader/Loader";
+import { Box } from "./Classic/Box/Box";
+import { Button } from "./Classic/Button/Button";
+import { ButtonGroup } from "./Classic/ButtonGroup/ButtonGroup";
+import { Dropdown } from "./Classic/Dropdown/Dropdown";
+import { Footer, MemoizedFooter } from "./Classic/Footer/Footer";
+import { Header } from "./Classic/Header/Header";
+import { Input } from "./Classic/Input/Input";
+import { Loader } from "./Classic/Loader/Loader";
 import {
   Modal,
   ModalCard,
@@ -15,20 +15,20 @@ import {
   ModalInputForm,
   ModalInputLabel,
   ModalInputWrap,
-} from "./basic/Modal/Modal";
-import { MultiInput } from "./basic/MultiInput/MultiInput";
-import { Panel } from "./basic/Panel/Panel";
-import { Submit } from "./basic/Submit/Submit";
-import { Suggester } from "./basic/Suggester/Suggester";
-import { Tag } from "./basic/Tag/Tag";
-import { TagGroup } from "./basic/TagGroup/TagGroup";
-import { Toast } from "./basic/Toast/Toast";
-import { Tooltip } from "./basic/Tooltip/Tooltip";
-import { AttributeIcon } from "./basic/AttributeIcon/AttributeIcon";
-import { Checkbox } from "./basic/Checkbox/Checkbox";
-import { PanelSeparator } from "./basic/PanelSeparator/PanelSeparator";
-import { Table } from "./basic/Table/Table";
-import { TypeBar } from "./basic/TypeBar/TypeBar";
+} from "./Classic/Modal/Modal";
+import { MultiInput } from "./Classic/MultiInput/MultiInput";
+import { Panel } from "./Classic/Panel/Panel";
+import { Submit } from "./Classic/Submit/Submit";
+import { Suggester } from "./Classic/Suggester/Suggester";
+import { Tag } from "./Classic/Tag/Tag";
+import { TagGroup } from "./Classic/TagGroup/TagGroup";
+import { Toast } from "./Classic/Toast/Toast";
+import { Tooltip } from "./Classic/Tooltip/Tooltip";
+import { AttributeIcon } from "./Classic/AttributeIcon/AttributeIcon";
+import { Checkbox } from "./Classic/Checkbox/Checkbox";
+import { PanelSeparator } from "./Classic/PanelSeparator/PanelSeparator";
+import { Table } from "./Classic/Table/Table";
+import { TypeBar } from "./Classic/TypeBar/TypeBar";
 
 export {
   AttributeIcon,
