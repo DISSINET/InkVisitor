b8c7dfe0acc49ba4dd66637fc1e595ea9e4f2260
jancimertel <10438157+jancimertel@users.noreply.github.com>
2022-07-26 10:10:33 +0200

    test CI

--- packages/server/src/settings.ts
+++ packages/server/src/settings.ts
@@ -1,14 +1,19 @@
 import dotenv from "dotenv";
 
-console.log(
-  `[Settings] loading .env file: .env.${process.env.NODE_ENV || "development"}`
-);
+console.log("NODE ENV", process.env.NODE_ENV);
+if (!process.env.NODE_ENV) {
+  console.log(
+    `[Settings] loading .env file: .env.${
+      process.env.NODE_ENV || "development"
+    }`
+  );
 
-// Set the env file
-const result = dotenv.config({
-  path: `./env/.env.${process.env.NODE_ENV || "development"}`,
-});
+  // Set the env file
+  const result = dotenv.config({
+    path: `./env/.env.${process.env.NODE_ENV || "development"}`,
+  });
 
-if (result.error) {
-  throw result.error;
+  if (result.error) {
+    throw result.error;
+  }
 }
