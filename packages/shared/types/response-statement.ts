0980b290e64193b3003d0472ee4655b47c92c80e
adammertel <mertel.adam@gmail.com>
2022-08-06 17:13:45 +0200

    Update response models

--- packages/shared/types/response-statement.ts
+++ packages/shared/types/response-statement.ts
@@ -6,7 +6,7 @@ import { IEntity, IStatement } from ".";
 import { UserRoleMode } from "../enums";
 
 export interface IResponseStatement extends IStatement {
-  entities: { [key: string]: IEntity }; // all entities (IEntity) used in actions/actants, actions/actants.props.type/value, territory, references and tags
+  entities: { [key: string]: IEntity }; // all entities (IEntity) used in actions/actants, actions/actants.props.type/value, territory, references, tags, actant identifications and classifications
   // usedIn?: IStatement[];
   right?: UserRoleMode;
 }
