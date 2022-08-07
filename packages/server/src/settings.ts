0af1905f001d78388499df057ad6f41a10e59105
jancimertel <10438157+jancimertel@users.noreply.github.com>
2022-08-07 18:24:29 +0200

    fix env-file loading for server deployed in docker/podman

--- packages/server/src/settings.ts
+++ packages/server/src/settings.ts
@@ -1,7 +1,7 @@
 import dotenv from "dotenv";
 
 // will load .env.development by default
-if (!process.env.NODE_ENV || process.env.ENV_FILE != "") {
+if (!process.env.NODE_ENV || !!process.env.ENV_FILE) {
   console.log(
     `[Settings] loading .env file: .env.${
       process.env.ENV_FILE || "development"
