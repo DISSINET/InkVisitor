506d6faf4b2ba703427f5e59938322234b4d121a
jancimertel <10438157+jancimertel@users.noreply.github.com>
2022-08-07 08:40:37 +0200

    remove old deploy scripts

--- deploy-backend-staging.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-username=$1
-host=$2
-
-rsync -a ./packages/server/package* ./packages/server/env ./packages/server/secret ./packages/server/dist ${username}@${host}:/var/www/html/apps/inkvisitor-staging/server 
-rsync -a ./packages/server/src/service/emails ${username}@${host}:/var/www/html/apps/inkvisitor-staging/server/dist/server/src/service
-
-ssh ${username}@${host} 'cd /var/www/html/apps/inkvisitor-staging/server && npm install && npm run restart:staging'
--- deploy-frontend-staging.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# positional parameters
-username=$1
-host=$2
-
-# use rsync
-scp -r packages/client/dist/* ${username}@${host}:/var/www/html/apps/inkvisitor-staging
