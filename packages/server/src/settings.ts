7d410e81770d7566b437ffb5fb80391b48936182
jancimertel <10438157+jancimertel@users.noreply.github.com>
2022-08-07 17:08:25 +0200

    add overriding env variable for reading env file

--- packages/server/src/settings.ts
+++ packages/server/src/settings.ts
@@ -1,16 +1,16 @@
 import dotenv from "dotenv";
 
-console.log("NODE ENV", process.env.NODE_ENV);
-if (!process.env.NODE_ENV) {
+// will load .env.development by default
+if (!process.env.NODE_ENV || process.env.ENV_FILE != "") {
   console.log(
     `[Settings] loading .env file: .env.${
-      process.env.NODE_ENV || "development"
+      process.env.ENV_FILE || "development"
     }`
   );
 
   // Set the env file
   const result = dotenv.config({
-    path: `./env/.env.${process.env.NODE_ENV || "development"}`,
+    path: `./env/.env.${process.env.ENV_FILE || "development"}`,
   });
 
   if (result.error) {
