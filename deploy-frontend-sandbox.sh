33147dd1b3f1b5bc56b1a050b221f588811f1e91
jancimertel <10438157+jancimertel@users.noreply.github.com>
2022-08-07 08:40:37 +0200

    remove old deploy scripts

--- deploy-frontend-sandbox.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo -n "Enter your name and press [ENTER]: "
-read username
-
-echo -n "Enter host [ENTER]: "
-read host
-
-# use rsync
-scp -r packages/client/dist/* ${username}@${host}:/var/www/html/apps/inkvisitor-sandbox
