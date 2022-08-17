f27b75b33b2e0aa465052549d07c2a441010d801
Petr Hanak <petr.nahak@gmail.com>
2022-08-17 15:45:25 +0100

    clean up in UserPage styles

--- packages/client/src/pages/Users/index.tsx
+++ packages/client/src/pages/Users/index.tsx
@@ -1,17 +1,16 @@
-import { Page } from "components/advanced";
 import React from "react";
 import { UserList } from "./containers";
-import { StyledContent } from "./UsersPageStyles";
+import { StyledBoxWrap, StyledContent } from "./UsersPageStyles";
 
 interface UsersPageProps {}
 
 const UsersPage: React.FC<UsersPageProps> = ({}) => {
   return (
-    <div style={{ width: "100%", display: "flex", justifyContent: "center" }}>
-      <StyledContent>
+    <StyledContent>
+      <StyledBoxWrap>
         <UserList />
-      </StyledContent>
-    </div>
+      </StyledBoxWrap>
+    </StyledContent>
   );
 };
 
