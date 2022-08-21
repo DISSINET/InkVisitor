6411315b9d0e717a1c85fa5dd798e970aa5ff595
jancimertel <10438157+jancimertel@users.noreply.github.com>
2022-08-21 15:32:35 +0200

    rename taginfo types -> entity tooltip types

--- packages/shared/types/response-entity-tooltip.ts
+++ packages/shared/types/response-entity-tooltip.ts
@@ -1,23 +1,19 @@
-/**
- * type of the /user endpoint response
- */
-
 import { IEntity, IResponseEntity } from ".";
 import { Certainty, Logic, UserRoleMode } from "../enums";
 
-export interface IResponseTagInfo extends IResponseEntity {
+export interface IResponseEntityTooltip extends IResponseEntity {
   // usedCount?: number;
   // usedIn?: IStatement[];
   right?: UserRoleMode;
   entities: { [key: string]: IEntity }; //  all entities mentioned in relations
 
   // C, A
-  superclassTrees: IResponseTagInfoSuperclassTree[];
+  superclassTrees: IResponseEntityTooltipSuperclassTree[];
   // C
-  synonymClouds: IResponseTagInfoSynonymCloud[];
-  troponymClouds: IResponseTagInfoTroponymCloud[];
-  superordinateLocationTrees: IResponseTagInfoSuperordinateLocationTree[];
-  identifications: IResponseTagInfoIdentifications[];
+  synonymClouds: IResponseEntityTooltipSynonymCloud[];
+  troponymClouds: IResponseEntityTooltipTroponymCloud[];
+  superordinateLocationTrees: IResponseEntityTooltipSuperordinateLocationTree[];
+  identifications: IResponseEntityTooltipIdentifications[];
 }
 
 // these are full of entity ids
@@ -29,24 +25,24 @@ export interface IResponseTagInfo extends IResponseEntity {
 // but also O:icecream -> C:icecrean -> C:cold thing -> C:everything,
 // and also O:icecream -> C:grocery store products -> C:everyhing,
 // in that case, I propose, the output will be {O:icecream: [C: icecream], C:icecream: [C:sweet, C:cold thing, C:grocery store product], C:sweet: ...}
-export type IResponseTagInfoSuperclassTree = { [key: string]: string[] };
+export type IResponseEntityTooltipSuperclassTree = { [key: string]: string[] };
 
-// This should work the same way as IResponseTagInfoSuperclassTree but only for L and relation of type SuperordinateLocation
-export type IResponseTagInfoSuperordinateLocationTree = string[];
+// This should work the same way as IResponseEntityTooltipSuperclassTree but only for L and relation of type SuperordinateLocation
+export type IResponseEntityTooltipSuperordinateLocationTree = string[];
 
 // only for C and A
 // this is much simpler - its only about IRelationSynonym relation "clouds" where entityId is within `entityIds`
 // e.g., for A:breakfasting - [A:eating, A:swallowing, A:consume, A:having dinner], [A:morning activity, A:waking up]
-export type IResponseTagInfoSynonymCloud = string[];
+export type IResponseEntityTooltipSynonymCloud = string[];
 
 // only for A
-// I am not very much familiar what is this but this should work the same as IResponseTagInfoSynonymCloud
-export type IResponseTagInfoTroponymCloud = string[];
+// I am not very much familiar what is this but this should work the same as IResponseEntityTooltipSynonymCloud
+export type IResponseEntityTooltipTroponymCloud = string[];
 
 // for all entity classes
 // this is basicaly a list of all Identification relations found for this entity,
 // returning everything, no matter what position is the entity, the entityId parameter in the model is the other entity id
-export type IResponseTagInfoIdentifications = IResponseIdentification[];
+export type IResponseEntityTooltipIdentifications = IResponseIdentification[];
 
 export type IResponseIdentification = {
   entityId: string;
