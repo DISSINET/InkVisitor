5698f785eda0b6481bf3d4b7cba2753dc2c28b21
Ján Mertel <10438157+jancimertel@users.noreply.github.com>
2022-08-30 23:36:01 +0200

    update swagger annotations for /entities endpoints
    IEntity divided to extensions

--- packages/server/src/modules/statements/index.ts
+++ packages/server/src/modules/statements/index.ts
@@ -11,35 +11,58 @@ import { IResponseStatement, IStatement } from "@shared/types";
 import Statement from "@models/statement/statement";
 import { ResponseStatement } from "@models/statement/response";
 
-export default Router().get(
-  "/:statementId",
-  asyncRouteHandler<IResponseStatement>(async (request: Request) => {
-    const statementId = request.params.statementId;
+export default Router()
+  /**
+   * @openapi
+   * /statements/{statementId}/:
+   *   get:
+   *     description: Returns detail for statement-entity object
+   *     tags:
+   *       - entities
+   *     parameters:
+   *       - in: path
+   *         name: statementId
+   *         schema:
+   *           type: string
+   *         required: true
+   *         description: ID of the statement-entity entry
+   *     responses:
+   *       200:
+   *         description: Returns a IResponseStatement object
+   *         content:
+   *           application/json:
+   *             schema:
+   *               $ref: "#/components/schemas/IResponseStatement"
+   */
+  .get(
+    "/:statementId",
+    asyncRouteHandler<IResponseStatement>(async (request: Request) => {
+      const statementId = request.params.statementId;
 
-    if (!statementId) {
-      throw new BadParams("statement id has to be set");
-    }
+      if (!statementId) {
+        throw new BadParams("statement id has to be set");
+      }
 
-    const statementData = await findEntityById<IStatement>(
-      request.db,
-      statementId
-    );
-    if (!statementData) {
-      throw new StatementDoesNotExits(
-        `statement ${statementId} was not found`,
+      const statementData = await findEntityById<IStatement>(
+        request.db,
         statementId
       );
-    }
+      if (!statementData) {
+        throw new StatementDoesNotExits(
+          `statement ${statementId} was not found`,
+          statementId
+        );
+      }
 
-    const statementModel = new Statement({ ...statementData });
+      const statementModel = new Statement({ ...statementData });
 
-    if (!statementModel.canBeViewedByUser(request.getUserOrFail())) {
-      throw new PermissionDeniedError("statement cannot be accessed");
-    }
+      if (!statementModel.canBeViewedByUser(request.getUserOrFail())) {
+        throw new PermissionDeniedError("statement cannot be accessed");
+      }
 
-    const response = new ResponseStatement(statementData);
-    await response.prepare(request);
+      const response = new ResponseStatement(statementData);
+      await response.prepare(request);
 
-    return response;
-  })
-);
+      return response;
+    })
+  );
