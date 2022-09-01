a60c15130433c78e6035741d6103b9990ecc4909
Petr Hanak <petr.nahak@gmail.com>
2022-09-01 15:28:45 +0100

    debounce width and height values

--- packages/client/src/hooks/useWindowSize.tsx
+++ packages/client/src/hooks/useWindowSize.tsx
@@ -1,7 +1,7 @@
 import { useLayoutEffect, useState } from "react";
 
 export function useWindowSize() {
-  const [size, setSize] = useState([0, 0]);
+  const [size, setSize] = useState<number[]>([0, 0]);
   useLayoutEffect(() => {
     function updateSize() {
       setSize([window.innerWidth, window.innerHeight]);
