5f41a102ea93dc5c056ea51d54bbfa2487db569e
Petr Hanak <petr.nahak@gmail.com>
2022-09-03 18:39:05 +0100

    fix search results dynamic height

--- packages/client/src/pages/MainPage/containers/EntitySearchBox/EntitySearchResults/EntitySearchResultsStyles.tsx
+++ packages/client/src/pages/MainPage/containers/EntitySearchBox/EntitySearchResults/EntitySearchResultsStyles.tsx
@@ -1,6 +1,10 @@
+import { animated } from "react-spring";
 import styled from "styled-components";
 
 export const StyledRow = styled.div`
   display: grid;
   height: 2.5rem;
+`;
+export const StyledResultsAnimatedWrap = styled(animated.div)`
+  overflow: hidden;
 `;
