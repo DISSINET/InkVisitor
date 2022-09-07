30fbb75712e6397555fa2088f3f032bf55f4f934
Ján Mertel <10438157+jancimertel@users.noreply.github.com>
2022-09-07 08:02:40 +0200

    fix constructor in Identification model

--- packages/server/src/models/relation/identification.ts
+++ packages/server/src/models/relation/identification.ts
@@ -1,14 +1,18 @@
-import { EntityEnums } from "@shared/enums";
+import { EntityEnums, RelationEnums } from "@shared/enums";
 import { EnumValidators } from "@shared/enums";
 import Relation from "./relation";
 import { Relation as RelationTypes } from "@shared/types";
 
 export default class Identification extends Relation implements RelationTypes.IIdentification {
   certainty: EntityEnums.Certainty;
+  type: RelationEnums.Type.Identification;
+  entityIds: [string, string];
 
   constructor(data: Partial<RelationTypes.IIdentification>) {
     super(data);
     this.certainty = data.certainty as EntityEnums.Certainty;
+    this.entityIds = data.entityIds as [string, string];
+    this.type = RelationEnums.Type.Identification;
   }
 
   /**
