58759a17a25acabc99721e0307937f14cfb74b06
Petr Hanak <petr.nahak@gmail.com>
2022-09-23 15:54:31 +0100

    component improvements

--- packages/client/src/components/basic/LetterIcon/LetterIcon.tsx
+++ packages/client/src/components/basic/LetterIcon/LetterIcon.tsx
@@ -1,14 +1,20 @@
 import React from "react";
 import { Colors } from "types";
-import { StyledCircle } from "./LetterIconStyles";
+import { StyledCircle, StyledLetter } from "./LetterIconStyles";
 
 interface LetterIcon {
   letter: string;
-  color: typeof Colors[number];
+  color?: typeof Colors[number];
+  size?: number;
 }
 export const LetterIcon: React.FC<LetterIcon> = ({
   letter = "X",
-  color = "white",
+  color = "black",
+  size = 16,
 }) => {
-  return <StyledCircle color={color}>{letter}</StyledCircle>;
+  return (
+    <StyledCircle color={color} size={size}>
+      <StyledLetter size={size}>{letter}</StyledLetter>
+    </StyledCircle>
+  );
 };
--- packages/client/src/components/basic/LetterIcon/LetterIconStyles.tsx
+++ packages/client/src/components/basic/LetterIcon/LetterIconStyles.tsx
@@ -3,9 +3,23 @@ import { Colors } from "types";
 
 interface StyledCircle {
   color: typeof Colors[number];
+  size: number;
 }
 export const StyledCircle = styled.div<StyledCircle>`
   border: 1px solid;
   border-color: ${({ theme, color }) => theme.color[color]};
   border-radius: 5rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 1.6rem;
+  height: 1.6rem;
+`;
+
+interface StyledLetter {
+  size: number;
+}
+export const StyledLetter = styled.p<StyledLetter>`
+  font-size: ${({ theme }) => theme.fontSize["xs"]};
+  margin-left: 1px;
 `;
