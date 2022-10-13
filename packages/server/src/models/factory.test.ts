d0eeeb1934957bf529ccc906ad957d598452bf14
Ján Mertel <10438157+jancimertel@users.noreply.github.com>
2022-10-13 21:52:56 +0200

    add relations factory test

--- packages/server/src/models/factory.test.ts
+++ packages/server/src/models/factory.test.ts
@@ -1,9 +1,9 @@
-import { EntityEnums } from "@shared/enums";
+import { EntityEnums, RelationEnums } from "@shared/enums";
 import "ts-jest";
-import { getEntityClass } from "./factory";
+import { getEntityClass, getRelationClass } from "./factory";
 import "ts-jest";
 
-describe("Factory test", function () {
+describe("Entity factory test", function () {
   describe("known types", () => {
     it("should return implementation", () => {
       for (const aType of Object.values(EntityEnums.Class)) {
@@ -13,17 +13,44 @@ describe("Factory test", function () {
   });
   describe("unknown types", () => {
     it("should return null", () => {
-      expect(getEntityClass({ class: "random" })).toBeNull();
+      expect(() => getEntityClass({ class: "random" })).toThrowError();
     });
   });
   describe("empty type", () => {
     it("should return null", () => {
-      expect(getEntityClass({})).toBeNull();
+      expect(() => getEntityClass({})).toThrowError();
     });
   });
   describe("undefined input", () => {
     it("should return null", () => {
-      expect(getEntityClass(undefined)).toBeNull();
+      expect(() => getEntityClass(undefined)).toThrowError();
+    });
+  });
+});
+
+describe("Relationship factory test", function () {
+  describe("known types", () => {
+    it("should return implementation", () => {
+      for (const rType of Object.values(RelationEnums.Type).filter(type => !!type)) {
+        const instance = getRelationClass({ type: rType });
+        expect(instance).not.toBeNull();
+        expect(instance.type).toEqual(rType);
+      }
+    });
+  });
+  describe("unknown types", () => {
+    it("should return null", () => {
+      expect(() => getRelationClass({ class: "random" })).toThrowError();
+    });
+  });
+  describe("empty type", () => {
+    it("should return null", () => {
+      expect(() => getRelationClass({})).toThrowError();
+    });
+  });
+  describe("undefined input", () => {
+    it("should return null", () => {
+      expect(() => getRelationClass(undefined)).toThrowError();
     });
   });
 });
