87ab5c4210366c048d3a3e5e0b0c5e7369b2c24b
tomas.hampejs <caerulus@gmail.com>
2022-10-14 10:31:09 +0200

    update of json schema generations, update of parsing scripts so it process relation parsings

--- packages/database/scripts/generate-json-schemas.py
+++ packages/database/scripts/generate-json-schemas.py
@@ -34,7 +34,7 @@ files_and_classes = [
     {'file': input_path + 'types/relation.ts', 'class': 'Relation.ISuperordinateLocation'},
     {'file': input_path + 'types/relation.ts', 'class': 'Relation.IPropertyReciprocal'},
     {'file': input_path + 'types/relation.ts', 'class': 'Relation.ISuperclass'},
-    {'file': input_path + 'types/relation.ts', 'class': 'Relation.IHolonymy'},
+    {'file': input_path + 'types/relation.ts', 'class': 'Relation.IHolonym'},
   {'file': input_path + 'types/prop.ts', 'class': 'IProp'},
     {'file': input_path + 'types/value.ts', 'class': 'IValue'},
     {'file': input_path + 'types/action.ts', 'class': 'IAction'},
@@ -44,13 +44,13 @@ files_and_classes = [
 
 ]
 
-files_and_classes = [
+# files_and_classes = [
 #  {'file': input_path + 'types/relation.ts', 'class': 'Relation.IModel'},
 #  {'file': input_path + 'types/actant.ts', 'class': 'IActant'},
 #  {'file': input_path + 'types/relation.ts', 'class': 'Relation.IClassification'},
-  {'file': input_path + 'types/relation.ts', 'class': 'Relation.ISubjectActant1Reciprocal'},
-{'file': input_path + 'types/relation.ts', 'class': 'Relation.ISuperordinateLocation'},
-]
+ # {'file': input_path + 'types/relation.ts', 'class': 'Relation.ISubjectActant1Reciprocal'},
+# {'file': input_path + 'types/relation.ts', 'class': 'Relation.ISuperordinateLocation'},
+#]
 
 for case in files_and_classes:
     # command = command_base + " " + case['file'] + " " + case['class'] + " " + params + " --id " + case['class'] + " --out " + output_path+case['class'] + '.schema.json'
