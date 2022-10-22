1b3f4da7f32899e98b2807a206b28782c462e056
Ján Mertel <10438157+jancimertel@users.noreply.github.com>
2022-10-22 19:11:03 +0200

    implement logic for Actant1Semantics.areEntitiesValid + test

--- packages/server/src/models/relation/actant1-semantics.ts
+++ packages/server/src/models/relation/actant1-semantics.ts
@@ -1,6 +1,7 @@
-import { RelationEnums } from "@shared/enums";
+import { EntityEnums, RelationEnums } from "@shared/enums";
 import Relation from "./relation";
 import { Relation as RelationTypes } from "@shared/types";
+import { ModelNotValidError } from "@shared/types/errors";
 
 export default class Actant1Semantics extends Relation implements RelationTypes.IActant1Semantics {
   type: RelationEnums.Type.Actant1Semantics;
@@ -10,5 +11,25 @@ export default class Actant1Semantics extends Relation implements RelationTypes.
     super(data);
     this.entityIds = data.entityIds as [string, string];
     this.type = RelationEnums.Type.Actant1Semantics;
+  }
+
+  /**
+  * areEntitiesValid checks if entities have acceptable classes
+  * @returns 
+  */
+  areEntitiesValid(): Error | null {
+    if (this.entityIds.length !== 2) {
+      return new ModelNotValidError("# of entities should be 2");
+    }
+
+    if (this.entities?.find(e => e.id === this.entityIds[0])?.class !== EntityEnums.Class.Action) {
+      return new ModelNotValidError(`First entity should of class '${EntityEnums.Class.Action}'`);
+    }
+
+    if (this.entities?.find(e => e.id === this.entityIds[1])?.class !== EntityEnums.Class.Concept) {
+      return new ModelNotValidError(`Second entity should of class '${EntityEnums.Class.Concept}'`);
+    }
+
+    return null;
   }
 }
