f96411709f3a91647188838fbf567ba00fde4848
Ján Mertel <10438157+jancimertel@users.noreply.github.com>
2022-10-23 13:59:41 +0200

    implement logic for Identification.areEntitiesValid + test

--- packages/server/src/models/relation/identification.ts
+++ packages/server/src/models/relation/identification.ts
@@ -2,6 +2,7 @@ import { EntityEnums, RelationEnums } from "@shared/enums";
 import { EnumValidators } from "@shared/enums";
 import Relation from "./relation";
 import { Relation as RelationTypes } from "@shared/types";
+import { ModelNotValidError } from "@shared/types/errors";
 
 export default class Identification extends Relation implements RelationTypes.IIdentification {
   certainty: EntityEnums.Certainty;
@@ -13,6 +14,25 @@ export default class Identification extends Relation implements RelationTypes.II
     this.certainty = data.certainty as EntityEnums.Certainty;
     this.entityIds = data.entityIds as [string, string];
     this.type = RelationEnums.Type.Identification;
+  }
+
+  /**
+  * areEntitiesValid checks if entities have acceptable classes
+  * @returns 
+  */
+  areEntitiesValid(): Error | null {
+    if (this.entityIds.length !== 2) {
+      return new ModelNotValidError("# of entities should be 2");
+    }
+
+    for (let i = 0; i < this.entityIds.length; i++) {
+      const entityClass = this.entities?.find(e => e.id === this.entityIds[i])?.class || "" as EntityEnums.Class;
+      if (EntityEnums.PLOGESTRB.indexOf(entityClass) === -1) {
+        return new ModelNotValidError(`#${i} entity should be of class 'PLOGESTRB'`);
+      }
+    }
+
+    return null;
   }
 
   /**
