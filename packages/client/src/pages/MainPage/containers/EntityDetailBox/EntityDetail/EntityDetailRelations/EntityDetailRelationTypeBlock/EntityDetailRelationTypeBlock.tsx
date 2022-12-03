c2386b4cb11f2aee109c337e95b0333558ad7279
Ptrhnk <petr.nahak@gmail.com>
2022-12-03 13:37:20 +0000

    implement has order attr (#1409)

--- packages/client/src/pages/MainPage/containers/EntityDetailBox/EntityDetail/EntityDetailRelations/EntityDetailRelationTypeBlock/EntityDetailRelationTypeBlock.tsx
+++ packages/client/src/pages/MainPage/containers/EntityDetailBox/EntityDetail/EntityDetailRelations/EntityDetailRelationTypeBlock/EntityDetailRelationTypeBlock.tsx
@@ -55,253 +55,256 @@ interface EntityDetailRelationTypeBlock {
   >;
   isCloudType: boolean;
   isMultiple: boolean;
+  hasOrder: boolean;
   entity: IResponseDetail;
 }
-export const EntityDetailRelationTypeBlock: React.FC<EntityDetailRelationTypeBlock> =
-  ({
-    relations,
-    relationType,
-    entities,
-    relationCreateMutation,
-    relationUpdateMutation,
-    relationDeleteMutation,
-    isCloudType,
-    isMultiple,
-    entity,
-  }) => {
-    const relationRule = Relation.RelationRules[relationType];
+export const EntityDetailRelationTypeBlock: React.FC<
+  EntityDetailRelationTypeBlock
+> = ({
+  relations,
+  relationType,
+  entities,
+  relationCreateMutation,
+  relationUpdateMutation,
+  relationDeleteMutation,
+  isCloudType,
+  isMultiple,
+  hasOrder,
+  entity,
+}) => {
+  const relationRule = Relation.RelationRules[relationType];
 
-    // For suggester
-    const getCategoryTypes = (): EntityEnums.ExtendedClass[] | undefined => {
-      const entitiesPattern = relationRule.allowedEntitiesPattern;
+  // For suggester
+  const getCategoryTypes = (): EntityEnums.ExtendedClass[] | undefined => {
+    const entitiesPattern = relationRule.allowedEntitiesPattern;
 
-      if (entitiesPattern.length > 0) {
-        if (isCloudType) {
-          if (relationRule.allowedSameEntityClassesOnly) {
-            return [entity.class];
-          } else {
-            return entitiesPattern.flat(1);
-          }
-        } else if (!relationRule.asymmetrical) {
-          // Symetrical
-          const pairs = entitiesPattern.filter(
-            (array) => array[0] === entity.class
-          );
-          if (pairs.length > 0) {
-            return [...new Set(pairs.map((pair) => pair[1]))];
-          }
+    if (entitiesPattern.length > 0) {
+      if (isCloudType) {
+        if (relationRule.allowedSameEntityClassesOnly) {
+          return [entity.class];
         } else {
-          // Asymetrical
-          let collectedEntities: EntityEnums.Class[] = [];
-          const leftSide = entitiesPattern.filter(
-            (array) => array[0] === entity.class
-          );
-          if (leftSide.length > 0) {
-            const collectedLeft = leftSide.map((r) => r[1]);
-            collectedEntities.push(...collectedLeft);
-          }
-
-          return [...new Set(collectedEntities)];
+          return entitiesPattern.flat(1);
+        }
+      } else if (!relationRule.asymmetrical) {
+        // Symetrical
+        const pairs = entitiesPattern.filter(
+          (array) => array[0] === entity.class
+        );
+        if (pairs.length > 0) {
+          return [...new Set(pairs.map((pair) => pair[1]))];
         }
       } else {
-        // Multiple
-        const allEntities = entitiesDict.map(
-          (e) => e.value as EntityEnums.Class
+        // Asymetrical
+        let collectedEntities: EntityEnums.Class[] = [];
+        const leftSide = entitiesPattern.filter(
+          (array) => array[0] === entity.class
         );
-        if (relationType === RelationEnums.Type.Identification) {
-          return allEntities.filter((e) => !restrictedIDEClasses.includes(e));
-        } else {
-          return allEntities;
+        if (leftSide.length > 0) {
+          const collectedLeft = leftSide.map((r) => r[1]);
+          collectedEntities.push(...collectedLeft);
         }
-      }
-    };
 
-    const handleMultiSelected = (selectedId: string) => {
+        return [...new Set(collectedEntities)];
+      }
+    } else {
+      // Multiple
+      const allEntities = entitiesDict.map((e) => e.value as EntityEnums.Class);
       if (relationType === RelationEnums.Type.Identification) {
-        const newRelation: Relation.IIdentification = {
-          id: uuidv4(),
-          entityIds: [entity.id, selectedId],
-          type: RelationEnums.Type.Identification,
-          certainty: EntityEnums.Certainty.Certain,
-        };
-        relationCreateMutation.mutate(newRelation);
+        return allEntities.filter((e) => !restrictedIDEClasses.includes(e));
       } else {
-        const newRelation: Relation.IRelation = {
-          id: uuidv4(),
-          entityIds: [entity.id, selectedId],
-          type: relationType as RelationEnums.Type,
-        };
-        relationCreateMutation.mutate(newRelation);
+        return allEntities;
       }
-    };
-
-    const [usedEntityIds, setUsedEntityIds] = useState<string[]>([]);
-
-    useEffect(() => {
-      const entityIds = relations
-        .map((relation) => relation.entityIds.map((entityId) => entityId))
-        .flat(1)
-        .concat(entity.id);
-      setUsedEntityIds([...new Set(entityIds)]);
-    }, [entities, relations]);
-
-    const [tempCloudEntityId, setTempCloudEntityId] =
-      useState<string | false>(false);
-
-    const {} = useQuery(
-      ["relation-entity-temp", tempCloudEntityId],
-      async () => {
-        if (tempCloudEntityId) {
-          const res = await api.detailGet(tempCloudEntityId);
-          if (res.data) {
-            addToCloud(res.data);
-            setTempCloudEntityId(false);
-          }
-        }
-      },
-      {
-        enabled: api.isLoggedIn() && !!tempCloudEntityId,
-      }
-    );
-
-    const addToCloud = (cloudEntity: IResponseDetail) => {
-      const selectedEntityRelation = cloudEntity.relations.find(
-        (r) => r.type === relationType
-      );
-      if (selectedEntityRelation) {
-        // update existing relation
-        const changes = {
-          entityIds: [...selectedEntityRelation.entityIds, entity.id],
-        };
-        relationUpdateMutation.mutate({
-          relationId: selectedEntityRelation.id,
-          changes: changes,
-        });
-      } else {
-        // Create new relation (cloud init)
-        const newRelation: Relation.IRelation = {
-          id: uuidv4(),
-          entityIds: [entity.id, cloudEntity.id],
-          type: relationType as RelationEnums.Type,
-        };
-        relationCreateMutation.mutate(newRelation);
-      }
-    };
-
-    useEffect(() => {
-      setCurrentRelations(relations);
-    }, [relations]);
-
-    const [currentRelations, setCurrentRelations] = useState<
-      Relation.IRelation[]
-    >([]);
-
-    const moveRow = useCallback(
-      (dragIndex: number, hoverIndex: number) => {
-        const dragRecord = currentRelations[dragIndex];
-        setCurrentRelations(
-          update(currentRelations, {
-            $splice: [
-              [dragIndex, 1],
-              [hoverIndex, 0, dragRecord],
-            ],
-          })
-        );
-      },
-      [currentRelations]
-    );
-
-    const updateOrderFn = (relationId: string, newOrder: number) => {
-      let allOrders: number[] = relations.map((relation, key) =>
-        relation.order !== undefined ? relation.order : 0
-      );
-      let finalOrder: number = 0;
-
-      const currentRelation = relations.find(
-        (relation) => relation.id === relationId
-      );
-
-      if (newOrder === 0) {
-        finalOrder = allOrders[0] - 1;
-      } else if (newOrder === relations.length - 1) {
-        finalOrder = allOrders[newOrder - 1] + 1;
-      } else {
-        if (currentRelation?.order === allOrders[newOrder - 1]) {
-          finalOrder = allOrders[newOrder];
-        } else {
-          finalOrder = allOrders[newOrder - 1];
-        }
-      }
-      relationUpdateMutation.mutate({
-        relationId: relationId,
-        changes: { order: finalOrder },
-      });
-    };
-
-    return (
-      <>
-        {/* Type column */}
-        <StyledRelationType>
-          <LetterIcon letter={relationType} color="info" />
-          {relationRule.inverseLabel.length ? (
-            <TbArrowsHorizontal color={theme.color["info"]} />
-          ) : (
-            <TbArrowNarrowRight color={theme.color["info"]} />
-          )}
-        </StyledRelationType>
-        {/* Label & Suggester column */}
-        <StyledLabelSuggester>
-          <StyledLabel>{relationRule.label}</StyledLabel>
-          {(isMultiple || relations.length < 1) && (
-            <StyledSuggesterWrapper>
-              <EntitySuggester
-                categoryTypes={
-                  getCategoryTypes() ||
-                  ([EntityEnums.Extension.Empty] as [EntityEnums.ExtendedClass])
-                }
-                onSelected={(selectedId: string) => {
-                  if (isCloudType) {
-                    setTempCloudEntityId(selectedId);
-                  } else {
-                    handleMultiSelected(selectedId);
-                  }
-                }}
-                excludedActantIds={usedEntityIds}
-              />
-            </StyledSuggesterWrapper>
-          )}
-        </StyledLabelSuggester>
-        {/* Values column */}
-        <StyledRelationValues>
-          {currentRelations.map((relation, key) =>
-            isCloudType ? (
-              <EntityDetailCloudRelation
-                key={key}
-                relation={relation}
-                entityId={entity.id}
-                relations={relations}
-                entities={entities}
-                relationUpdateMutation={relationUpdateMutation}
-                relationDeleteMutation={relationDeleteMutation}
-              />
-            ) : (
-              <EntityDetailRelationRow
-                key={key}
-                index={key}
-                relation={relation}
-                entities={entities}
-                entityId={entity.id}
-                relationRule={relationRule}
-                relationType={relationType as RelationEnums.Type}
-                relationUpdateMutation={relationUpdateMutation}
-                relationDeleteMutation={relationDeleteMutation}
-                isMultiple={isMultiple}
-                moveRow={moveRow}
-                updateOrderFn={updateOrderFn}
-              />
-            )
-          )}
-        </StyledRelationValues>
-      </>
-    );
+    }
   };
+
+  const handleMultiSelected = (selectedId: string) => {
+    if (relationType === RelationEnums.Type.Identification) {
+      const newRelation: Relation.IIdentification = {
+        id: uuidv4(),
+        entityIds: [entity.id, selectedId],
+        type: RelationEnums.Type.Identification,
+        certainty: EntityEnums.Certainty.Certain,
+      };
+      relationCreateMutation.mutate(newRelation);
+    } else {
+      const newRelation: Relation.IRelation = {
+        id: uuidv4(),
+        entityIds: [entity.id, selectedId],
+        type: relationType as RelationEnums.Type,
+      };
+      relationCreateMutation.mutate(newRelation);
+    }
+  };
+
+  const [usedEntityIds, setUsedEntityIds] = useState<string[]>([]);
+
+  useEffect(() => {
+    const entityIds = relations
+      .map((relation) => relation.entityIds.map((entityId) => entityId))
+      .flat(1)
+      .concat(entity.id);
+    setUsedEntityIds([...new Set(entityIds)]);
+  }, [entities, relations]);
+
+  const [tempCloudEntityId, setTempCloudEntityId] = useState<string | false>(
+    false
+  );
+
+  const {} = useQuery(
+    ["relation-entity-temp", tempCloudEntityId],
+    async () => {
+      if (tempCloudEntityId) {
+        const res = await api.detailGet(tempCloudEntityId);
+        if (res.data) {
+          addToCloud(res.data);
+          setTempCloudEntityId(false);
+        }
+      }
+    },
+    {
+      enabled: api.isLoggedIn() && !!tempCloudEntityId,
+    }
+  );
+
+  const addToCloud = (cloudEntity: IResponseDetail) => {
+    const selectedEntityRelation = cloudEntity.relations.find(
+      (r) => r.type === relationType
+    );
+    if (selectedEntityRelation) {
+      // update existing relation
+      const changes = {
+        entityIds: [...selectedEntityRelation.entityIds, entity.id],
+      };
+      relationUpdateMutation.mutate({
+        relationId: selectedEntityRelation.id,
+        changes: changes,
+      });
+    } else {
+      // Create new relation (cloud init)
+      const newRelation: Relation.IRelation = {
+        id: uuidv4(),
+        entityIds: [entity.id, cloudEntity.id],
+        type: relationType as RelationEnums.Type,
+      };
+      relationCreateMutation.mutate(newRelation);
+    }
+  };
+
+  useEffect(() => {
+    setCurrentRelations(relations);
+  }, [relations]);
+
+  const [currentRelations, setCurrentRelations] = useState<
+    Relation.IRelation[]
+  >([]);
+
+  const moveRow = useCallback(
+    (dragIndex: number, hoverIndex: number) => {
+      const dragRecord = currentRelations[dragIndex];
+      setCurrentRelations(
+        update(currentRelations, {
+          $splice: [
+            [dragIndex, 1],
+            [hoverIndex, 0, dragRecord],
+          ],
+        })
+      );
+    },
+    [currentRelations]
+  );
+
+  const updateOrderFn = (relationId: string, newOrder: number) => {
+    let allOrders: number[] = relations.map((relation, key) =>
+      relation.order !== undefined ? relation.order : 0
+    );
+    let finalOrder: number = 0;
+
+    const currentRelation = relations.find(
+      (relation) => relation.id === relationId
+    );
+
+    if (newOrder === 0) {
+      finalOrder = allOrders[0] - 1;
+    } else if (newOrder === relations.length - 1) {
+      finalOrder = allOrders[newOrder - 1] + 1;
+    } else {
+      if (currentRelation?.order === allOrders[newOrder - 1]) {
+        finalOrder = allOrders[newOrder];
+      } else {
+        finalOrder = allOrders[newOrder - 1];
+      }
+    }
+    relationUpdateMutation.mutate({
+      relationId: relationId,
+      changes: { order: finalOrder },
+    });
+  };
+
+  return (
+    <>
+      {/* Type column */}
+      <StyledRelationType>
+        <LetterIcon letter={relationType} color="info" />
+        {relationRule.inverseLabel.length ? (
+          <TbArrowsHorizontal color={theme.color["info"]} />
+        ) : (
+          <TbArrowNarrowRight color={theme.color["info"]} />
+        )}
+      </StyledRelationType>
+      {/* Label & Suggester column */}
+      <StyledLabelSuggester>
+        <StyledLabel>{relationRule.label}</StyledLabel>
+        {(isMultiple || relations.length < 1) && (
+          <StyledSuggesterWrapper>
+            <EntitySuggester
+              categoryTypes={
+                getCategoryTypes() ||
+                ([EntityEnums.Extension.Empty] as [EntityEnums.ExtendedClass])
+              }
+              onSelected={(selectedId: string) => {
+                if (isCloudType) {
+                  setTempCloudEntityId(selectedId);
+                } else {
+                  handleMultiSelected(selectedId);
+                }
+              }}
+              excludedActantIds={usedEntityIds}
+            />
+          </StyledSuggesterWrapper>
+        )}
+      </StyledLabelSuggester>
+      {/* Values column */}
+      <StyledRelationValues>
+        {currentRelations.map((relation, key) =>
+          isCloudType ? (
+            <EntityDetailCloudRelation
+              key={key}
+              relation={relation}
+              entityId={entity.id}
+              relations={relations}
+              entities={entities}
+              relationUpdateMutation={relationUpdateMutation}
+              relationDeleteMutation={relationDeleteMutation}
+            />
+          ) : (
+            <EntityDetailRelationRow
+              key={key}
+              index={key}
+              relation={relation}
+              entities={entities}
+              entityId={entity.id}
+              relationRule={relationRule}
+              relationType={relationType as RelationEnums.Type}
+              relationUpdateMutation={relationUpdateMutation}
+              relationDeleteMutation={relationDeleteMutation}
+              isMultiple={isMultiple}
+              hasOrder={hasOrder}
+              moveRow={moveRow}
+              updateOrderFn={updateOrderFn}
+            />
+          )
+        )}
+      </StyledRelationValues>
+    </>
+  );
+};
