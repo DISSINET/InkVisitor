a92e6046ac00b2f3192b3855aa817e7387130bca
Petr Hanak <petr.nahak@gmail.com>
2022-12-17 23:38:02 +0000

    relation styles

--- packages/client/src/pages/MainPage/containers/EntityDetailBox/EntityDetail/EntityDetailRelations/EntityDetailRelationsStyles.tsx
+++ packages/client/src/pages/MainPage/containers/EntityDetailBox/EntityDetail/EntityDetailRelations/EntityDetailRelationsStyles.tsx
@@ -4,4 +4,5 @@ export const StyledRelationsGrid = styled.div`
   display: grid;
   grid-template-columns: auto auto 1fr;
   grid-gap: ${({ theme }) => theme.space[4]};
+  padding-right: ${({ theme }) => theme.space[8]};
 `;
