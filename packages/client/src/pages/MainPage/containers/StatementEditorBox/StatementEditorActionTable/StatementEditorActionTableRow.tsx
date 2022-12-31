50cb108fda18c68e64070aa4e562e242daee4429
adammertel <mertel.adam@gmail.com>
2022-12-31 12:54:25 +0100

    Fix actants grid for editors and viewers

--- packages/client/src/pages/MainPage/containers/StatementEditorBox/StatementEditorActionTable/StatementEditorActionTableRow.tsx
+++ packages/client/src/pages/MainPage/containers/StatementEditorBox/StatementEditorActionTable/StatementEditorActionTableRow.tsx
@@ -46,268 +46,267 @@ interface StatementEditorActionTableRow {
   territoryActants?: string[];
 }
 
-export const StatementEditorActionTableRow: React.FC<StatementEditorActionTableRow> =
-  ({
-    filteredAction,
-    index,
-    moveRow,
-    statement,
-    userCanEdit = false,
-    updateOrderFn,
-    addProp,
-    updateProp,
-    removeProp,
-    movePropToIndex,
-    updateActionsMutation,
-    territoryParentId,
-    territoryActants,
-  }) => {
-    const isInsideTemplate = statement.isTemplate || false;
-    const { statementId, territoryId } = useSearchParams();
+export const StatementEditorActionTableRow: React.FC<
+  StatementEditorActionTableRow
+> = ({
+  filteredAction,
+  index,
+  moveRow,
+  statement,
+  userCanEdit = false,
+  updateOrderFn,
+  addProp,
+  updateProp,
+  removeProp,
+  movePropToIndex,
+  updateActionsMutation,
+  territoryParentId,
+  territoryActants,
+}) => {
+  const isInsideTemplate = statement.isTemplate || false;
+  const { statementId, territoryId } = useSearchParams();
 
-    const dropRef = useRef<HTMLTableRowElement>(null);
-    const dragRef = useRef<HTMLTableCellElement>(null);
+  const dropRef = useRef<HTMLTableRowElement>(null);
+  const dragRef = useRef<HTMLTableCellElement>(null);
 
-    const updateAction = (statementActionId: string, changes: any) => {
-      if (statement && statementActionId) {
-        const updatedActions = statement.data.actions.map((a) =>
-          a.id === statementActionId ? { ...a, ...changes } : a
-        );
-        const newData = { actions: updatedActions };
-        updateActionsMutation.mutate(newData);
-      }
-    };
-    const removeAction = (statementActionId: string) => {
-      if (statement) {
-        const updatedActions = statement.data.actions.filter(
-          (a) => a.id !== statementActionId
-        );
-        const newData = { actions: updatedActions };
-        updateActionsMutation.mutate(newData);
-      }
-    };
-
-    const [, drop] = useDrop({
-      accept: ItemTypes.ACTION_ROW,
-      hover(item: DragItem, monitor: DropTargetMonitor) {
-        dndHoverFn(item, index, monitor, dropRef, moveRow);
-      },
-    });
-
-    const [{ isDragging }, drag, preview] = useDrag({
-      item: {
-        type: ItemTypes.ACTION_ROW,
-        index,
-        id: filteredAction.id.toString(),
-      },
-      collect: (monitor: DragSourceMonitor) => ({
-        isDragging: monitor.isDragging(),
-      }),
-      end: (item: DragItem | undefined, monitor: DragSourceMonitor) => {
-        if (item) updateOrderFn();
-      },
-    });
-
-    const opacity = isDragging ? 0.2 : 1;
-
-    preview(drop(dropRef));
-    drag(dragRef);
-
-    const renderActionCell = () => {
-      const { action, sAction } = filteredAction.data;
-      return action ? (
-        <EntityTag
-          fullWidth
-          entity={action}
-          button={
-            userCanEdit && (
-              <Button
-                key="d"
-                tooltipLabel="unlink action"
-                icon={<FaUnlink />}
-                inverted
-                color="plain"
-                onClick={() => {
-                  updateAction(sAction.id, {
-                    actionId: "",
-                  });
-                }}
-              />
-            )
-          }
-        />
-      ) : (
-        userCanEdit && (
-          <EntitySuggester
-            onSelected={(newSelectedId: string) => {
-              updateAction(sAction.id, {
-                actionId: newSelectedId,
-              });
-            }}
-            openDetailOnCreate
-            categoryTypes={[EntityEnums.Class.Action]}
-            excludedEntities={excludedSuggesterEntities}
-            placeholder={"add new action"}
-            isInsideTemplate={isInsideTemplate}
-            territoryParentId={territoryParentId}
-            territoryActants={territoryActants}
-          />
-        )
+  const updateAction = (statementActionId: string, changes: any) => {
+    if (statement && statementActionId) {
+      const updatedActions = statement.data.actions.map((a) =>
+        a.id === statementActionId ? { ...a, ...changes } : a
       );
-    };
+      const newData = { actions: updatedActions };
+      updateActionsMutation.mutate(newData);
+    }
+  };
+  const removeAction = (statementActionId: string) => {
+    if (statement) {
+      const updatedActions = statement.data.actions.filter(
+        (a) => a.id !== statementActionId
+      );
+      const newData = { actions: updatedActions };
+      updateActionsMutation.mutate(newData);
+    }
+  };
 
-    const [modalOpen, setModalOpen] = useState<boolean>(false);
+  const [, drop] = useDrop({
+    accept: ItemTypes.ACTION_ROW,
+    hover(item: DragItem, monitor: DropTargetMonitor) {
+      dndHoverFn(item, index, monitor, dropRef, moveRow);
+    },
+  });
 
-    const renderButtonsCell = () => {
-      const { action, sAction } = filteredAction.data;
-      const { actionId: propOriginId, id: rowId } = sAction;
+  const [{ isDragging }, drag, preview] = useDrag({
+    item: {
+      type: ItemTypes.ACTION_ROW,
+      index,
+      id: filteredAction.id.toString(),
+    },
+    collect: (monitor: DragSourceMonitor) => ({
+      isDragging: monitor.isDragging(),
+    }),
+    end: (item: DragItem | undefined, monitor: DragSourceMonitor) => {
+      if (item) updateOrderFn();
+    },
+  });
 
-      return (
-        <ButtonGroup noMarginRight>
-          {sAction && (
-            <AttributesEditor
-              modalOpen={modalOpen}
-              setModalOpen={setModalOpen}
-              modalTitle={`Action attribute`}
-              entity={action}
-              disabledAllAttributes={!userCanEdit}
-              data={{
-                elvl: sAction.elvl,
-                certainty: sAction.certainty,
-                logic: sAction.logic,
-                mood: sAction.mood,
-                moodvariant: sAction.moodvariant,
-                bundleOperator: sAction.bundleOperator,
-                bundleStart: sAction.bundleStart,
-                bundleEnd: sAction.bundleEnd,
-              }}
-              handleUpdate={(newData) => {
-                updateAction(sAction.id, newData);
-              }}
-              updateActantId={(newId: string) => {
-                updateAction(sAction.id, { actionId: newId });
-              }}
-              userCanEdit={userCanEdit}
-              classEntitiesActant={[EntityEnums.Class.Action]}
-              loading={updateActionsMutation.isLoading}
-              isInsideTemplate={isInsideTemplate}
-              territoryParentId={territoryParentId}
-            />
-          )}
-          {userCanEdit && (
+  const opacity = isDragging ? 0.2 : 1;
+
+  preview(drop(dropRef));
+  drag(dragRef);
+
+  const renderActionCell = () => {
+    const { action, sAction } = filteredAction.data;
+    return action ? (
+      <EntityTag
+        fullWidth
+        entity={action}
+        button={
+          userCanEdit && (
             <Button
               key="d"
-              icon={<FaTrashAlt />}
-              color="plain"
+              tooltipLabel="unlink action"
+              icon={<FaUnlink />}
               inverted
-              tooltipLabel="remove action row"
+              color="plain"
               onClick={() => {
-                removeAction(filteredAction.data.sAction.id);
+                updateAction(sAction.id, {
+                  actionId: "",
+                });
               }}
             />
-          )}
-          {userCanEdit && (
-            <Button
-              key="a"
-              icon={<FaPlus />}
-              color="plain"
-              inverted
-              tooltipLabel="add new prop"
-              onClick={() => {
-                addProp(rowId);
-              }}
-            />
-          )}
-          {sAction.logic == "2" && (
-            <Button
-              key="neg"
-              tooltipLabel="Negative logic"
-              color="success"
-              inverted
-              noBorder
-              icon={<AttributeIcon attributeName={"negation"} />}
-              onClick={() => setModalOpen(true)}
-            />
-          )}
-          {sAction.bundleOperator && (
-            <Button
-              key="oper"
-              tooltipLabel="Logical operator type"
-              color="success"
-              inverted
-              noBorder
-              icon={sAction.bundleOperator}
-              onClick={() => setModalOpen(true)}
-            />
-          )}
-        </ButtonGroup>
-      );
-    };
-
-    const dispatch = useAppDispatch();
-    const draggedActantRow: DraggedActantRowItem = useAppSelector(
-      (state) => state.rowDnd.draggedActantRow
-    );
-
-    useEffect(() => {
-      if (isDragging) {
-        dispatch(
-          setDraggedActantRow({ category: DraggedPropRowCategory.ACTION })
-        );
-      } else {
-        dispatch(setDraggedActantRow({}));
-      }
-    }, [isDragging]);
-
-    const renderPropGroup = useCallback(
-      (originId: string, props: IProp[], category: DraggedPropRowCategory) => {
-        const originActant = statement.entities[originId];
-
-        if (props.length > 0) {
-          return (
-            <PropGroup
-              boxEntity={statement}
-              originId={originActant ? originActant.id : ""}
-              entities={statement.entities}
-              props={props}
-              territoryId={territoryId}
-              updateProp={updateProp}
-              removeProp={removeProp}
-              addProp={addProp}
-              movePropToIndex={movePropToIndex}
-              userCanEdit={userCanEdit}
-              openDetailOnCreate={false}
-              category={category}
-              isInsideTemplate={isInsideTemplate}
-              territoryParentId={territoryParentId}
-            />
-          );
+          )
         }
-      },
-      [statement]
-    );
-
-    return (
-      <React.Fragment key={index}>
-        <StyledGrid ref={dropRef} style={{ opacity }}>
-          {userCanEdit && (
-            <StyledGridColumn ref={dragRef} style={{ cursor: "move" }}>
-              <FaGripVertical />
-            </StyledGridColumn>
-          )}
-          <StyledGridColumn>{renderActionCell()}</StyledGridColumn>
-          <StyledGridColumn>{renderButtonsCell()}</StyledGridColumn>
-        </StyledGrid>
-
-        {!(
-          draggedActantRow.category &&
-          draggedActantRow.category === DraggedPropRowCategory.ACTION
-        ) &&
-          renderPropGroup(
-            filteredAction.data.sAction.actionId,
-            filteredAction.data.sAction.props,
-            DraggedPropRowCategory.ACTION
-          )}
-      </React.Fragment>
+      />
+    ) : (
+      userCanEdit && (
+        <EntitySuggester
+          onSelected={(newSelectedId: string) => {
+            updateAction(sAction.id, {
+              actionId: newSelectedId,
+            });
+          }}
+          openDetailOnCreate
+          categoryTypes={[EntityEnums.Class.Action]}
+          excludedEntities={excludedSuggesterEntities}
+          placeholder={"add new action"}
+          isInsideTemplate={isInsideTemplate}
+          territoryParentId={territoryParentId}
+          territoryActants={territoryActants}
+        />
+      )
     );
   };
+
+  const [modalOpen, setModalOpen] = useState<boolean>(false);
+
+  const renderButtonsCell = () => {
+    const { action, sAction } = filteredAction.data;
+    const { actionId: propOriginId, id: rowId } = sAction;
+
+    return (
+      <ButtonGroup noMarginRight>
+        {sAction && (
+          <AttributesEditor
+            modalOpen={modalOpen}
+            setModalOpen={setModalOpen}
+            modalTitle={`Action attribute`}
+            entity={action}
+            disabledAllAttributes={!userCanEdit}
+            data={{
+              elvl: sAction.elvl,
+              certainty: sAction.certainty,
+              logic: sAction.logic,
+              mood: sAction.mood,
+              moodvariant: sAction.moodvariant,
+              bundleOperator: sAction.bundleOperator,
+              bundleStart: sAction.bundleStart,
+              bundleEnd: sAction.bundleEnd,
+            }}
+            handleUpdate={(newData) => {
+              updateAction(sAction.id, newData);
+            }}
+            updateActantId={(newId: string) => {
+              updateAction(sAction.id, { actionId: newId });
+            }}
+            userCanEdit={userCanEdit}
+            classEntitiesActant={[EntityEnums.Class.Action]}
+            loading={updateActionsMutation.isLoading}
+            isInsideTemplate={isInsideTemplate}
+            territoryParentId={territoryParentId}
+          />
+        )}
+        {userCanEdit && (
+          <Button
+            key="d"
+            icon={<FaTrashAlt />}
+            color="plain"
+            inverted
+            tooltipLabel="remove action row"
+            onClick={() => {
+              removeAction(filteredAction.data.sAction.id);
+            }}
+          />
+        )}
+        {userCanEdit && (
+          <Button
+            key="a"
+            icon={<FaPlus />}
+            color="plain"
+            inverted
+            tooltipLabel="add new prop"
+            onClick={() => {
+              addProp(rowId);
+            }}
+          />
+        )}
+        {sAction.logic == "2" && (
+          <Button
+            key="neg"
+            tooltipLabel="Negative logic"
+            color="success"
+            inverted
+            noBorder
+            icon={<AttributeIcon attributeName={"negation"} />}
+            onClick={() => setModalOpen(true)}
+          />
+        )}
+        {sAction.bundleOperator && (
+          <Button
+            key="oper"
+            tooltipLabel="Logical operator type"
+            color="success"
+            inverted
+            noBorder
+            icon={sAction.bundleOperator}
+            onClick={() => setModalOpen(true)}
+          />
+        )}
+      </ButtonGroup>
+    );
+  };
+
+  const dispatch = useAppDispatch();
+  const draggedActantRow: DraggedActantRowItem = useAppSelector(
+    (state) => state.rowDnd.draggedActantRow
+  );
+
+  useEffect(() => {
+    if (isDragging) {
+      dispatch(
+        setDraggedActantRow({ category: DraggedPropRowCategory.ACTION })
+      );
+    } else {
+      dispatch(setDraggedActantRow({}));
+    }
+  }, [isDragging]);
+
+  const renderPropGroup = useCallback(
+    (originId: string, props: IProp[], category: DraggedPropRowCategory) => {
+      const originActant = statement.entities[originId];
+
+      if (props.length > 0) {
+        return (
+          <PropGroup
+            boxEntity={statement}
+            originId={originActant ? originActant.id : ""}
+            entities={statement.entities}
+            props={props}
+            territoryId={territoryId}
+            updateProp={updateProp}
+            removeProp={removeProp}
+            addProp={addProp}
+            movePropToIndex={movePropToIndex}
+            userCanEdit={userCanEdit}
+            openDetailOnCreate={false}
+            category={category}
+            isInsideTemplate={isInsideTemplate}
+            territoryParentId={territoryParentId}
+          />
+        );
+      }
+    },
+    [statement]
+  );
+
+  return (
+    <React.Fragment key={index}>
+      <StyledGrid ref={dropRef} style={{ opacity }}>
+        <StyledGridColumn ref={dragRef} style={{ cursor: "move" }}>
+          {userCanEdit && <FaGripVertical />}
+        </StyledGridColumn>
+        <StyledGridColumn>{renderActionCell()}</StyledGridColumn>
+        <StyledGridColumn>{renderButtonsCell()}</StyledGridColumn>
+      </StyledGrid>
+
+      {!(
+        draggedActantRow.category &&
+        draggedActantRow.category === DraggedPropRowCategory.ACTION
+      ) &&
+        renderPropGroup(
+          filteredAction.data.sAction.actionId,
+          filteredAction.data.sAction.props,
+          DraggedPropRowCategory.ACTION
+        )}
+    </React.Fragment>
+  );
+};
