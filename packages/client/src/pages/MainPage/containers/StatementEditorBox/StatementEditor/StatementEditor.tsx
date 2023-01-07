df25dec65fe82b554c13b7e30264f5f30bb4a0d3
adammertel <mertel.adam@gmail.com>
2023-01-07 17:00:11 +0100

    Disable templates in several places

--- packages/client/src/pages/MainPage/containers/StatementEditorBox/StatementEditor/StatementEditor.tsx
+++ packages/client/src/pages/MainPage/containers/StatementEditorBox/StatementEditor/StatementEditor.tsx
@@ -691,6 +691,7 @@ export const StatementEditor: React.FC<StatementEditor> = ({
                     toast.info("Tag already added!");
                   }
                 }}
+                disableTemplatesAccept
                 categoryTypes={classesEditorTags}
                 placeholder={"add new tag"}
                 excludedEntities={excludedSuggesterEntities}
