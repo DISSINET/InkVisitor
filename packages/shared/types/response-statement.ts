e5db7d2efe3d126002b1e83fa30e80d4710d66d8
Ján Mertel <10438157+jancimertel@users.noreply.github.com>
2023-01-11 23:11:30 +0100

    add additional types for Classif/Identif orders

--- packages/shared/types/response-statement.ts
+++ packages/shared/types/response-statement.ts
@@ -6,18 +6,30 @@ import { IEntity, IStatement } from ".";
 import { UserEnums } from "../enums";
 
 export interface EntityOrder {
-  entityId: string;
-  linkId: string;
+  entityId: string; // actant.entityId
+  linkId: string; // actant.id
 }
 
 export interface PropOrder {
   propValueId: string;
   propTypeId: string;
-  originId: string;
-  linkId: string;
+  originId: string; // action|actant.entityId
+  linkId: string; // action|actant.id
 }
 
-export type OrderType = EntityOrder | PropOrder;
+export interface ClassificationOrder {
+  entityId: string; // actant.classification.entityId
+  originId: string; // actant.entityId
+  linkId: string; // actant.id
+}
+
+export interface IdentificationOrder {
+  entityId: string; // actant.identification.entityId
+  originId: string; // actant.entityId
+  linkId: string; // actant.id
+}
+
+export type OrderType = EntityOrder | PropOrder | ClassificationOrder | IdentificationOrder;
 
 export interface IResponseStatement extends IStatement {
   entities: { [key: string]: IEntity; }; // all entities (IEntity) used in actions/actants, actions/actants.props.type/value, territory, references, tags, actant identifications and classifications
