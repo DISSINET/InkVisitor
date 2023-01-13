5a97adad202a9dc7fb44b944f4ba58b968f99436
adammertel <mertel.adam@gmail.com>
2023-01-13 18:47:35 +0100

    Rewrite linkId to elementId in response statement model

--- packages/shared/types/response-statement.ts
+++ packages/shared/types/response-statement.ts
@@ -7,32 +7,36 @@ import { UserEnums } from "../enums";
 
 export interface EntityOrder {
   entityId: string; // actant.entityId | action.actionId
-  linkId: string; // actant.id | action.id
+  elementId: string; // action|actant.id
 }
 
 export interface PropOrder {
   propValueId: string;
   propTypeId: string;
   originId: string; // action|actant.entityId
-  linkId: string; // action|actant.id
+  elementId: string; // action|actant.prop.id
 }
 
 export interface ClassificationOrder {
   entityId: string; // actant.classification.entityId
   originId: string; // actant.entityId
-  linkId: string; // actant.id
+  elementId: string; // action|actant.classification.id
 }
 
 export interface IdentificationOrder {
   entityId: string; // actant.identification.entityId
   originId: string; // actant.entityId
-  linkId: string; // actant.id
+  elementId: string; // actant.identification.id
 }
 
-export type OrderType = EntityOrder | PropOrder | ClassificationOrder | IdentificationOrder;
+export type OrderType =
+  | EntityOrder
+  | PropOrder
+  | ClassificationOrder
+  | IdentificationOrder;
 
 export interface IResponseStatement extends IStatement {
-  entities: { [key: string]: IEntity; }; // all entities (IEntity) used in actions/actants, actions/actants.props.type/value, territory, references, tags, actant identifications and classifications
+  entities: { [key: string]: IEntity }; // all entities (IEntity) used in actions/actants, actions/actants.props.type/value, territory, references, tags, actant identifications and classifications
   statementOrders: OrderType[];
   // usedIn?: IStatement[];
   right?: UserEnums.RoleMode;
