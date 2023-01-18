ce22f8bd947e9bec4e0af93ffd6ba17f69e42e9c
Ján Mertel <10438157+jancimertel@users.noreply.github.com>
2023-01-18 08:34:02 +0100

    update entityIds filter to be truly optional

--- packages/server/src/models/entity/response-search.ts
+++ packages/server/src/models/entity/response-search.ts
@@ -239,15 +239,14 @@ export class SearchQuery {
       }
 
       const assocEntityIds = await this.getStatementObjectIdsForTerritories(territoryIds);
-      
+
       if (!req.entityIds) {
         req.entityIds = [];
       }
       req.entityIds = req.entityIds.concat(assocEntityIds);
+    }
 
-      // TODO check this
-      this.whereEntityIds(req.entityIds);
-    } else if (req.entityIds?.length) {
+    if (req.entityIds) {
       this.whereEntityIds(req.entityIds);
     }
 
@@ -270,6 +269,8 @@ export class SearchQuery {
     if (req.label) {
       this.whereLabel(req.label);
     }
+
+    console.log(this.query.toString());
   }
 
   /**
