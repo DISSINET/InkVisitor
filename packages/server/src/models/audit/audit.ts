a588d8b4bbf9a3a789354c4ca5cc0175410285f2
Ján Mertel <10438157+jancimertel@users.noreply.github.com>
2023-01-19 22:09:43 +0100

    move audit response methods to Audit static methods

--- packages/server/src/models/audit/audit.ts
+++ packages/server/src/models/audit/audit.ts
@@ -3,6 +3,7 @@ import { r as rethink, Connection, WriteResult } from "rethinkdb-ts";
 import { IAudit } from "@shared/types";
 import { InternalServerError } from "@shared/types/errors";
 import { IRequest } from "src/custom_typings/request";
+import { DbEnums } from "@shared/enums";
 
 export default class Audit implements IAudit, IDbModel {
   static table = "audits";
@@ -90,5 +91,42 @@ export default class Audit implements IAudit, IDbModel {
       changes: updateData,
     });
     return entry.save(req.db.connection);
+  }
+
+  /**
+ * Retrieves first created audit entry for entity.
+ * First audit entry stands for created-at entry.
+ * @param db rethinkdb Connection
+ * @param entityId string
+ * @returns Promise<Audit | null>
+ */
+  static async getFirstForEntity(db: Connection, entityId: string): Promise<Audit | null> {
+    const result = await rethink
+      .table(Audit.table)
+      .getAll(entityId, { index: DbEnums.Indexes.AuditEntityId })
+      .orderBy(rethink.asc("date"))
+      .limit(1)
+      .run(db);
+
+    return result && result.length ? new Audit(result[0]) : null;
+  }
+
+  /**
+ * Retrieves N audits for entity, ordered by date DESC (last N items)
+ * @param dbConn rethinkdb Connection
+ * @param entityId string
+ * @param n limit for returned entries
+ * @returns Promise<Audit[]>
+ */
+  static async getLastNForEntity(dbConn: Connection, entityId: string, n: number = 5): Promise<Audit[]> {
+    const result = await rethink
+      .table(Audit.table)
+      .getAll(entityId, { index: DbEnums.Indexes.AuditEntityId })
+      .orderBy(rethink.desc("date"))
+      .limit(n)
+      .run(dbConn);
+
+
+    return result.map((r) => new Audit(r));
   }
 }
