501054bd6095ed44b68657a013a3dc47b9028e4c
Petr Hanak <petr.nahak@gmail.com>
2023-01-22 20:46:32 +0000

    fix admin page

--- packages/client/src/pages/Users/index.tsx
+++ packages/client/src/pages/Users/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { UserList } from "./containers";
-import { StyledBoxWrap, StyledContent } from "./UsersPageStyles";
+import { StyledBoxWrap, StyledContent, StyledWrapper } from "./UsersPageStyles";
 
 interface UsersPageProps {}
 
