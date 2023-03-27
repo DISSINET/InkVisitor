3aca31c4a41feb42f4d6272dec9990435f0dbbbc
Ján Mertel <10438157+jancimertel@users.noreply.github.com>
2023-03-27 09:59:41 +0200

    add tests

--- packages/server/src/modules/entities/entities.delete.test.ts
+++ packages/server/src/modules/entities/entities.delete.test.ts
@@ -1,9 +1,5 @@
 import { clean, testErroneousResponse } from "@modules/common.test";
-import {
-  EntityDoesNotExist,
-  BadParams,
-  InvalidDeleteError,
-} from "@shared/types/errors";
+import { EntityDoesNotExist, InvalidDeleteError } from "@shared/types/errors";
 import { Db } from "@service/RethinkDB";
 import request from "supertest";
 import { apiPath } from "@common/constants";
@@ -12,18 +8,11 @@ import { findEntityById } from "@service/shorthands";
 import { supertestConfig } from "..";
 import { IEntity } from "@shared/types";
 import Territory from "@models/territory/territory";
+import Classification from "@models/relation/classification";
+import Person from "@models/person/person";
+import Concept from "@models/concept/concept";
 
 describe("Entities delete", function () {
-  describe("empty data", () => {
-    it("should return a BadParams error wrapped in IResponseGeneric", (done) => {
-      return request(app)
-        .delete(`${apiPath}/entities`)
-        .set("authorization", "Bearer " + supertestConfig.token)
-        .expect("Content-Type", /json/)
-        .expect(testErroneousResponse.bind(undefined, new BadParams("")))
-        .then(() => done());
-    });
-  });
   describe("faulty data", () => {
     it("should return a EntityDoesNotExist error wrapped in IResponseGeneric", (done) => {
       return request(app)
@@ -37,48 +26,71 @@ describe("Entities delete", function () {
     });
   });
   describe("ok data", () => {
-    it("should return a 200 code with successful response", async (done) => {
-      const db = new Db();
-      await db.initDb();
-      const territory = new Territory({});
-      await territory.save(db.connection);
+    const db = new Db();
+    const rand = Math.random().toString();
+    const rootTerritory = new Territory({ id: `root-${rand}` });
+    const leafTerritory = new Territory({
+      data: { parent: { territoryId: rootTerritory.id, order: -1 } },
+    });
+    const deletableTerritory = new Territory({
+      data: { parent: { territoryId: rootTerritory.id, order: -1 } },
+    });
+    const personEntity = new Person({ id: `P-${rand}` });
+    const conceptEntity = new Concept({ id: `C-${rand}` });
+    const relation = new Classification({
+      entityIds: [personEntity.id, conceptEntity.id],
+    });
 
+    beforeAll(async () => {
+      await db.initDb();
+      await rootTerritory.save(db.connection);
+      await leafTerritory.save(db.connection);
+      await deletableTerritory.save(db.connection);
+      await personEntity.save(db.connection);
+      await conceptEntity.save(db.connection);
+      await relation.save(db.connection);
+      console.log("done");
+    });
+
+    afterAll(async () => await clean(db));
+
+    it("should return a 200 code with successful response", async () => {
       await request(app)
-        .delete(`${apiPath}/entities/${territory.id}`)
+        .delete(`${apiPath}/entities/${deletableTerritory.id}`)
         .set("authorization", "Bearer " + supertestConfig.token)
         .expect("Content-Type", /json/)
         .expect(200)
         .expect(async () => {
-          const deletedEntity = await findEntityById<IEntity>(db, territory.id);
+          const deletedEntity = await findEntityById<IEntity>(
+            db,
+            deletableTerritory.id
+          );
           expect(deletedEntity).toBeNull();
         });
-
-      await clean(db);
-      done();
     });
-  });
 
-  describe("territory with childs", () => {
-    it("should return an InvalidDeleteError error wrapped in IResponseGeneric", async (done) => {
-      const db = new Db();
-      await db.initDb();
-      const root = new Territory({});
-      await root.save(db.connection);
-      const leaf = new Territory({
-        data: { parent: { territoryId: root.id, order: -1 } },
+    describe("territory with childs", () => {
+      it("should return an InvalidDeleteError error wrapped in IResponseGeneric", async () => {
+        await request(app)
+          .delete(`${apiPath}/entities/${rootTerritory.id}`)
+          .set("authorization", "Bearer " + supertestConfig.token)
+          .expect("Content-Type", /json/)
+          .expect(
+            testErroneousResponse.bind(undefined, new InvalidDeleteError(""))
+          );
       });
-      await leaf.save(db.connection);
+    });
 
-      await request(app)
-        .delete(`${apiPath}/entities/${root.id}`)
-        .set("authorization", "Bearer " + supertestConfig.token)
-        .expect("Content-Type", /json/)
-        .expect(
-          testErroneousResponse.bind(undefined, new InvalidDeleteError(""))
-        );
-
-      await clean(db);
-      done();
+    describe("entity with relations", () => {
+      it("should return an InvalidDeleteError error wrapped in IResponseGeneric", async () => {
+        await request(app)
+          .delete(`${apiPath}/entities/${personEntity.id}`)
+          .set("authorization", "Bearer " + supertestConfig.token)
+          .expect("Content-Type", /json/)
+          .expect(
+            testErroneousResponse.bind(undefined, new InvalidDeleteError(""))
+          );
+      });
     });
   });
 });
