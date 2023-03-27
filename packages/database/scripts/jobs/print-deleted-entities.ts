9b91b0a0677638e491dc1a7e9c8f6ffcd180b14b
Ján Mertel <10438157+jancimertel@users.noreply.github.com>
2023-03-27 09:57:58 +0200

    add job for printing not deleted entity ids in relations

---
+++ packages/database/scripts/jobs/print-deleted-entities.ts
@@ -0,0 +1,29 @@
+import { IAudit, IEntity } from "@shared/types";
+import { r, Connection, RDatum, } from "rethinkdb-ts";
+import { ITask } from ".";
+
+const printDeletedEntities: ITask = async (db: Connection): Promise<void> => {
+  const entityIdsFromRelations = (await r.table("relations").pluck("entityIds", "id").run(db)) as { entityIds: string[], id: string}[];
+  const idsMap = entityIdsFromRelations.reduce<Record<string, {relationIds: string[]}>>((acc, curr) => {
+    for(const id of curr.entityIds) {
+      if (!acc[id]) {
+        acc[id] = { relationIds: []}
+      }
+      if (!id) {
+        console.log("WTF")
+      }
+      acc[id].relationIds.push(curr.id);
+    }
+    return acc
+  }, {})
+
+  console.log(`Got ${Object.keys(idsMap).length} items`)
+  for (const entityId of Object.keys(idsMap)) {
+    const entity = await r.table("entities").get(entityId).run(db);
+    if (!entity || entity.length === 0) {
+      console.log(`Entity ${entityId} not found in: ${idsMap[entityId].relationIds.join(",")}`)
+    }
+  }
+}
+
+export default printDeletedEntities;
