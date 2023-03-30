dee04ebe74965da422eab076ea1c4eeedad137a6
Ján Mertel <10438157+jancimertel@users.noreply.github.com>
2023-03-30 23:52:40 +0200

    add information log to printDeletedEntities job

--- packages/database/scripts/jobs/print-deleted-entities.ts
+++ packages/database/scripts/jobs/print-deleted-entities.ts
@@ -10,7 +10,7 @@ const printDeletedEntities: ITask = async (db: Connection): Promise<void> => {
         acc[id] = { relationIds: []}
       }
       if (!id) {
-        console.log("WTF")
+        console.log("Empty id")
       }
       acc[id].relationIds.push(curr.id);
     }
@@ -18,10 +18,15 @@ const printDeletedEntities: ITask = async (db: Connection): Promise<void> => {
   }, {})
 
   console.log(`Got ${Object.keys(idsMap).length} items`)
+  let i = 0;
   for (const entityId of Object.keys(idsMap)) {
+    i++;
     const entity = await r.table("entities").get(entityId).run(db);
     if (!entity || entity.length === 0) {
       console.log(`Entity ${entityId} not found in: ${idsMap[entityId].relationIds.join(",")}`)
+    }
+    if (i % 100 === 0) {
+      console.log(`${i}/${Object.keys(idsMap).length}`)
     }
   }
 }
