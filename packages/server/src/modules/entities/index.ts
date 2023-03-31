07760079abb75680d6e4128b86a736201239fd40
Ján Mertel <10438157+jancimertel@users.noreply.github.com>
2023-04-01 00:48:48 +0200

    add POST /entities/:entityId/clone + test

--- packages/server/src/modules/entities/index.ts
+++ packages/server/src/modules/entities/index.ts
@@ -1,7 +1,7 @@
 import { mergeDeep } from "@common/functions";
 import { ResponseEntity, ResponseEntityDetail } from "@models/entity/response";
 import Audit from "@models/audit/audit";
-import { getEntityClass } from "@models/factory";
+import { getEntityClass, getRelationClass } from "@models/factory";
 import { findEntityById } from "@service/shorthands";
 import {
   IEntity,
@@ -193,6 +193,81 @@ export default Router()
       await Audit.createNew(request, model.id, request.body);
       return {
         result: true,
+      };
+    })
+  )
+  /**
+   * @openapi
+   * /entities/:entityId/clone:
+   *   post:
+   *     description: Create a new cloned entity from another
+   *     tags:
+   *       - entities
+   *     responses:
+   *       200:
+   *         description: Returns generic response
+   *         content:
+   *           application/json:
+   *             schema:
+   *               $ref: "#/components/schemas/IResponseGeneric"
+   */
+  .post(
+    "/:entityId/clone",
+    asyncRouteHandler<IResponseGeneric>(async (request: IRequest) => {
+      const originalId = request.params.entityId;
+      const original = await findEntityById(request.db, originalId);
+      if (!original) {
+        throw new EntityDoesNotExist(
+          "cannot copy entity - does not exist",
+          originalId
+        );
+      }
+
+      // clone the entry without id - should be created anew
+      const clone = getEntityClass({ ...original, id: "" });
+      if (!clone.isValid()) {
+        throw new ModelNotValidError("");
+      }
+
+      if (!clone.canBeCreatedByUser(request.getUserOrFail())) {
+        throw new PermissionDeniedError("entity cannot be copied");
+      }
+
+      await request.db.lock();
+
+      const saved = await clone.save(request.db.connection);
+      if (!saved) {
+        throw new InternalServerError("cannot copy entity");
+      }
+
+      const relations = await Relation.getForEntity(
+        request.db.connection,
+        originalId
+      );
+
+      let relationConflict = false;
+
+      for (const relation of relations) {
+        // replace original entity id with cloned id + remove original relation id - should be created anew
+        relation.entityIds = relation.entityIds.map((id) =>
+          id === originalId ? clone.id : id
+        );
+        const clonedRelation = getRelationClass({ ...relation, id: "" });
+
+        // todo beforeSave can fail - should be done before entity cloning
+        await clonedRelation.beforeSave(request);
+        await clonedRelation.save(request.db.connection);
+        await clonedRelation.afterSave(request);
+
+        relationConflict = true;
+      }
+
+      return {
+        result: true,
+        message: relationConflict
+          ? "There has been at least one conflict while copying relations"
+          : undefined,
+        data: clone.id,
       };
     })
   )
