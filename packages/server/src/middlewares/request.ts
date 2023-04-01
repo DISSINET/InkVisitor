340dee0ff72a69433c86a093a5a2bc6c8eade504
Ján Mertel <10438157+jancimertel@users.noreply.github.com>
2023-04-01 23:36:18 +0200

    update custom after-auth middleware to check active status of user + tests

--- packages/server/src/middlewares/request.ts
+++ packages/server/src/middlewares/request.ts
@@ -1,7 +1,13 @@
-import { InternalServerError } from "@shared/types/errors";
+import { InternalServerError, UserNotActiveError } from "@shared/types/errors";
 import { Response, Request, NextFunction } from "express";
 import User from "@models/user/user";
 
+/**
+ * Function checks if bound user variable is null and it either returns it or throws an error,
+ * removing the need to use condition further in the code.
+ * @param user
+ * @returns
+ */
 const getUserOrFail = function (user: User | null): User {
   if (user) {
     return user;
@@ -12,7 +18,15 @@ const getUserOrFail = function (user: User | null): User {
   }
 };
 
-export default async function customizeRequest(
+/**
+ * Middleware that assigns current user getter to the request object.
+ * The getter will trigger an error if called while the user object is empty.
+ * This middleware should be therefore applied after the jwt middleware.
+ * @param req
+ * @param res
+ * @param next
+ */
+export default async function customizeAuthenticatedRequest(
   req: Request,
   res: Response,
   next: NextFunction
@@ -20,6 +34,11 @@ export default async function customizeRequest(
   let user: User | null = null;
   if (req.user && req.user.user) {
     user = await User.findUserById(req.db.connection, req.user.user.id);
+    if (user && !user.active) {
+      next(
+        new UserNotActiveError(UserNotActiveError.message, req.user.user.id)
+      );
+    }
   }
 
   req.getUserOrFail = getUserOrFail.bind(undefined, user);
