165ed2ba878ead841c706ce3aad2168adb50c094
Ján Mertel <10438157+jancimertel@users.noreply.github.com>
2023-04-16 00:53:50 +0200

    add tests for valid warningless response

--- packages/server/src/models/statement/response.test.ts
+++ packages/server/src/models/statement/response.test.ts
@@ -1,6 +1,12 @@
-import { StatementEnums } from "@shared/enums";
+import Action from "@models/action/action";
+import Group from "@models/group/group";
+import Person from "@models/person/person";
+import { EntityEnums, StatementEnums } from "@shared/enums";
+import { InternalServerError } from "@shared/types/errors";
 import "ts-jest";
 import { ResponseStatement } from "./response";
+import Statement, { StatementActant, StatementAction } from "./statement";
+import { prepareStatement } from "./statement.test";
 
 describe("models/statement/response", function () {
   test("test ResponseStatement.sortListOfStatementItems", function () {
@@ -39,10 +45,53 @@ describe("models/statement/response", function () {
         entityId: "6",
         elementId: "6",
         type: StatementEnums.ElementType.Actant,
-        order: false
+        order: false,
       },
     ]);
 
-    expect(sorted.map(e => e.elementId)).toStrictEqual(["1", "5", "4", "3", "2", "6"]);
+    expect(sorted.map((e) => e.elementId)).toStrictEqual([
+      "1",
+      "5",
+      "4",
+      "3",
+      "2",
+      "6",
+    ]);
+  });
+
+  describe("test ResponseStatement.getWarnings", function () {
+    test("not prepared entity should thrown an error", () => {
+      const [, statement] = prepareStatement();
+      const response = new ResponseStatement(statement);
+      expect(() => response.getWarnings()).toThrowError(InternalServerError);
+    });
+
+    test("warningless statement response", () => {
+      const statement = new Statement({ id: "statement" });
+      const action1 = new Action({ id: "action1" });
+      action1.data.entities.s = EntityEnums.PLOGESTRB;
+      const action2 = new Action({ id: "action2" });
+      action2.data.entities.s = EntityEnums.PLOGESTRB;
+      statement.data.actions = [
+        new StatementAction({ actionId: action1.id }),
+        new StatementAction({ actionId: action2.id }),
+      ];
+
+      const person = new Person({ id: "person" });
+      const group = new Group({ id: "group" });
+      statement.data.actants = [
+        new StatementActant({ entityId: person.id }),
+        new StatementActant({ entityId: group.id }),
+      ];
+
+      const response = new ResponseStatement(statement);
+      response.entities[statement.id] = statement;
+      response.entities[action1.id] = action1;
+      response.entities[action2.id] = action2;
+      response.entities[person.id] = person;
+      response.entities[group.id] = group;
+
+      expect(response.getWarnings()).toHaveLength(0);
+    });
   });
 });
