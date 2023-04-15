50b4dee2b8e24fbdcfad45cf8181cbee1b16557d
Ján Mertel <10438157+jancimertel@users.noreply.github.com>
2023-04-15 22:06:56 +0200

    add implementation for ActionValency & ActionEntity change signature of ActionEntity fields

--- packages/server/src/models/action/action.ts
+++ packages/server/src/models/action/action.ts
@@ -2,34 +2,64 @@ import { fillFlatObject, UnknownObject, IModel } from "@models/common";
 import { EntityEnums } from "@shared/enums";
 import Entity from "@models/entity/entity";
 import { IAction } from "@shared/types";
-import { ActionEntity, ActionValency, IActionData } from "@shared/types/action";
+import {
+  IActionEntity,
+  IActionValency,
+  IActionData,
+} from "@shared/types/action";
 
-class ActionData implements IActionData, IModel {
-  valencies: ActionValency = {
-    a1: "",
-    a2: "",
-    s: "",
-  };
-  entities: ActionEntity = {
-    a1: [],
-    a2: [],
-    s: [],
-  };
-  status: EntityEnums.Status = EntityEnums.Status.Pending;
+class ActionValency implements IActionValency, IModel {
+  a1: string;
+  a2: string;
+  s: string;
 
-  constructor(data: Partial<IActionData>) {
-    if (!data) {
-      return;
-    }
-
-    fillFlatObject(this.valencies, data.valencies as any);
-    if (data.entities) {
-      this.entities = data.entities as any;
-    }
+  constructor(data: Partial<IActionValency>) {
+    this.a1 = data.a1 || "";
+    this.a2 = data.a2 || "";
+    this.s = data.s || "";
   }
 
   isValid(): boolean {
-    return true;
+    return (
+      typeof this.a1 === "string" &&
+      typeof this.a2 === "string" &&
+      typeof this.s === "string"
+    );
+  }
+}
+
+class ActionEntity implements IActionEntity, IModel {
+  a1: EntityEnums.Class[];
+  a2: EntityEnums.Class[];
+  s: EntityEnums.Class[];
+
+  constructor(data: Partial<IActionEntity>) {
+    this.a1 = data.a1 || [];
+    this.a2 = data.a2 || [];
+    this.s = data.s || [];
+  }
+
+  isValid(): boolean {
+    return (
+      this.a1.constructor.name === "Array" &&
+      this.a2.constructor.name === "Array" &&
+      this.s.constructor.name === "Array"
+    );
+  }
+}
+
+class ActionData implements IActionData, IModel {
+  valencies: ActionValency;
+  entities: ActionEntity;
+  status: EntityEnums.Status = EntityEnums.Status.Pending;
+
+  constructor(data: Partial<IActionData>) {
+    this.valencies = new ActionValency(data.valencies || {});
+    this.entities = new ActionEntity(data.entities || {});
+  }
+
+  isValid(): boolean {
+    return this.valencies.isValid() && this.entities.isValid();
   }
 }
 
