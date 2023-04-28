6839d8836ab3964a9a3f00b24e6b1bb83bae5efc
Ján Mertel <10438157+jancimertel@users.noreply.github.com>
2023-04-28 19:39:02 +0200

    add PositionRules implementation for compiling common warning finalize warnings implementation + tests

---
+++ packages/server/src/models/statement/PositionRules.ts
@@ -0,0 +1,32 @@
+import { ActionEntity } from "@models/action/action";
+import { EntityEnums } from "@shared/enums";
+import { IAction } from "@shared/types";
+
+export class Rules {
+  classes: EntityEnums.Class[] = [];
+  undefined = false;
+  allEmpty = true;
+  mismatch = false;
+
+  constructor(actions: IAction[], position: EntityEnums.Position) {
+    for (const action of actions) {
+      const rules = ActionEntity.toRules(action.data.entities)[position];
+      this.undefined = this.undefined || !rules;
+      this.allEmpty = this.allEmpty && ((rules && !rules.length) as boolean);
+
+      if (!this.mismatch) {
+        if (this.classes.length) {
+          this.mismatch = true;
+          for (const rule of rules || []) {
+            console.log("test", this.classes, rule);
+            if (this.classes.indexOf(rule) !== -1) {
+              this.mismatch = false;
+              break;
+            }
+          }
+        }
+      }
+      this.classes = this.classes.concat(rules || []);
+    }
+  }
+}
