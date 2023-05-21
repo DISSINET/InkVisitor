315fe40a5a48610b90033e04892ba28d5c144c41
Ján Mertel <10438157+jancimertel@users.noreply.github.com>
2023-05-21 22:38:31 +0200

    add warninigs for relations

--- packages/shared/enums/warning.ts
+++ packages/shared/enums/warning.ts
@@ -8,5 +8,12 @@ export enum WarningTypeEnums {
   WA = "WA", // "Actant’s entity type does not match the Action",
   ANA = "ANA", // "This actant position allows no actant",
   WAC = "WAC", // "Entity type valencies of the actions not matching",
-  AVU = "AVU" // Action valency not defined
+  AVU = "AVU", // Action valency not defined
+
+  IELVL = "IELVL", // Inconsistent Epistemic levels for the Property
+  SCLM = "SCLM", // Superclass missing
+  ISYNC = "ISYNC", // Inconsistent superclasses in the synonym cloud
+  MVAL = "MVAL", // Missing at least one entity-type valency
+  AVAL = "AVAL", // Asymmetrical valency
+  MAEE = "MAEE", // Missing action/event equivalent
 }
