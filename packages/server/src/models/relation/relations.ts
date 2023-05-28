43ac4d7e9fe81a6e0a69cc07210fdd3482c5989d
Ján Mertel <10438157+jancimertel@users.noreply.github.com>
2023-05-28 22:40:27 +0200

    move common entity warnings to Entity detail response

--- packages/server/src/models/relation/relations.ts
+++ packages/server/src/models/relation/relations.ts
@@ -1,5 +1,5 @@
 import { nonenumerable } from "@common/decorators";
-import { EntityEnums, RelationEnums, WarningTypeEnums } from "@shared/enums";
+import { EntityEnums, RelationEnums } from "@shared/enums";
 import { IWarning, Relation as RelationTypes } from "@shared/types";
 import { Connection } from "rethinkdb-ts";
 import { IRequest } from "src/custom_typings/request";
@@ -18,7 +18,6 @@ import SubjectSemantics from "./subject-semantics";
 import Superclass from "./superclass";
 import SuperordinateLocation from "./superordinate-location";
 import Synonym from "./synonym";
-import Relation from "./relation";
 
 type ISuperclass = RelationTypes.ISuperclass;
 type ISuperordinateLocation = RelationTypes.ISuperordinateLocation;
@@ -353,8 +352,6 @@ export class UsedRelations implements RelationTypes.IUsedRelations {
     if (types.indexOf(RelationEnums.Type.Related) != -1) {
       await this.prepareRelateds(req.db.connection);
     }
-
-    this.warnings = await this.getWarnings(req.db.connection);
   }
 
   getEntityIdsFromType(relationType: RelationEnums.Type): string[] {
@@ -386,105 +383,5 @@ export class UsedRelations implements RelationTypes.IUsedRelations {
     }
 
     return out;
-  }
-
-  /**
-   * Shorthand for creating new warning
-   * @param warningType
-   * @param relId
-   * @returns new instance of warning
-   */
-  newWarning(warningType: WarningTypeEnums, relId?: string): IWarning {
-    return {
-      type: warningType,
-      message: "",
-      origin: relId || "",
-    };
-  }
-
-  /**
-   * prepares warning instances for current response object
-   * @param conn
-   * @returns
-   */
-  async getWarnings(conn: Connection): Promise<IWarning[]> {
-    const warnings: IWarning[] = [];
-
-    const sclmWarning = await this.hasSCLM(conn);
-    if (sclmWarning) {
-      warnings.push(sclmWarning);
-    }
-
-    const isyncWarning = await this.hasISYNC(conn);
-    if (isyncWarning) {
-      warnings.push(isyncWarning);
-    }
-
-    return warnings;
-  }
-
-  /**
-   * Tests if there is SCLM warning and returns it
-   * @param conn
-   * @returns
-   */
-  async hasSCLM(conn: Connection): Promise<IWarning | null> {
-    if (this.entityClass !== EntityEnums.Class.Concept) {
-      return null;
-    }
-
-    const scls = await Relation.findForEntity(
-      conn,
-      this.entityId,
-      RelationEnums.Type.Superclass
-    );
-    let warn: IWarning | null = null;
-    for (const scl of scls) {
-      if (scl.entityIds[0] === this.entityId) {
-        warn = this.newWarning(WarningTypeEnums.SCLM, scl.id);
-        break;
-      }
-    }
-
-    return warn;
-  }
-
-  /**
-   * Tests if there is ISYNC warning and returns it
-   * @param conn
-   * @returns
-   */
-  async hasISYNC(conn: Connection): Promise<IWarning | null> {
-    if (this.entityClass !== EntityEnums.Class.Concept) {
-      return null;
-    }
-
-    const synonym = await Relation.findForEntity(
-      conn,
-      this.entityId,
-      RelationEnums.Type.Synonym
-    );
-
-    let entityIds: string[] = [];
-    for (const syn of synonym) {
-      entityIds = entityIds.concat(syn.entityIds);
-    }
-    entityIds = Array.from(new Set(entityIds));
-
-    // find SCL relations with checked entities on 0 index
-    const scls = await Superclass.findForEntities(
-      conn,
-      entityIds,
-      RelationEnums.Type.Superclass,
-      0
-    );
-
-    let warn: IWarning | null = null;
-    const sclTargets = Array.from(new Set(scls.map((s) => s.entityIds[1])));
-    if (sclTargets.length === 1 && scls.length > 1) {
-      warn = this.newWarning(WarningTypeEnums.ISYNC);
-    }
-
-    return warn;
   }
 }
