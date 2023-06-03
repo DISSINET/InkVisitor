1b2e8463867494dc08649a58742a501bbe8da4ea
Petr Hanak <petr.nahak@gmail.com>
2023-06-03 15:57:23 +0200

    allow basename redirect

--- packages/client/src/ensure-basename.ts
+++ packages/client/src/ensure-basename.ts
@@ -1,14 +1,14 @@
-// if (process.env.ROOT_URL) {
-//   if (!window.location.pathname.includes(process.env.ROOT_URL)) {
-//     window.history.replaceState(
-//       "",
-//       "",
-//       process.env.ROOT_URL +
-//         window.location.pathname +
-//         window.location.search +
-//         window.location.hash
-//     );
-//   }
-// } else {
-//   console.log("ROOT_URL is not set");
-// }
+if (process.env.ROOT_URL) {
+  if (!window.location.pathname.includes(process.env.ROOT_URL)) {
+    window.history.replaceState(
+      "",
+      "",
+      process.env.ROOT_URL +
+        window.location.pathname +
+        window.location.search +
+        window.location.hash
+    );
+  }
+} else {
+  console.log("ROOT_URL is not set");
+}
