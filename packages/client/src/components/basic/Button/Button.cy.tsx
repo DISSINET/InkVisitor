0151f7fc5b7fdb6e52da47c45f71c38a7c41d8ba
Petr Hanak <petr.nahak@gmail.com>
2023-06-28 12:41:22 +0100

    button click test attempt

--- packages/client/src/components/basic/Button/Button.cy.tsx
+++ packages/client/src/components/basic/Button/Button.cy.tsx
@@ -2,11 +2,27 @@ import React from "react";
 import { Button } from "./Button";
 
 describe("<Button />", () => {
-  // beforeEach(() => {
-  //   cy.viewport(500, 500);
-  // });
   it("renders", () => {
-    // see: https://on.cypress.io/mounting-react
     cy.mount(<Button label="test" color="danger" />);
+    cy.get("button").should("exist");
+  });
+  it("label should exist", () => {
+    const buttonText = "Click Me";
+    cy.mount(<Button label={buttonText} color="danger" />);
+    cy.contains("button", buttonText).should("exist");
+  });
+
+  it("should call the onClick handler when clicked", () => {
+    const onClick = cy.stub();
+
+    cy.mount(<Button label="button" onClick={onClick} color="danger" />);
+    cy.get("button").click();
+
+    expect(onClick).to.be.calledOnce;
+  });
+
+  it("should be disabled when the disabled prop is provided", () => {
+    cy.mount(<Button label="T" disabled />);
+    cy.get("button").should("be.disabled");
   });
 });
