2bb864147af437e1f9d7e7199b60e8fc3d742334
adammertel <12932677+adammertel@users.noreply.github.com>
2023-07-03 13:30:02 +0100

    Create pos enums and dictionaries

---
+++ packages/shared/dictionaries/partofspeech.ts
@@ -0,0 +1,39 @@
+import { EntityEnums } from "../enums";
+
+export const conceptPartOfSpeechDict = [
+  { value: EntityEnums.ConceptPartOfSpeech.Noun, label: "noun", infos: "" },
+  { value: EntityEnums.ConceptPartOfSpeech.Adj, label: "adjective", infos: "" },
+  { value: EntityEnums.ConceptPartOfSpeech.Pron, label: "pronoun", infos: "" },
+  { value: EntityEnums.ConceptPartOfSpeech.Adv, label: "adverb", infos: "" },
+  { value: EntityEnums.ConceptPartOfSpeech.Num, label: "numeral", infos: "" },
+  {
+    value: EntityEnums.ConceptPartOfSpeech.Adp,
+    label: "adposition",
+    infos: "",
+  },
+  {
+    value: EntityEnums.ConceptPartOfSpeech.CConj,
+    label: "coordinating conjunction",
+    infos: "",
+  },
+  {
+    value: EntityEnums.ConceptPartOfSpeech.SConj,
+    label: "subordinating conjunction",
+    infos: "",
+  },
+  {
+    value: EntityEnums.ConceptPartOfSpeech.Det,
+    label: "determiner",
+    infos: "",
+  },
+  {
+    value: EntityEnums.ConceptPartOfSpeech.Intj,
+    label: "interjection",
+    infos: "",
+  },
+  { value: EntityEnums.ConceptPartOfSpeech.Part, label: "particle", infos: "" },
+];
+
+export const actionPartOfSpeechDict = [
+  { value: EntityEnums.ActionPartOfSpeech.Verb, label: "verb", infos: "" },
+];
