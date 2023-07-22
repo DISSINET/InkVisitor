4e08efaaa7c4f4147ff59bf35c7e834336a00db5
Ján Mertel <10438157+jancimertel@users.noreply.github.com>
2023-07-22 11:21:56 +0200

    update server settings to load only if ENV_FILE is provided - should depend on variables from env

--- packages/server/src/settings.ts
+++ packages/server/src/settings.ts
@@ -1,18 +1,18 @@
 import dotenv from "dotenv";
 
-// will load .env.development by default
-if (!process.env.NODE_ENV || !!process.env.ENV_FILE) {
-  console.log(
-    `[Settings] loading .env file: .env.${process.env.ENV_FILE || "development"
-    }`
-  );
+// will load .env.${ENV_FILE} if set
+if (process.env.ENV_FILE) {
+  console.log(`[Settings] loading .env file: .env.${process.env.ENV_FILE}`);
 
-  // Set the env file
   const result = dotenv.config({
-    path: `./env/.env.${process.env.ENV_FILE || "development"}`,
+    path: `./env/.env.${process.env.ENV_FILE}`,
   });
 
   if (result.error) {
     throw result.error;
   }
+} else if (!process.env.PORT) {
+  throw new Error(
+    "ENV variables not properly set - either use ENV_FILE to specify the file or provide variables from running environment"
+  );
 }
