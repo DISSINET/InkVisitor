ec58bd96fe37aca1c84f1f315ca9da242c76a482
Ján Mertel <10438157+jancimertel@users.noreply.github.com>
2023-07-22 18:55:29 +0200

    add tests

--- packages/server/src/modules/documents/documents.get.test.ts
+++ packages/server/src/modules/documents/documents.get.test.ts
@@ -6,8 +6,9 @@ import { apiPath } from "@common/constants";
 import app from "../../Server";
 import { Db } from "@service/RethinkDB";
 import Document from "@models/document/document";
+import { IDocument } from "@shared/types";
 
-describe("modules/documents GET", function () {
+describe("modules/documents/:documentId GET", function () {
   describe("Wrong param", () => {
     it("should return an DocumentDoesNotExist error wrapped in IResponseGeneric", () => {
       return request(app)
@@ -44,6 +45,62 @@ describe("modules/documents GET", function () {
         .expect((res) => {
           expect(typeof res.body).toEqual("object");
           expect(res.body.id).toEqual(document.id);
+        });
+    });
+  });
+});
+
+describe("modules/documents GET", function () {
+  describe("Without params", () => {
+    it("should return a 200 code", async () => {
+      return request(app)
+        .get(`${apiPath}/documents`)
+        .set("authorization", "Bearer " + supertestConfig.token)
+        .expect(200);
+    });
+  });
+  describe("Find by ids param", () => {
+    const db = new Db();
+
+    const document1 = new Document({
+      id: Math.random().toString(),
+      content: "test",
+      title: "test",
+    });
+    const document2 = new Document({
+      id: Math.random().toString(),
+      content: "test",
+      title: "test",
+    });
+
+    beforeAll(async () => {
+      await db.initDb();
+      await document1.save(db.connection);
+      await document2.save(db.connection);
+    });
+
+    afterAll(async () => {
+      await document1.delete(db.connection);
+      await document2.delete(db.connection);
+      await db.close();
+    });
+
+    it("should return a 200 code with both documents", async () => {
+      await request(app)
+        .get(
+          `${apiPath}/documents?ids=${[document1.id, document2.id].join(",")}`
+        )
+        .set("authorization", "Bearer " + supertestConfig.token)
+        .expect(200)
+        .expect((res) => {
+          expect(typeof res.body).toEqual("object");
+          expect(res.body.length).toEqual(2);
+          expect(
+            (res.body as IDocument[]).find((d) => d.id === document1.id)
+          ).toBeTruthy();
+          expect(
+            (res.body as IDocument[]).find((d) => d.id === document2.id)
+          ).toBeTruthy();
         });
     });
   });
