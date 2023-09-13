e2e9a5a71253e4a837ddc79446662a3e1d063859
adammertel <12932677+adammertel@users.noreply.github.com>
2023-09-13 09:48:26 +0200

    Get lorem ipsum package

---
+++ packages/client/src/pages/CanvasTest/index.tsx
@@ -0,0 +1,29 @@
+import { MemoizedLoginModal } from "components/advanced";
+import TextCanvas from "components/basic/TextCanvas/TextCanvas";
+import React from "react";
+import { LoremIpsum } from "lorem-ipsum";
+
+interface ILoginPage {}
+
+const lorem = new LoremIpsum({
+  sentencesPerParagraph: {
+    max: 8,
+    min: 4,
+  },
+  wordsPerSentence: {
+    max: 16,
+    min: 4,
+  },
+});
+
+const CanvasTestPage: React.FC<ILoginPage> = ({}) => {
+  const veryLongText = lorem.generateParagraphs(700);
+
+  return (
+    <div>
+      <TextCanvas text={veryLongText} width={500} height={400} />
+    </div>
+  );
+};
+
+export default CanvasTestPage;
