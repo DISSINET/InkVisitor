dee27b0ca7db1ca9d8691e134b7835bafca9a43d
Ján Mertel <10438157+jancimertel@users.noreply.github.com>
2023-09-17 18:31:30 +0200

    add base for scroller, update styles

--- packages/client/src/components/basic/TextCanvas/TextCanvas.tsx
+++ packages/client/src/components/basic/TextCanvas/TextCanvas.tsx
@@ -27,20 +27,20 @@ const TextCanvas: React.FC<TextCanvasProps> = ({ text, width, height }) => {
   const canvasCursorRef = useRef<HTMLCanvasElement>(null);
 
   const numberOfLines = useMemo<number>(() => {
-return Math.floor(height / lineHeight)
-  }, [height, lineHeight])
+    return Math.floor(height / lineHeight);
+  }, [height, lineHeight]);
   /**
    * the first and last line displayed in the canvas
    */
   const viewPort = useMemo<[number, number]>(() => {
-    return [scrollLine, scrollLine + numberOfLines]
-  }, [scrollLine, lineHeight])
+    return [scrollLine, scrollLine + numberOfLines];
+  }, [scrollLine, lineHeight]);
 
   const charWidth = useMemo<number>(() => {
     const canvas = document.createElement("canvas");
     const txt = "Rando text";
     if (canvas) {
-      var ctx = canvas.getContext("2d");
+      const ctx = canvas.getContext("2d");
       ctx!.font = FONT;
       ctx!.fillText(txt, 0, 0);
       const textW = ctx!.measureText(txt).width;
@@ -127,22 +127,21 @@ return Math.floor(height / lineHeight)
       // TODO rerenders the cursor
       const cursorX = cursorChar * charWidth;
       const cursorY = (cursorLine - scrollLine) * lineHeight;
-      ctx!.fillRect(cursorX, cursorY, 2, lineHeight);
+      ctx!.fillRect(cursorX, cursorY, 2, lineHeight + 5);
     }
   }, [currentText, scrollLine, cursorChar, cursorLine, width, height]);
 
   /**
-   * This function checks if the scrolled viewport is not off the current cursor 
+   * This function checks if the scrolled viewport is not off the current cursor
    */
-  useEffect( ()=> {
+  useEffect(() => {
     if (cursorLine >= viewPort[1]) {
-        setScrollLine(cursorLine -  numberOfLines + 1)
-    } 
+      setScrollLine(cursorLine - numberOfLines + 1);
+    }
     if (cursorLine < viewPort[0]) {
-        setScrollLine(cursorLine)
-    } 
-  }, [cursorChar, cursorLine]
-  )  
+      setScrollLine(cursorLine);
+    }
+  }, [cursorChar, cursorLine]);
 
   const handleKeyDown = (e: React.KeyboardEvent<HTMLCanvasElement>) => {
     e.preventDefault();
@@ -165,6 +164,11 @@ return Math.floor(height / lineHeight)
           setCursorChar(lineMap[cursorLine - 1].text.length);
         }
         break;
+      case "PageDown":
+        setCursorLine(
+          cursorLine < lineMap.length - 50 ? cursorLine + 50 : lineMap.length - 50
+        );
+        break;
 
       case "ArrowRight":
         if (cursorChar < lineMap[cursorLine].text.length) {
@@ -182,36 +186,55 @@ return Math.floor(height / lineHeight)
 
   return (
     <div style={{ padding: "20px" }}>
-      <div
-        style={{
-          position: "relative",
-          width: `${width}px`,
-          height: `${height}px`,
-          border: "2px solid black",
-        }}
-      >
-        {/* canvas for text */}
-        <canvas
-          ref={canvasTextRef}
-          width={width}
-          height={height}
-          style={{ position: "absolute", top: "0", left: "0" }}
-          onKeyDown={handleKeyDown}
-          tabIndex={0}
-        ></canvas>
-
-        {/* canvas for cursor */}
-        <canvas
-          ref={canvasCursorRef}
-          width={width}
-          height={height}
-          style={{ position: "absolute", top: "0", left: "0", zIndex: 1 }}
-          onKeyDown={handleKeyDown}
-          tabIndex={0}
-        ></canvas>
-      </div>
-      <div>
+      <div className="textarea" style={{    display: "flex", flexDirection: "row"}}>
+        <div className="canvasses-wrapper" style={{
+            border: "1px solid black",
+            padding: "2px"
+        }}>
         <div
+          style={{
+            position: "relative",
+            width: `${width}px`,
+            height: `${height}px`,
+          }}
+        >
+          {/* canvas for text */}
+           <canvas
+            ref={canvasTextRef}
+            width={width}
+            height={height}
+            style={{ position: "absolute", top: "0", left: "0" }}
+            onKeyDown={handleKeyDown}
+            tabIndex={0}
+          ></canvas>
+
+          {/* canvas for cursor */}
+          <canvas
+            ref={canvasCursorRef}
+            width={width}
+            height={height}
+            style={{ position: "absolute", top: "0", left: "0", zIndex: 1 }}
+            onKeyDown={handleKeyDown}
+            tabIndex={0}
+          ></canvas>
+          </div>
+        </div>
+        <div className="scroller" style={{
+              display: "flex",
+              flexDirection: "column",
+              position: "relative",
+              marginBottom: "15px"
+        }}>
+          <span style={{
+              position: "absolute",
+              top: `calc(${(cursorLine +1)/lineMap.length * 100}%)`,
+              height: `15px`,
+              background: "pink",
+              border: "1px solid blue"
+          }}>{((cursorLine +1)/lineMap.length * 100).toFixed(0)}%</span>
+        </div>
+      </div>
+      <div className="info-container"
           style={{
             padding: "10px",
             border: "1px solid black",
@@ -224,7 +247,6 @@ return Math.floor(height / lineHeight)
           <p>
             Scroll Line: {scrollLine + 1} / {lineMap.length}
           </p>
-        </div>
       </div>
     </div>
   );
