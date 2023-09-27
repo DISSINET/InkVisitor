6ff221c3e2081cc247449a007c2c788c059e8da1
Petr Hanak <petr.nahak@gmail.com>
2023-09-27 09:12:53 +0100

    fix apply template dropdown in editor

--- packages/client/src/pages/MainPage/containers/StatementEditorBox/StatementEditor/StatementEditor.tsx
+++ packages/client/src/pages/MainPage/containers/StatementEditorBox/StatementEditor/StatementEditor.tsx
@@ -655,8 +655,8 @@ export const StatementEditor: React.FC<StatementEditor> = ({
                   width="full"
                   options={templateOptions}
                   value={templateOptions[0]}
-                  onChange={(templateToApply: any) => {
-                    handleAskForTemplateApply(templateToApply);
+                  onChange={(templateToApply) => {
+                    handleAskForTemplateApply(templateToApply[0]);
                   }}
                 />
               </StyledEditorContentRowValue>
