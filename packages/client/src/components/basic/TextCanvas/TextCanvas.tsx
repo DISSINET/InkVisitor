ff0e6c566e1da91e6b7ad353c94004edded76aa3
adammertel <12932677+adammertel@users.noreply.github.com>
2023-10-22 14:31:35 +0200

    Handle ctrl button keyPress in one line

--- packages/client/src/components/basic/TextCanvas/TextCanvas.tsx
+++ packages/client/src/components/basic/TextCanvas/TextCanvas.tsx
@@ -8,6 +8,12 @@ interface TextCanvasProps {
 }
 interface ILine {
   text: string; // textual content of the particular line
+  words: IWord[];
+}
+interface IWord {
+  text: string; // actual word
+  iFrom: number; // where the word starts
+  iTo: number; // where the word ends
 }
 
 const TextCanvas: React.FC<TextCanvasProps> = ({ text, width, height }) => {
@@ -29,6 +35,7 @@ const TextCanvas: React.FC<TextCanvasProps> = ({ text, width, height }) => {
   const numberOfLines = useMemo<number>(() => {
     return Math.floor(height / lineHeight);
   }, [height, lineHeight]);
+
   /**
    * the first and last line displayed in the canvas
    */
@@ -72,12 +79,28 @@ const TextCanvas: React.FC<TextCanvasProps> = ({ text, width, height }) => {
             splitAt--;
           }
 
-          // If no space or punctuation is found, just split at current position
+          // If no space or punctuation is found, just split at the current position
           if (splitAt === lineStart) {
             splitAt = charI;
           }
 
-          lines.push({ text: text.slice(lineStart, splitAt) });
+          const lineText = text.slice(lineStart, splitAt);
+          const words: IWord[] = [];
+
+          // Split the lineText into words
+          const wordMatches = lineText.match(/\b\w+\b/g);
+
+          if (wordMatches) {
+            let wordStart = 0;
+            wordMatches.forEach((word) => {
+              const wordIndex = lineText.indexOf(word, wordStart);
+              const wordEnd = wordIndex + word.length;
+              words.push({ text: word, iFrom: wordIndex, iTo: wordEnd });
+              wordStart = wordEnd;
+            });
+          }
+
+          lines.push({ text: lineText, words });
           lineStart = splitAt + 1;
         }
       }
@@ -85,7 +108,7 @@ const TextCanvas: React.FC<TextCanvasProps> = ({ text, width, height }) => {
 
     // Add any remaining text
     if (lineStart < text.length) {
-      lines.push({ text: text.slice(lineStart) });
+      lines.push({ text: text.slice(lineStart), words: [] });
     }
 
     const time2 = new Date();
@@ -96,6 +119,49 @@ const TextCanvas: React.FC<TextCanvasProps> = ({ text, width, height }) => {
     );
     return lines;
   }, [text, charWidth]);
+
+  const lineAtCursor = useMemo<ILine>(() => {
+    return lineMap[cursorLine];
+  }, [cursorLine, lineMap]);
+
+  // actual word at the cursor position
+  const cursorWord = useMemo<IWord>(() => {
+    return (
+      lineAtCursor.words.find((word) => {
+        return word.iFrom <= cursorChar && word.iTo >= cursorChar;
+      }) ?? lineAtCursor.words[0]
+    );
+  }, [cursorChar, lineAtCursor, lineMap]);
+
+  // next word at the cursor position
+  const cursorWordNext = useMemo<IWord | undefined>(() => {
+    let foundWord: IWord | undefined = undefined;
+    lineAtCursor.words.forEach((word, wi) => {
+      const wordNext = lineAtCursor.words[wi + 1];
+      if (wordNext) {
+        if (word.iFrom <= cursorChar && word.iTo >= cursorChar) {
+          foundWord = wordNext;
+        }
+      }
+    });
+
+    return foundWord;
+  }, [cursorChar, lineAtCursor, lineMap]);
+
+  // previous word at the cursor position
+  const cursorWordPrevious = useMemo<IWord | undefined>(() => {
+    let foundWord: IWord | undefined = undefined;
+    lineAtCursor.words.forEach((word, wi) => {
+      const wordPrev = lineAtCursor.words[wi - 1];
+      if (wordPrev) {
+        if (word.iFrom <= cursorChar && word.iTo >= cursorChar) {
+          foundWord = wordPrev;
+        }
+      }
+    });
+
+    return foundWord;
+  }, [cursorChar, lineAtCursor, lineMap]);
 
   useEffect(() => {
     const canvas = canvasTextRef.current;
@@ -146,7 +212,10 @@ const TextCanvas: React.FC<TextCanvasProps> = ({ text, width, height }) => {
   const handleKeyDown = (e: React.KeyboardEvent<HTMLCanvasElement>) => {
     e.preventDefault();
 
-    const pageJump = 50
+    // Check if the Ctrl key is also pressed
+    const ctrlKey = e.ctrlKey || e.metaKey;
+
+    const pageJump = 50;
     switch (e.key) {
       case "ArrowUp":
         setCursorLine(cursorLine > 0 ? cursorLine - 1 : 0);
@@ -159,7 +228,14 @@ const TextCanvas: React.FC<TextCanvasProps> = ({ text, width, height }) => {
         break;
 
       case "ArrowLeft":
-        if (cursorChar > 0) {
+        if (ctrlKey) {
+          // Ctrl + Arrow Left: Move to the left by a whole word
+          if (cursorChar === cursorWord.iFrom && cursorWordPrevious) {
+            setCursorChar(cursorWordPrevious.iFrom);
+          } else {
+            setCursorChar(cursorWord.iFrom);
+          }
+        } else if (cursorChar > 0) {
           setCursorChar(cursorChar - 1);
         } else if (cursorLine > 0) {
           setCursorLine(cursorLine - 1);
@@ -167,20 +243,15 @@ const TextCanvas: React.FC<TextCanvasProps> = ({ text, width, height }) => {
         }
         break;
 
-      // page up + down
-      case "PageDown":
-        setCursorLine(
-          cursorLine > pageJump ? cursorLine - pageJump : 0
-        );
-        break;
-      case "PageDown":
-        setCursorLine(
-          cursorLine < lineMap.length - pageJump ? cursorLine + pageJump : lineMap.length
-        );
-        break;
-
       case "ArrowRight":
-        if (cursorChar < lineMap[cursorLine].text.length) {
+        if (ctrlKey) {
+          // Ctrl + Arrow Right: Move to the right by a whole word
+          if (cursorChar === cursorWord.iTo && cursorWordNext) {
+            setCursorChar(cursorWordNext.iTo);
+          } else {
+            setCursorChar(cursorWord.iTo);
+          }
+        } else if (cursorChar < lineMap[cursorLine].text.length) {
           setCursorChar(cursorChar + 1);
         } else if (cursorLine < lineMap.length - 1) {
           setCursorLine(cursorLine + 1);
@@ -188,76 +259,105 @@ const TextCanvas: React.FC<TextCanvasProps> = ({ text, width, height }) => {
         }
         break;
 
+      // page up + down
+      case "PageDown":
+        setCursorLine(cursorLine > pageJump ? cursorLine - pageJump : 0);
+        break;
+      case "PageDown":
+        setCursorLine(
+          cursorLine < lineMap.length - pageJump
+            ? cursorLine + pageJump
+            : lineMap.length
+        );
+        break;
+
       default:
         // writing to text
-        console.log(e.key)
+        console.log(e.key);
         break;
     }
   };
 
   return (
     <div style={{ padding: "20px" }}>
-      <div className="textarea" style={{    display: "flex", flexDirection: "row"}}>
-        <div className="canvasses-wrapper" style={{
-            border: "1px solid black",
-            padding: "2px"
-        }}>
+      <div
+        className="textarea"
+        style={{ display: "flex", flexDirection: "row" }}
+      >
         <div
+          className="canvasses-wrapper"
           style={{
-            position: "relative",
-            width: `${width}px`,
-            height: `${height}px`,
+            border: "1px solid black",
+            padding: "2px",
           }}
         >
-          {/* canvas for text */}
-           <canvas
-            ref={canvasTextRef}
-            width={width}
-            height={height}
-            style={{ position: "absolute", top: "0", left: "0" }}
-            onKeyDown={handleKeyDown}
-            tabIndex={0}
-          ></canvas>
+          <div
+            style={{
+              position: "relative",
+              width: `${width}px`,
+              height: `${height}px`,
+            }}
+          >
+            {/* canvas for text */}
+            <canvas
+              ref={canvasTextRef}
+              width={width}
+              height={height}
+              style={{ position: "absolute", top: "0", left: "0" }}
+              onKeyDown={handleKeyDown}
+              tabIndex={0}
+            ></canvas>
 
-          {/* canvas for cursor */}
-          <canvas
-            ref={canvasCursorRef}
-            width={width}
-            height={height}
-            style={{ position: "absolute", top: "0", left: "0", zIndex: 1 }}
-            onKeyDown={handleKeyDown}
-            tabIndex={0}
-          ></canvas>
+            {/* canvas for cursor */}
+            <canvas
+              ref={canvasCursorRef}
+              width={width}
+              height={height}
+              style={{ position: "absolute", top: "0", left: "0", zIndex: 1 }}
+              onKeyDown={handleKeyDown}
+              tabIndex={0}
+            ></canvas>
           </div>
         </div>
-        <div className="scroller" style={{
-              display: "flex",
-              flexDirection: "column",
-              position: "relative",
-              marginBottom: "15px"
-        }}>
-          <span style={{
-              position: "absolute",
-              top: `calc(${(cursorLine +1)/lineMap.length * 100}%)`,
-              height: `15px`,
-              background: "pink",
-              border: "1px solid blue"
-          }}>{((cursorLine +1)/lineMap.length * 100).toFixed(0)}%</span>
-        </div>
-      </div>
-      <div className="info-container"
+        <div
+          className="scroller"
           style={{
-            padding: "10px",
-            border: "1px solid black",
-            marginTop: "10px",
+            display: "flex",
+            flexDirection: "column",
+            position: "relative",
+            marginBottom: "15px",
           }}
         >
-          <p>Cursor Line: {cursorLine + 1}</p>{" "}
-          {/* +1 to make it 1-based instead of 0-based */}
-          <p>Cursor Char: {cursorChar + 1}</p>
-          <p>
-            Scroll Line: {scrollLine + 1} / {lineMap.length}
-          </p>
+          <span
+            style={{
+              position: "absolute",
+              top: `calc(${((cursorLine + 1) / lineMap.length) * 100}%)`,
+              height: `15px`,
+              background: "pink",
+              border: "1px solid blue",
+            }}
+          >
+            {(((cursorLine + 1) / lineMap.length) * 100).toFixed(0)}%
+          </span>
+        </div>
+      </div>
+      <div
+        className="info-container"
+        style={{
+          padding: "10px",
+          border: "1px solid black",
+          marginTop: "10px",
+        }}
+      >
+        <p>Cursor Line: {cursorLine}</p>{" "}
+        {/* +1 to make it 1-based instead of 0-based */}
+        <p>Cursor Char: {cursorChar}</p>
+        <p>
+          Scroll Line: {scrollLine} / {lineMap.length}
+        </p>
+        <p>
+          {`Active word: ${cursorWord.text} (${cursorWord.iFrom} - ${cursorWord.iTo}) `}{" "}
+        </p>
       </div>
     </div>
   );
