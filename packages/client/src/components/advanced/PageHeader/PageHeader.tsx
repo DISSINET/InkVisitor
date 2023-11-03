ee8ecbb02c9b2df3592d5d384a782981c4bb6134
Petr Hanak <petr.nahak@gmail.com>
2023-11-03 11:26:25 +0000

    simplify ping color condition

--- packages/client/src/components/advanced/PageHeader/PageHeader.tsx
+++ packages/client/src/components/advanced/PageHeader/PageHeader.tsx
@@ -118,8 +118,7 @@ export const LeftHeader: React.FC<LeftHeader> = React.memo(
                 color="white"
               />
             )}
-            {ping === -2 && <StyledPingColor pingColor={pingColor} />}
-            {ping >= -1 && <StyledPingColor pingColor={pingColor} />}
+            {ping >= -2 && <StyledPingColor pingColor={pingColor} />}
             {ping >= 0 && <StyledPingText>{ping}ms</StyledPingText>}
           </StyledFlexRow>
         </StyledFlexColumn>
