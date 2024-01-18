3c932746299ec308ddf5148d07ca5aa3248eec4a
Petr Hanak <petr.nahak@gmail.com>
2024-01-18 22:12:25 +0000

    improve password validation handling, handle password errors

--- packages/client/src/pages/PasswordReset/PasswordResetPage.tsx
+++ packages/client/src/pages/PasswordReset/PasswordResetPage.tsx
@@ -20,6 +20,9 @@ import {
 } from "./PasswordResetPageStyles";
 import { TbLockExclamation, TbLockPlus, TbMailFilled } from "react-icons/tb";
 
+const UNSAFE_PASSWORD_ERROR = "The entered password is not safe.";
+const PASSWORDS_DONT_MATCH_ERROR = "Passwords do not match.";
+
 interface PasswordResetPage {}
 export const PasswordResetPage: React.FC<PasswordResetPage> = ({}) => {
   const navigate = useNavigate();
@@ -43,34 +46,29 @@ export const PasswordResetPage: React.FC<PasswordResetPage> = ({}) => {
     if (password.length < 12) {
       return false;
     }
-
     // Check if the password contains at least one uppercase letter
     if (!/[A-Z]/.test(password)) {
       return false;
     }
-
     // Check if the password contains at least one lowercase letter
     if (!/[a-z]/.test(password)) {
       return false;
     }
-
     // Check if the password contains at least one digit
     if (!/\d/.test(password)) {
       return false;
     }
-
     // Check if the password contains at least one symbol
     if (!/[!@#$%^&*()_+{}\[\]:;<>,.?/~\\-]/.test(password)) {
       return false;
     }
-
     // If all conditions are met, the password is considered safe
     return true;
   }
 
   useEffect(() => {
     if (password.length > 0 && !isSafePassword(password)) {
-      setError("The entered password is not safe.");
+      setError(UNSAFE_PASSWORD_ERROR);
     } else {
       setError(false);
     }
@@ -126,14 +124,19 @@ export const PasswordResetPage: React.FC<PasswordResetPage> = ({}) => {
           {error !== false && <StyledErrorText>{error}</StyledErrorText>}
           <StyledButtonWrap>
             <Button
+              disabled={
+                error === UNSAFE_PASSWORD_ERROR || password.length === 0
+              }
               icon={<FaKey />}
               label="Reset Password"
               color="success"
               onClick={() => {
-                if (password.length > 0 || passwordRepeat.length > 0) {
-                  handleReset();
-                } else {
+                if (password.length === 0 || passwordRepeat.length === 0) {
                   toast.info("Fill both fields");
+                } else if (password !== passwordRepeat) {
+                  setError(PASSWORDS_DONT_MATCH_ERROR);
+                } else {
+                  handleReset();
                 }
               }}
             />
