337f35650898e6a6ebf5eb13a4ee14003b16e913
adammertel <12932677+adammertel@users.noreply.github.com>
2024-03-14 11:41:48 +0100

    Refactor Cloud component and add originEntity prop

--- packages/client/src/components/basic/Cloud/Cloud.tsx
+++ packages/client/src/components/basic/Cloud/Cloud.tsx
@@ -2,12 +2,18 @@ import { Button } from "components";
 import React, { ReactElement } from "react";
 import { FaUnlink } from "react-icons/fa";
 import { StyledButtonWrap, StyledCloud, StyledCloudWrap } from "./CloudStyles";
+import { IEntity } from "@shared/types";
 
 interface Cloud {
   children: ReactElement;
   onUnlink: () => void;
+  originEntity: IEntity | undefined;
 }
-export const Cloud: React.FC<Cloud> = ({ children, onUnlink }) => {
+export const Cloud: React.FC<Cloud> = ({
+  children,
+  onUnlink,
+  originEntity,
+}) => {
   return (
     <StyledCloudWrap>
       <StyledCloud>{children}</StyledCloud>
@@ -15,7 +21,9 @@ export const Cloud: React.FC<Cloud> = ({ children, onUnlink }) => {
         <Button
           color="plain"
           inverted
-          tooltipLabel="unlink from cloud"
+          tooltipLabel={`unlink ${
+            originEntity?.label ?? "entity"
+          } from the cloud`}
           icon={<FaUnlink />}
           onClick={onUnlink}
         />
