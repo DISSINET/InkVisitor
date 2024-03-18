4576563d4a5f6b589389619f703e9dcadda8e335
adammertel <12932677+adammertel@users.noreply.github.com>
2024-03-18 12:10:09 +0100

    Create campaign interface

--- packages/shared/types/territory.ts
+++ packages/shared/types/territory.ts
@@ -8,9 +8,28 @@ export interface ITerritory extends IEntity {
 
 export interface ITerritoryData {
   parent: IParentTerritory | false;
+  campaign?: ITerritoryCampaign;
 }
 
 export interface IParentTerritory {
   territoryId: string; // '' in case of root
   order: number;
+}
+
+export interface ITerritoryCampaign {
+  project: string;
+  guidelinesVersion: string;
+  guidelinesURL: string;
+  variant: ECASTEMOVariant;
+  description: string;
+  startDate: string; // V class entity
+  endDate: string; // V class entity
+}
+
+export enum ECASTEMOVariant {
+  SumCASTEMO = "SumCASTEMO",
+  NoCASTEMO = "NoCASTEMO",
+  SelCASTEMO = "SelCASTEMO",
+  FullCASTEMO = "FullCASTEMO",
+  MGSTEMO = "MGSTEMO",
 }
