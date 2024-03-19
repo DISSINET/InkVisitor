e3b4789bba5472c89172e612093acb7190c5c280
adammertel <12932677+adammertel@users.noreply.github.com>
2024-03-19 15:16:51 +0100

    Add protocols to territory model

--- packages/shared/types/territory.ts
+++ packages/shared/types/territory.ts
@@ -8,9 +8,28 @@ export interface ITerritory extends IEntity {
 
 export interface ITerritoryData {
   parent: IParentTerritory | false;
+  protocols?: ITerritoryProtocol[];
 }
 
 export interface IParentTerritory {
   territoryId: string; // '' in case of root
   order: number;
+}
+
+export interface ITerritoryProtocol {
+  entityClasses: EntityEnums.Class[];
+  classifications: string[];
+  tieType: EProtocolTieType;
+  propType?: string[]; // only in case of MetaProp and InStatementProp tie
+  allowedClasses?: EntityEnums.Class[];
+  allowedEntities?: string[];
+  detail: string;
+}
+
+export enum EProtocolTieType {
+  MetaProp = "MetaProp",
+  InStatementProp = "InStatementProp",
+  CLARelation = "CLARelation",
+  InStatementCLARelation = "InStatementCLARelation",
+  Reference = "Reference",
 }
