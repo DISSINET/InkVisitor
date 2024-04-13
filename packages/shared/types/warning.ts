9ac09828e821af8a94f62410543e825bdd16f658
adammertel <12932677+adammertel@users.noreply.github.com>
2024-04-13 21:36:05 +0200

    Extend warning dictionaries

--- packages/shared/types/warning.ts
+++ packages/shared/types/warning.ts
@@ -1,9 +1,11 @@
 import { WarningTypeEnums } from "../enums";
+import { ITerritoryValidation } from "./territory";
 
 export interface IWarning {
   type: WarningTypeEnums;
   position?: IWarningPosition;
   origin: string;
+  validation?: ITerritoryValidation;
 }
 
 export interface IWarningPosition {
