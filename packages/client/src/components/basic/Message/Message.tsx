83be89120a06691c54b0eca987d6d960e395c963
adammertel <12932677+adammertel@users.noreply.github.com>
2024-04-15 09:29:58 +0200

    Add message texts for T-based validation warnings

--- packages/client/src/components/basic/Message/Message.tsx
+++ packages/client/src/components/basic/Message/Message.tsx
@@ -2,8 +2,8 @@ import { WarningTypeEnums } from "@shared/enums";
 import { IEntity, IWarning } from "@shared/types";
 import React, { useEffect, useState } from "react";
 import { TiWarningOutline } from "react-icons/ti";
-import { StyledMessage } from "./MessateStyles";
 import { getShortLabelByLetterCount } from "utils/utils";
+import { StyledMessage } from "./MessateStyles";
 
 interface Message {
   warning: IWarning;
@@ -19,16 +19,44 @@ export const Message: React.FC<Message> = ({ warning, entities }) => {
     pa: "Pseudo-Actant",
   };
 
-  const [entity, setEntity] = useState<IEntity | false>(false);
+  const [entity, setEntity] = useState<IEntity | undefined>(undefined);
 
   useEffect(() => {
     if (warning.position?.entityId && entities) {
       const entity = entities[warning.position.entityId];
       if (entity) {
         setEntity(entity);
+      } else {
+        setEntity(undefined);
       }
     }
   }, [warning, entities]);
+
+  function renderEntityTags(entityIds: (string | undefined)[]): JSX.Element {
+    return (
+      <>
+        {entityIds.map((eid) => {
+          if (eid) {
+            const entity = entities?.[eid];
+            return (
+              <span key={eid}>
+                <b>{`<${entity ? entity.label : eid}>`}</b>{" "}
+              </span>
+            );
+          } else {
+            return <></>;
+          }
+        })}
+      </>
+    );
+  }
+  function renderValidationLabel(warning: IWarning): JSX.Element {
+    if (warning.validation?.detail) {
+      return <span>[warning.validation?.detail]</span>;
+    } else {
+      return <></>;
+    }
+  }
 
   function getWarningMessage({ type, position }: IWarning): JSX.Element {
     const positionName = position?.subSection
@@ -139,19 +167,65 @@ export const Message: React.FC<Message> = ({ warning, entities }) => {
 
       // T-based validations
       case WarningTypeEnums.TVEP:
-        return <b>T-based validations: Property missing</b>;
+        return (
+          <span>
+            T-based validations: Entity{" "}
+            {renderEntityTags([warning?.position?.entityId])} should have a
+            property {renderValidationLabel(warning)}
+          </span>
+        );
       case WarningTypeEnums.TVEPT:
-        return <b>T-based validations: Property wrong type</b>;
+        return (
+          <span>
+            T-based validations: Entity{" "}
+            {renderEntityTags([warning?.position?.entityId])} is missing a
+            required property with{" "}
+            {renderEntityTags(warning.validation?.propType ?? [])}{" "}
+            {renderValidationLabel(warning)}
+          </span>
+        );
       case WarningTypeEnums.TVEPV:
-        return <b>T-based validations: Property wrong value</b>;
+        return (
+          <span>
+            T-based validations: Entity{" "}
+            {renderEntityTags([warning?.position?.entityId])} has a wrong
+            property value ([value-label]) assigned to [class-label]{" "}
+            {renderValidationLabel(warning)}
+          </span>
+        );
       case WarningTypeEnums.TVEC:
-        return <b>T-based validations: Classification missing</b>;
+        return (
+          <span>
+            T-based validations: Entity{" "}
+            {renderEntityTags([warning?.position?.entityId])} should have a
+            classification {renderValidationLabel(warning)}
+          </span>
+        );
       case WarningTypeEnums.TVECE:
-        return <b>T-based validations: Classification wrong entity</b>;
+        return (
+          <span>
+            T-based validations: Entity{" "}
+            {renderEntityTags([warning?.position?.entityId])} is not classified
+            to valid entity [classification-label]{" "}
+            {renderValidationLabel(warning)}
+          </span>
+        );
       case WarningTypeEnums.TVER:
-        return <b>T-based validations: Reference missing</b>;
+        return (
+          <span>
+            T-based validations: Entity{" "}
+            {renderEntityTags([warning?.position?.entityId])} should have a
+            reference {renderValidationLabel(warning)}
+          </span>
+        );
       case WarningTypeEnums.TVERE:
-        return <b>T-based validations: Reference wrong entity</b>;
+        return (
+          <span>
+            T-based validations: Entity{" "}
+            {renderEntityTags([warning?.position?.entityId])} is not referenced
+            to a valid entity {renderValidationLabel(warning)}
+          </span>
+        );
 
       default:
         return <></>;
