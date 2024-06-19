94c3757b53fa333f22eb24f6bf4d79f6f0bb1033
Ptrhnk <petr.nahak@gmail.com>
2024-06-19 15:41:49 +0100

    open S in editor btn to search results (#2249)

--- packages/client/src/pages/Main/containers/EntitySearchBox/EntitySearchResults/EntitySearchResults.tsx
+++ packages/client/src/pages/Main/containers/EntitySearchBox/EntitySearchResults/EntitySearchResults.tsx
@@ -9,6 +9,10 @@ import {
   StyledResultsAnimatedWrap,
   StyledRow,
 } from "./EntitySearchResultsStyles";
+import { Button } from "components";
+import { FaEdit } from "react-icons/fa";
+import { useSearchParams } from "hooks";
+import { EntityEnums } from "@shared/enums";
 
 interface EntitySearchResults {
   results?: IResponseEntity[];
@@ -52,10 +56,27 @@ interface Row {
 const Row: React.FC<Row> = ({ data, index, style }) => {
   const entity = data[index];
 
+  const { setStatementId } = useSearchParams();
+
   return (
     <StyledRow style={style}>
       <StyledResultItem>
-        <EntityTag entity={entity} tooltipPosition="left" fullWidth />
+        <EntityTag
+          entity={entity}
+          tooltipPosition="left"
+          fullWidth
+          button={
+            entity.class === EntityEnums.Class.Statement && (
+              <Button
+                tooltipLabel="open statement in editor"
+                color="plain"
+                inverted
+                icon={<FaEdit />}
+                onClick={() => setStatementId(entity.id)}
+              />
+            )
+          }
+        />
       </StyledResultItem>
     </StyledRow>
   );
