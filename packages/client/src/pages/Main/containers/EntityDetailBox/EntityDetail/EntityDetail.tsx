96ed867ae07957ecd3c9940e5a9dc45dd9ca1ece
Petr Hanak <petr.nahak@gmail.com>
2024-07-02 22:43:02 +0100

    implement UI

--- packages/client/src/pages/Main/containers/EntityDetailBox/EntityDetail/EntityDetail.tsx
+++ packages/client/src/pages/Main/containers/EntityDetailBox/EntityDetail/EntityDetail.tsx
@@ -42,6 +42,7 @@ import {
   StyledDetailSectionContentUsedIn,
   StyledDetailSectionEntityList,
   StyledDetailSectionHeader,
+  StyledDetailSectionHeading,
   StyledDetailWarnings,
   StyledDetailWrapper,
   StyledPropGroupWrap,
@@ -55,6 +56,7 @@ import { EntityDetailStatementPropsTable } from "./EntityDetailUsedInTable/Entit
 import { EntityDetailStatementsTable } from "./EntityDetailUsedInTable/EntityDetailStatementsTable/EntityDetailStatementsTable";
 import { EntityDetailValency } from "./EntityDetailValency/EntityDetailValency";
 import { EntityDetailValidationSection } from "./EntityDetailValidationSection/EntityDetailValidationSection";
+import { EntityDetailSectionButtons } from "./EntityDetailSectionButtons/EntityDetailSectionButtons";
 
 const allowedEntityChangeClasses = [
   EntityEnums.Class.Value,
@@ -582,6 +584,9 @@ export const EntityDetail: React.FC<EntityDetail> = ({
 
   const isInsideTemplate = entity?.isTemplate || false;
 
+  const [showBatchRemovePropSubmit, setShowBatchRemovePropSubmit] =
+    useState(false);
+
   return (
     <>
       {entity && (
@@ -718,8 +723,21 @@ export const EntityDetail: React.FC<EntityDetail> = ({
             {/* metaprops section */}
             <StyledDetailSection $metaSection>
               <StyledDetailSectionHeader>
-                Metaproperties
+                <StyledDetailSectionHeading>
+                  Metaproperties
+                </StyledDetailSectionHeading>
+                {userCanEdit && (
+                  <EntityDetailSectionButtons
+                    props={entity.props}
+                    entityId={entity.id}
+                    setShowSubmit={setShowBatchRemovePropSubmit}
+                    handleCopyFromEntity={(pickedEntity, replace) => {
+                      console.log(pickedEntity);
+                    }}
+                  />
+                )}
               </StyledDetailSectionHeader>
+
               <StyledDetailSectionContent>
                 <StyledPropGroupWrap>
                   <PropGroup
@@ -919,6 +937,18 @@ export const EntityDetail: React.FC<EntityDetail> = ({
         onCancel={() => setShowRemoveSubmit(false)}
         show={showRemoveSubmit}
         loading={deleteEntityMutation.isPending}
+      />
+      <Submit
+        title="Delete metaprops"
+        text="Do you really want to delete all metaprops from this entity?"
+        submitLabel="Delete"
+        onSubmit={() => {
+          // TODO: mutation for delete props
+          setShowBatchRemovePropSubmit(false);
+        }}
+        onCancel={() => setShowBatchRemovePropSubmit(false)}
+        show={showBatchRemovePropSubmit}
+        // loading={propsDeleteMutation.isPending}
       />
       <Submit
         title="Change entity type"
---
+++ packages/client/src/pages/Main/containers/EntityDetailBox/EntityDetail/EntityDetailSectionButtons/EntityDetailSectionButtons.tsx
@@ -0,0 +1,68 @@
+import { IEntity, IProp } from "@shared/types";
+import { Button, ButtonGroup } from "components";
+import { AttributeButtonGroup, EntitySuggester } from "components/advanced";
+import React, { useState } from "react";
+import { FaPlus, FaTrashAlt } from "react-icons/fa";
+import { TbReplace } from "react-icons/tb";
+import { StyledSectionButtonsBorder } from "./EntityDetailSectionButtonsStyles";
+import { classesAll } from "types";
+
+interface EntityDetailSectionButtons {
+  props: IProp[];
+  setShowSubmit: (value: React.SetStateAction<boolean>) => void;
+  entityId: string;
+  handleCopyFromEntity: (pickedEntity: IEntity, replace: boolean) => void;
+}
+export const EntityDetailSectionButtons: React.FC<
+  EntityDetailSectionButtons
+> = ({ props, setShowSubmit, entityId, handleCopyFromEntity }) => {
+  const [replaceSection, setReplaceSection] = useState(false);
+
+  return (
+    <>
+      <ButtonGroup
+        height={19}
+        style={{ marginLeft: "0.5rem", marginRight: "1rem" }}
+      >
+        <Button
+          disabled={!props.length}
+          icon={<FaTrashAlt />}
+          inverted
+          color="danger"
+          tooltipLabel={`remove all metaprops from entity`}
+          onClick={() => setShowSubmit(true)}
+        />
+        <StyledSectionButtonsBorder />
+        <AttributeButtonGroup
+          options={[
+            {
+              longValue: "append",
+              shortValue: "",
+              onClick: () => setReplaceSection(false),
+              selected: !replaceSection,
+              shortIcon: <FaPlus />,
+            },
+            {
+              longValue: "replace",
+              shortValue: "",
+              onClick: () => setReplaceSection(true),
+              selected: replaceSection,
+              shortIcon: <TbReplace />,
+            },
+          ]}
+        />
+      </ButtonGroup>
+      <EntitySuggester
+        categoryTypes={classesAll}
+        onPicked={(entity: IEntity) =>
+          handleCopyFromEntity(entity, replaceSection)
+        }
+        excludedActantIds={[entityId]}
+        disableTemplatesAccept
+        disableCreate
+        inputWidth={90}
+        placeholder="another Entity"
+      />
+    </>
+  );
+};
