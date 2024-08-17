89a06bd559b9f4bb2ef8e697e683c0c5eb6b95a9
Petr Hanak <petr.nahak@gmail.com>
2024-08-17 10:52:18 +0100

    fix basename redirect

--- packages/client/src/ensure-basename.ts
+++ packages/client/src/ensure-basename.ts
@@ -1,14 +1,18 @@
-if (process.env.ROOT_URL) {
-  if (!window.location.pathname.includes(process.env.ROOT_URL)) {
-    window.history.replaceState(
-      "",
-      "",
-      process.env.ROOT_URL +
-        window.location.pathname +
-        window.location.search +
-        window.location.hash
-    );
+export const ensureBasename = () => {
+  console.log("checking basename");
+
+  if (process.env.ROOT_URL) {
+    if (!window.location.pathname.includes(process.env.ROOT_URL)) {
+      window.history.replaceState(
+        "",
+        "",
+        process.env.ROOT_URL +
+          window.location.pathname +
+          window.location.search +
+          window.location.hash
+      );
+    }
+  } else {
+    console.log("ROOT_URL is not set");
   }
-} else {
-  console.log("ROOT_URL is not set");
-}
+};
