6d7b8c43fb02ca38860d28dcb6a6505d9de41748
Adam Mertel <12932677+adammertel@users.noreply.github.com>
2024-08-19 21:21:48 +0200

    983 create stats endpoint models (#2251)
    * Create stats models
    
    * Add activities to filters
    
    * fix correct stats req/resp data
    
    * add basic implementation for stats response, add audits index, add type field to Audit
    
    * update return format
    
    * add event type to audits in datasets + job
    
    ---------
    
    Co-authored-by: Ján Mertel <10438157+jancimertel@users.noreply.github.com>

--- packages/database/scripts/import/indexes.ts
+++ packages/database/scripts/import/indexes.ts
@@ -137,6 +137,7 @@ const entitiesIndexes: ((table: RTable) => any)[] = [
 
 const auditsIndexes: ((table: RTable) => any)[] = [
   (table: RTable) => table.indexCreate(DbEnums.Indexes.AuditEntityId),
+  (table: RTable) => table.indexCreate(DbEnums.Indexes.AuditDate),
 ];
 
 const relationsIndexes: ((table: RTable) => any)[] = [
