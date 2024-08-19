70db6dddd06dabdccd884260d64716aa723b8277
Petr Hanak <petr.nahak@gmail.com>
2024-08-19 19:34:51 +0100

    add panels and boxes

--- packages/client/src/pages/AdvancedSearch/AdvancedSearch.tsx
+++ packages/client/src/pages/AdvancedSearch/AdvancedSearch.tsx
@@ -1,6 +1,27 @@
+import { Box, Panel } from "components";
 import React from "react";
+import { useAppSelector } from "redux/hooks";
 
 interface AdvancedSearchPage {}
 export const AdvancedSearchPage: React.FC<AdvancedSearchPage> = ({}) => {
-  return <div>advanced search</div>;
+  const layoutWidth: number = useAppSelector(
+    (state) => state.layout.layoutWidth
+  );
+  const contentHeight: number = useAppSelector(
+    (state) => state.layout.contentHeight
+  );
+  return (
+    <>
+      <Panel width={layoutWidth / 3}>
+        <Box borderColor="white" height={contentHeight} label="Search">
+          {/* <MemoizedAdvancedSearchBox /> */}
+        </Box>
+      </Panel>
+      <Panel width={(layoutWidth / 3) * 2}>
+        <Box borderColor="white" height={contentHeight} label="Explorer">
+          {/* <MemoizedExplorerBox /> */}
+        </Box>
+      </Panel>
+    </>
+  );
 };
