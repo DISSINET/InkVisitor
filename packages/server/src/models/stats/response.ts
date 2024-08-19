292f5552e65918922053e704e95eebaed9602a9c
Ján Mertel <10438157+jancimertel@users.noreply.github.com>
2024-08-19 18:55:51 +0200

    fix correct stats req/resp data

--- packages/server/src/models/stats/response.ts
+++ packages/server/src/models/stats/response.ts
@@ -1,10 +1,23 @@
 import { IResponseStats } from "@shared/types";
+import { Aggregation, TimeUnit } from "@shared/types/stats";
 import { IRequest } from "src/custom_typings/request";
+import { IRequestStats } from "@shared/types/request-stats";
 
 export class ResponseStats implements IResponseStats {
-  byType: { [key: string]: number } = {};
-  byEditor: { [key: string]: number } = {};
-  byTime: { [key: string]: number } = {};
+  fromDate: number;
+  toDate: number;
+  timeUnit: TimeUnit;
+  aggregateBy: Aggregation;
+  values: Record<string, Record<string, number>>;
+
+  constructor(request: IRequestStats) {
+    this.fromDate = request.fromDate;
+    this.toDate = request.toDate;
+    this.timeUnit = request.timeUnit;
+    this.aggregateBy = request.aggregateBy;
+
+    this.values = {};
+  }
 
   async prepare(req: IRequest) { }
 }
--- packages/server/src/modules/stats/index.ts
+++ packages/server/src/modules/stats/index.ts
@@ -3,11 +3,12 @@ import { Router } from "express";
 import { IResponseStats } from "@shared/types";
 import { ResponseStats } from "@models/stats/response";
 import { IRequest } from "src/custom_typings/request";
+import { IRequestStats } from "@shared/types/request-stats";
 
 export default Router().get(
   "/",
-  asyncRouteHandler<IResponseStats>(async (request: IRequest) => {
-    const resp = new ResponseStats();
+  asyncRouteHandler<IResponseStats>(async (request: IRequest<unknown, IRequestStats>) => {
+    const resp = new ResponseStats(request.body);
 
     await resp.prepare(request);
 
