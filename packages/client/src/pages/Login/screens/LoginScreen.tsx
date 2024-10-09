71e8d91d375f6b7c27c67528ef6a52234dc36d08
Adam Mertel <12932677+adammertel@users.noreply.github.com>
2024-10-09 20:07:40 +0200

    2291 the login button is disabled on the first render (#2372)
    * various attempts
    
    * Remove disabled from the login button
    
    * Clean imports
    
    * Remove ref
    
    ---------
    
    Co-authored-by: Petr Hanak <petr.nahak@gmail.com>

--- packages/client/src/pages/Login/screens/LoginScreen.tsx
+++ packages/client/src/pages/Login/screens/LoginScreen.tsx
@@ -1,5 +1,7 @@
+import { IErrorSignature, getErrorByCode } from "@shared/types/errors";
 import api from "api";
 import { Button, Input } from "components";
+import { StyledButtonWrap, StyledErrorText } from "pages/AuthModalSharedStyles";
 import React, { useState } from "react";
 import { FiLogIn } from "react-icons/fi";
 import { setUsername } from "redux/features/usernameSlice";
@@ -9,8 +11,6 @@ import {
   StyledInputRow,
   StyledTbMailFilled,
 } from "./LoginScreensStyles";
-import { IErrorSignature, getErrorByCode } from "@shared/types/errors";
-import { StyledButtonWrap, StyledErrorText } from "pages/AuthModalSharedStyles";
 
 interface LoginScreen {
   usernameLocal: string;
@@ -45,36 +45,39 @@ export const LoginScreen: React.FC<LoginScreen> = ({
 
   return (
     <>
-      <StyledInputRow>
-        <StyledTbMailFilled size={14} $isError={error !== false} />
-        <Input
-          width={200}
-          placeholder="email or username"
-          onChangeFn={(text: string) => setUsernameLocal(text)}
-          value={usernameLocal}
-          changeOnType
-          autoFocus
-          borderColor={error !== false ? "danger" : undefined}
-        />
-      </StyledInputRow>
-      <StyledInputRow>
-        <StyledFaLock size={14} $isError={error !== false} />
-        <Input
-          width={200}
-          type="password"
-          placeholder="password"
-          onChangeFn={(text: string) => setPassword(text)}
-          value={password}
-          changeOnType
-          borderColor={error !== false ? "danger" : undefined}
-        />
-      </StyledInputRow>
+      <form>
+        <StyledInputRow>
+          <StyledTbMailFilled size={14} $isError={error !== false} />
+          <Input
+            width={200}
+            autocomplete="username"
+            placeholder="email or username"
+            onChangeFn={(text: string) => setUsernameLocal(text)}
+            value={usernameLocal}
+            changeOnType
+            autoFocus
+            borderColor={error !== false ? "danger" : undefined}
+          />
+        </StyledInputRow>
+        <StyledInputRow>
+          <StyledFaLock size={14} $isError={error !== false} />
+          <Input
+            width={200}
+            autocomplete="current-password"
+            type="password"
+            placeholder="password"
+            onChangeFn={(text: string) => setPassword(text)}
+            value={password}
+            changeOnType
+            borderColor={error !== false ? "danger" : undefined}
+          />
+        </StyledInputRow>
+      </form>
 
       {error !== false && <StyledErrorText>{error}</StyledErrorText>}
 
       <StyledButtonWrap>
         <Button
-          disabled={usernameLocal.length === 0 || password.length === 0}
           icon={<FiLogIn />}
           label="Log In"
           color="success"
