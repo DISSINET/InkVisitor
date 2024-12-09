16d7422f937414b0afb8eb10b47445e3cb60bbaa
adammertel <12932677+adammertel@users.noreply.github.com>
2024-12-09 20:08:22 +0100

    Add ITerritoryValidationNode into entity response interface

--- packages/shared/types/response-detail.ts
+++ packages/shared/types/response-detail.ts
@@ -2,13 +2,14 @@
  * Very extensive object showing all the details about one actant
  */
 
-import { IEntity, Relation, IResponseEntity, IStatement, IWarning } from ".";
+import { IEntity, IResponseEntity, IStatement, IWarning, Relation } from ".";
 import { EntityEnums } from "../enums";
 import { IDocumentMeta } from "./document";
 import {
   IStatementClassification,
   IStatementIdentification,
 } from "./statement";
+import { ITerritoryValidation } from "./territory";
 
 export interface IResponseDetail extends IResponseEntity {
   entities: Record<string, IEntity>; // all entities from IStatement and entityIds...
@@ -16,15 +17,32 @@ export interface IResponseDetail extends IResponseEntity {
   usedInStatementProps: IResponseUsedInStatementProps[]; // all statements, where the detail id is used in props
   usedInMetaProps: IResponseUsedInMetaProp[]; // all entities, where the detail id is used in props (entity.props[])
 
+  usedInDocuments: IResponseUsedInDocument[]; // all documents, where the detail id is used
+
   usedInStatementIdentifications: IResponseUsedInStatementIdentification[]; // statement.data.actants[].identifications + from usedInStatements field if actant.entityId = detailId
   usedInStatementClassifications: IResponseUsedInStatementClassification[]; // statement.data.actants[].classifications + from usedInStatements field if actant.entityId = detailId
 
   usedAsTemplate?: string[];
-  usedInDocuments: IDocumentMeta[];
 
   relations: Relation.IUsedRelations;
 
   warnings: IWarning[];
+  tValidations?: ITerritoryValidationNode; // applicable only to T entities
+}
+
+export interface ITerritoryValidationNode {
+  territoryId: string;
+  validations: ITerritoryValidation[];
+  parent: ITerritoryValidationNode | null;
+}
+
+// model is reapeated for each anchor in each document,
+// so e.g., when the entity is used in 2 documents, and in one document it is used in 3 anchors, in the second is used in 1 anchor, model will be repeated 4 times
+export interface IResponseUsedInDocument {
+  document: IDocumentMeta;
+  anchorText: string; // content of the anchor
+  resourceId: string; // resource linked to the document
+  parentTerritoryId: string; // id of the closest territory anchor in the document, "" if no territory is found
 }
 
 export interface IResponseUsedInStatement<PositionEnum> {
