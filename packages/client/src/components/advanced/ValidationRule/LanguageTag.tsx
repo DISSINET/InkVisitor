a820d011238d04b1632a9c89d0261e6c82dad257
adammertel <12932677+adammertel@users.noreply.github.com>
2024-12-16 08:17:07 +0100

    Add tooltip to language tag

--- packages/client/src/components/advanced/ValidationRule/LanguageTag.tsx
+++ packages/client/src/components/advanced/ValidationRule/LanguageTag.tsx
@@ -1,35 +1,53 @@
 import { EntityEnums } from "@shared/enums";
 import { Button } from "components/basic/Button/Button";
+import { Tooltip } from "components/basic/Tooltip/Tooltip";
 
-import React from "react";
+import React, { useState } from "react";
 import { FaX } from "react-icons/fa6";
 import { styled } from "styled-components";
 import theme from "Theme/theme";
 
 interface LanguageTagProps {
   languageValue: EntityEnums.Language;
+  languageTooltip?: string;
   onUnlink?: () => void;
 }
 export const LanguageTag: React.FC<LanguageTagProps> = ({
   languageValue,
+  languageTooltip,
   onUnlink,
 }) => {
+  const [showTooltip, setShowTooltip] = useState(false);
+  const [referenceElement, setReferenceElement] =
+    useState<HTMLDivElement | null>(null);
+
   return (
-    <StyledLanguageTag>
-      {languageValue.toString()}
-      {onUnlink && (
-        <Button
-          key="d"
-          tooltipLabel={"remove language"}
-          icon={<FaX size={10} />}
-          color={"blue"}
-          noBorder
-          noBackground
-          inverted
-          onClick={onUnlink}
-        />
-      )}
-    </StyledLanguageTag>
+    <React.Fragment>
+      <StyledLanguageTag
+        ref={setReferenceElement}
+        onMouseEnter={() => setShowTooltip(true)}
+        onMouseLeave={() => setShowTooltip(false)}
+      >
+        {languageValue.toString()}
+        {onUnlink && (
+          <Button
+            key="d"
+            tooltipLabel={"remove language"}
+            icon={<FaX size={10} />}
+            color={"blue"}
+            noBorder
+            noBackground
+            inverted
+            onClick={onUnlink}
+          />
+        )}
+      </StyledLanguageTag>
+      <Tooltip
+        label={languageTooltip}
+        visible={showTooltip}
+        referenceElement={referenceElement}
+      />
+    </React.Fragment>
   );
 };
 
