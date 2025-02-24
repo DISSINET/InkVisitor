cb273bd9fd58c55b5c1e54a79b2443c9b10fed89
adammertel <12932677+adammertel@users.noreply.github.com>
2025-02-24 11:32:48 +0100

    Add tooltip to document tag

--- packages/client/src/components/basic/DocumentTitle/DocumentTitle.tsx
+++ packages/client/src/components/basic/DocumentTitle/DocumentTitle.tsx
@@ -1,19 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import { TiDocumentText } from "react-icons/ti";
+import { Tooltip } from "../Tooltip/Tooltip";
 import { StyledDocumentTag, StyledDocumentTitle } from "./DocumentTitleStyles";
 
 interface DocumentTitle {
   title?: string;
 }
 export const DocumentTitle: React.FC<DocumentTitle> = ({ title = "" }) => {
+  const [referenceElement, setReferenceElement] =
+    useState<HTMLDivElement | null>(null);
+
+  const [isTooltipOpen, setIsTooltipOpen] = React.useState(false);
   return (
-    <>
-      <StyledDocumentTag>
+    <React.Fragment>
+      <StyledDocumentTag
+        ref={setReferenceElement}
+        onMouseEnter={() => setIsTooltipOpen(true)}
+        onMouseLeave={() => setIsTooltipOpen(false)}
+      >
         <TiDocumentText style={{ marginRight: "0.2rem", flexShrink: "0" }} />
         <div style={{ display: "grid" }}>
           <StyledDocumentTitle>{title}</StyledDocumentTitle>
         </div>
       </StyledDocumentTag>
-    </>
+      <Tooltip
+        label={title}
+        visible={isTooltipOpen}
+        referenceElement={referenceElement}
+      />
+    </React.Fragment>
   );
 };
