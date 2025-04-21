f4aadd2f7e0f1febe4e35afb53ac7c168afed5ea
adammertel <12932677+adammertel@users.noreply.github.com>
2025-04-21 23:26:46 +0200

    Make document titles in various sizes

--- packages/client/src/components/basic/DocumentTitle/DocumentTitleStyles.tsx
+++ packages/client/src/components/basic/DocumentTitle/DocumentTitleStyles.tsx
@@ -1,13 +1,23 @@
 import styled from "styled-components";
 
-export const StyledDocumentTag = styled.div`
+export const StyledDocumentTag = styled.div<{ $size: "sm" | "md" | "lg" }>`
   display: inline-flex;
   margin: 0 0.6rem;
   background-color: ${({ theme }) => theme.color["blue"][400]};
-  padding: ${({ theme }) => theme.space[1] + " " + theme.space[3]};
+  padding: ${({ theme, $size }) =>
+    $size === "sm"
+      ? theme.space[1] + " " + theme.space[2]
+      : $size === "md"
+      ? theme.space[1] + " " + theme.space[3]
+      : theme.space[1] + " " + theme.space[4]};
   border-radius: ${({ theme }) => theme.borderRadius["md"]};
   color: white;
-  font-size: small;
+  font-size: ${({ theme, $size }) =>
+    $size === "sm"
+      ? theme.fontSize["2xs"]
+      : $size === "md"
+      ? theme.fontSize["xs"]
+      : theme.fontSize["sm"]};
   align-items: center;
 `;
 
