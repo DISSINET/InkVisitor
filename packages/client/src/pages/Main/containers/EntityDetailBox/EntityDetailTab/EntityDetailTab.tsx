0c6a0c53099fefafef7a1a1c342d1a26e24bc074
Ptrhnk <petr.nahak@gmail.com>
2025-04-22 17:55:57 +0100

    Refactor EntityDetailTab ref to explicitly define ref type

--- packages/client/src/pages/Main/containers/EntityDetailBox/EntityDetailTab/EntityDetailTab.tsx
+++ packages/client/src/pages/Main/containers/EntityDetailBox/EntityDetailTab/EntityDetailTab.tsx
@@ -55,7 +55,7 @@ export const EntityDetailTab: React.FC<EntityDetailTab> = ({
     middleware: [offset({ mainAxis: -14 })],
   });
 
-  const ref = useRef<HTMLDivElement>(null);
+  const ref = useRef<HTMLDivElement>(null) as React.RefObject<HTMLDivElement>;
 
   const [, drop] = useDrop<DragItem>({
     accept: ItemTypes.DETAIL_TAB,
