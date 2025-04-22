1425fc143321fb722bbac771a45c9c5c2357e39d
Ptrhnk <petr.nahak@gmail.com>
2025-04-22 21:28:26 +0100

    Remove margin and padding from StyledTextWrapper in AbbreviatedTextWithTooltipStyles

--- packages/client/src/components/advanced/AbbreviatedTextWithTooltip/AbbreviatedTextWithTooltipStyles.tsx
+++ packages/client/src/components/advanced/AbbreviatedTextWithTooltip/AbbreviatedTextWithTooltipStyles.tsx
@@ -2,8 +2,6 @@ import styled from "styled-components";
 
 export const StyledTextWrapper = styled.div`
   display: inline-flex;
-  margin: 0 0.6rem;
-  padding: ${({ theme }) => theme.space[1] + " " + theme.space[3]};
   font-size: small;
   align-items: center;
 `;
