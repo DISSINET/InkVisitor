61b01afa00802d9095d2f04a854e541528b80cd1
Ptrhnk <petr.nahak@gmail.com>
2025-05-09 20:48:04 +0100

    improve ordering color

--- packages/client/src/pages/Main/containers/StatementsListBox/StatementListTable/StatementListTableStyles.tsx
+++ packages/client/src/pages/Main/containers/StatementsListBox/StatementListTable/StatementListTableStyles.tsx
@@ -123,7 +123,7 @@ export const StyledOrderCorrection = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-  color: ${({ theme }) => theme.color["gray"]["500"]};
   font-size: ${({ theme }) => theme.fontSize["xs"]};
   width: 2rem;
+  color: ${({ theme }) => theme.color["gray"]["800"]};
 `;
