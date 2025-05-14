c040add510785ba1c59861d6bce6657402af117c
Ptrhnk <petr.nahak@gmail.com>
2025-05-14 15:40:26 +0100

    add requested languages and sort by alphabet (#2675)

--- packages/shared/enums/entities.ts
+++ packages/shared/enums/entities.ts
@@ -212,25 +212,30 @@ export namespace EntityEnums {
   // ISO 639-2 codes
   export enum Language {
     Empty = "",
-    Latin = "lat",
-    English = "eng",
-    MiddleEnglish = "enm",
-    Occitan = "oci",
-    OldOccitan = "pro",
+    Chinese = "chi",
     Czech = "ces",
-    Italian = "ita",
+    Dutch = "dut",
+    English = "eng",
     French = "fra",
     German = "deu",
-    Spanish = "spa",
     Hungarian = "hun",
+    Italian = "ita",
+    Japanese = "jpn",
+    Latin = "lat",
+    MiddleEnglish = "enm",
+    MiddleFrench = "frm",
+    MiddleHighGerman = "gmh",
+    Occitan = "oci",
+    OldChurchSlavonic = "chu", // same as the Church Slavic
+    OldEnglish = "ang",
+    OldFrench = "fro",
+    OldHighGerman = "goh",
+    OldItalian = "roa", // general code for Romance languages
+    OldNorse = "non",
+    OldOccitan = "pro",
     Polish = "pol",
     Portuguese = "por",
-    Dutch = "dut",
-    Chinese = "chi",
-    Japanese = "jpn",
-    OldChurchSlavonic = "chu",
-    OldFrench = "fro",
-    OldItalian = "roa",
+    Spanish = "spa",
   }
 
   export enum ResourceType {
