4c9cc8f55d955bd4478d9f44112c31770a3508ef
Ptrhnk <petr.nahak@gmail.com>
2025-06-06 23:38:10 +0100

    fix redux selector

--- packages/client/src/pages/Main/containers/EntityDetailBox/EntityDetail/EntityDetailUsedInTable/EntityDetailUsedInDocumentsTable/EntityDetailUsedInDocumentsTable.tsx
+++ packages/client/src/pages/Main/containers/EntityDetailBox/EntityDetail/EntityDetailUsedInTable/EntityDetailUsedInDocumentsTable/EntityDetailUsedInDocumentsTable.tsx
@@ -84,7 +84,7 @@ export const EntityDetailUsedInDocumentsTable: React.FC<
                       detailBoxState === DetailBoxState.FullHeight
                     ) {
                       // set more time to open statement list, annotator and/or find the territory
-                      timeout = 1000;
+                      timeout = 2000;
                     } else {
                       timeout = 100;
                     }
