6ff7e135a3f80ecb1cc678402ed8ea1c4655609c
jancimertel <10438157+jancimertel@users.noreply.github.com>
2025-06-13 09:26:08 +0200

    annotator: override default cursor-pointer color with required style

--- packages/annotator/src/lib/Cursor.ts
+++ packages/annotator/src/lib/Cursor.ts
@@ -224,7 +224,7 @@ export default class Cursor
       // in case there is no area selected, just drop a cursor at some
       this.drawLine(ctx, this.yLine, this.xLine, this.xLine, {
         ...drawingOptions,
-        color: "black",
+        color: this.style.color,
       });
     } else if (hStart && hEnd) {
       // selection active, iterate over displayed lines
