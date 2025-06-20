546ed41172aecd4b5f4a5dc1b813fe6cf33de2ba
Ptrhnk <petr.nahak@gmail.com>
2025-06-20 23:36:38 +0100

    clean up unused react import in vite

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -1,7 +1,5 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 import path from "path";
-import { loadEnv } from "vite";
+import { defineConfig, loadEnv } from "vite";
 
 export default defineConfig(({ mode }) => {
   // Load env file based on `mode` in the current working directory.
