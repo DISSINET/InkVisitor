8b6f69e02be01c8679b5163052c65e10befaff55
jancimertel <10438157+jancimertel@users.noreply.github.com>
2025-07-29 07:53:12 +0200

    fix: onSelectText callback should not ignore occurence change (same text)

--- packages/annotator/src/lib/Annotator.ts
+++ packages/annotator/src/lib/Annotator.ts
@@ -74,7 +74,7 @@ export class Annotator {
   annotatedPosition: SegmentPosition | null = null;
 
   // to control highlightChangeCb callback
-  lastSelectedText = "";
+  lastSelectedText?: Selected;
   ratio: number = 1;
 
   previousRenderViewportLineStart: number;
@@ -306,12 +306,12 @@ export class Annotator {
    * @param cb
    */
   onSelectText(cb: (selection: Selected) => void) {
-    this.lastSelectedText = "";
+    this.lastSelectedText = undefined;
     this.onSelectTextCb = (selection: Selected) => {
-      if (selection.text === this.lastSelectedText) {
+      if (JSON.stringify(this.lastSelectedText) === JSON.stringify(selection)) {
         return;
       }
-      this.lastSelectedText = selection.text;
+      this.lastSelectedText = selection;
       cb(selection);
     };
   }
@@ -877,6 +877,7 @@ export class Annotator {
     // Manually trigger onSelectText callback for search-based selections
     if (this.onSelectTextCb) {
       const [start, end] = this.cursor.getBounds();
+
       if (start && end) {
         const startSegment = this.text.getSegmentPosition(
           start.yLine,
@@ -899,7 +900,7 @@ export class Annotator {
   }
 
   onCopyText() {
-    window.navigator.clipboard.writeText(this.lastSelectedText);
+    window.navigator.clipboard.writeText(this.lastSelectedText?.text || "");
   }
 
   onPasteText() {
