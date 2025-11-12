a8fe712d5eea8b0b656b68796048dd717ccd3080
Ptrhnk <petr.nahak@gmail.com>
2025-11-12 16:22:44 +0000

    refactor(InputStyles): adjust positioning and styling of clearable input button

--- packages/client/src/components/basic/Input/InputStyles.tsx
+++ packages/client/src/components/basic/Input/InputStyles.tsx
@@ -165,7 +165,10 @@ interface StyledClearableInputButton {}
 export const StyledClearableInputButton = styled.div<StyledClearableInputButton>`
   position: absolute;
   right: 0.25rem;
-  top: 4px;
+  display: flex;
+  cursor: pointer;
+  top: 50%;
+  transform: translateY(-50%);
   svg {
     color: ${({ theme }) => theme.color["info"]};
     opacity: 0.7;
