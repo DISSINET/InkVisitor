d199f017666589edaec3272a7ff4c98e52b65a4d
Ptrhnk <petr.nahak@gmail.com>
2026-03-13 17:46:46 +0000

    update(annotator): tweak line height and underline margin

--- packages/annotator/src/lib/constants.ts
+++ packages/annotator/src/lib/constants.ts
@@ -10,7 +10,7 @@ export enum HighlightMode {
   FOCUS = "focus",
   UNDERLINE = "underline",
 }
-export const LINE_HEIGHT = 24;
+export const LINE_HEIGHT = 23;
 export const DEFAULT_FONT_SIZE = 13;
 export const DEFAULT_FONT = '"Roboto Mono", monospace';
 
@@ -18,4 +18,4 @@ export const DEFAULT_FONT = '"Roboto Mono", monospace';
 export const HIGHLIGHT_HEIGHT_RATIO = 0.75;
 
 /** Pixels to raise the underline above the bottom of the line band (UNDERLINE mode). Larger = smaller margin below text. */
-export const UNDERLINE_OFFSET_PX = 2.5;
+export const UNDERLINE_OFFSET_PX = 2;
