f0b7c6f92fd5eea9da69ce6dfee9050758678c40
Ptrhnk <petr.nahak@gmail.com>
2026-03-23 16:09:52 +0000

    update(EntitySearchAdvancedOptions): adjust timeout durations for pill menu and portal unmounting

--- packages/client/src/pages/Main/containers/EntitySearchBox/EntitySearchAdvancedOptions/EntitySearchAdvancedOptions.tsx
+++ packages/client/src/pages/Main/containers/EntitySearchBox/EntitySearchAdvancedOptions/EntitySearchAdvancedOptions.tsx
@@ -63,7 +63,7 @@ export const EntitySearchAdvancedOptions: React.FC<
     hideTimeoutRef.current = window.setTimeout(() => {
       setShowPillsMenu(false);
       hideTimeoutRef.current = null;
-    }, 400);
+    }, 500);
   };
 
   useEffect(() => {
@@ -72,7 +72,7 @@ export const EntitySearchAdvancedOptions: React.FC<
       unmountTimeoutRef.current = window.setTimeout(() => {
         setPortalMounted(false);
         unmountTimeoutRef.current = null;
-      }, 500);
+      }, 250);
       return () => {
         clearUnmountTimeout();
       };
