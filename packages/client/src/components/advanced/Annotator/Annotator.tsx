a9d6e86fefc1c950b51180be308c0ab30834bec9
jancimertel <10438157+jancimertel@users.noreply.github.com>
2026-03-23 21:49:37 +0100

    refactor(annotator): unify annotator modes across client/annotator

--- packages/client/src/components/advanced/Annotator/Annotator.tsx
+++ packages/client/src/components/advanced/Annotator/Annotator.tsx
@@ -18,7 +18,12 @@ import { FaPen, FaRegSave, FaTrash } from "react-icons/fa";
 import { toast } from "react-toastify";
 import { v4 as uuidv4 } from "uuid";
 
-import { Annotator, EditMode, Tag } from "@inkvisitor/annotator/src/lib";
+import {
+  Annotator,
+  EditMode,
+  Tag,
+  editModeDisplayLabel,
+} from "@inkvisitor/annotator/src/lib";
 import { EntityEnums, UserEnums } from "@shared/enums";
 import {
   IDocument,
@@ -973,7 +978,11 @@ export const TextAnnotator = ({
                   <FaPen size={11} />
                 </StyledDisplayModeButtonIconWrapper>
               }
-              label={!annotatorWidthTooNarrow ? EditMode.HIGHLIGHT : ""}
+              label={
+                !annotatorWidthTooNarrow
+                  ? editModeDisplayLabel[EditMode.HIGHLIGHT]
+                  : ""
+              }
               color="success"
               inverted={annotatorMode !== EditMode.HIGHLIGHT}
               onClick={() => {
@@ -992,7 +1001,11 @@ export const TextAnnotator = ({
                 </StyledDisplayModeButtonIconWrapper>
               }
               color="success"
-              label={!annotatorWidthTooNarrow ? "text edit" : ""}
+              label={
+                !annotatorWidthTooNarrow
+                  ? editModeDisplayLabel[EditMode.SEMI]
+                  : ""
+              }
               inverted={annotatorMode !== EditMode.SEMI}
               onClick={() => {
                 setAnnotatorMode(EditMode.SEMI);
@@ -1010,7 +1023,9 @@ export const TextAnnotator = ({
                 </StyledDisplayModeButtonIconWrapper>
               }
               color="success"
-              label={!annotatorWidthTooNarrow ? "XML" : ""}
+              label={
+                !annotatorWidthTooNarrow ? editModeDisplayLabel[EditMode.RAW] : ""
+              }
               inverted={annotatorMode !== EditMode.RAW}
               onClick={() => {
                 setAnnotatorMode(EditMode.RAW);
