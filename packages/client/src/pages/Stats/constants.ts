7bed6abc8adef6925a654732ba19b5451ce706f3
Ptrhnk <petr.nahak@gmail.com>
2026-05-25 12:18:35 +0100

    update(Stats UI): smaller stats filter selection debounce time

--- packages/client/src/pages/Stats/constants.ts
+++ packages/client/src/pages/Stats/constants.ts
@@ -13,7 +13,7 @@ export const VISIBLE_EVENT_TYPES = Object.values(EventType).filter(
 export const OTHERS_KEY = "others";
 export const TABLE_PADDING = 30;
 export const USER_THRESHOLD_MAX = 20;
-export const STATS_FILTER_DEBOUNCE_MS = 1200;
+export const STATS_FILTER_DEBOUNCE_MS = 800;
 
 export type TimeKey = string;
 export type UserId = string;
