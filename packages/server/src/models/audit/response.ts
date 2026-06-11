dce780893b67c7f312b2b274571c28707d33df52
Ptrhnk <petr.nahak@gmail.com>
2026-06-11 15:58:48 +0200

    3081 show 10 audits instead of 5 (#3099)

--- packages/server/src/models/audit/response.ts
+++ packages/server/src/models/audit/response.ts
@@ -13,7 +13,7 @@ export class ResponseAudit implements IResponseAudit {
   }
 
   async prepare(db: Connection): Promise<void> {
-    this.last = await Audit.getLastNForEntity(db, this.modelId, 5);
+    this.last = await Audit.getLastNForEntity(db, this.modelId, 10);
     if (this.last.length) {
       const firstEntity = await Audit.getFirstForEntity(db, this.modelId);
       if (firstEntity) {
