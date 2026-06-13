a60c58286b154f60fc1b6d9d5d2e1cfd6ff943f2
Ptrhnk <petr.nahak@gmail.com>
2026-06-13 17:56:16 +0200

    3095 relabel mouseover of annotator modes

--- packages/client/src/components/advanced/Annotator/Annotator.tsx
+++ packages/client/src/components/advanced/Annotator/Annotator.tsx
@@ -1355,7 +1355,7 @@ export const TextAnnotator = ({
               onClick={() => {
                 setAnnotatorMode(EditMode.HIGHLIGHT);
               }}
-              tooltipLabel="highlight (activate syntax highlighting mode)"
+              tooltipLabel="anchor entities"
               tooltipPosition="top"
             />
             <Button
@@ -1373,7 +1373,7 @@ export const TextAnnotator = ({
               onClick={() => {
                 setAnnotatorMode(EditMode.SEMI);
               }}
-              tooltipLabel="text edit (activate semi mode)"
+              tooltipLabel="edit plain text"
               tooltipPosition="top"
             />
             <Button
@@ -1391,7 +1391,7 @@ export const TextAnnotator = ({
               onClick={() => {
                 setAnnotatorMode(EditMode.RAW);
               }}
-              tooltipLabel="XML (activate edit mode)"
+              tooltipLabel="display and edit XML"
               tooltipPosition="top"
             />
           </ButtonGroup>
