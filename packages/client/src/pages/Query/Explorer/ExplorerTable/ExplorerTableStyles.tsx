6a89b1fb1da1764b5f3339ff0dd46af66330df7b
Ptrhnk <petr.nahak@gmail.com>
2026-06-18 12:48:49 +0100

    align left the empty query message

--- packages/client/src/pages/Query/Explorer/ExplorerTable/ExplorerTableStyles.tsx
+++ packages/client/src/pages/Query/Explorer/ExplorerTable/ExplorerTableStyles.tsx
@@ -47,11 +47,8 @@ export const StyledHeader = styled.div`
 export const StyledBody = styled.div``;
 
 export const StyledEmptyMessage = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
   padding: ${({ theme }) => theme.space[6]};
+  text-align: left;
   font-size: ${({ theme }) => theme.fontSize.sm};
   color: ${({ theme }) => theme.color.plain};
 `;
