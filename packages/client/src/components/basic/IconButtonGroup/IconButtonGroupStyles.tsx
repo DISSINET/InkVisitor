04612807c1b6f9954903d071b53e31d831ff1931
Ptrhnk <petr.nahak@gmail.com>
2026-06-26 20:22:43 +0100

    update border-radius in IconButtonGroupStyles

--- packages/client/src/components/basic/IconButtonGroup/IconButtonGroupStyles.tsx
+++ packages/client/src/components/basic/IconButtonGroup/IconButtonGroupStyles.tsx
@@ -8,7 +8,7 @@ export const StyledWrapper = styled.div<StyledWrapper>`
   height: ${({ theme, $border }) => ($border ? theme.space[8] : theme.space[8])};
   border: ${({ theme, $border }) =>
     $border ? `${theme.borderWidth[1]} solid ${theme.color["grey"]}` : ""};
-  border-radius: ${({ theme }) => theme.borderRadius["xs"]};
+  border-radius: ${({ theme }) => theme.borderRadius["sm"]};
   overflow: ${({ $border }) => ($border ? "hidden" : "")};
   background-color: ${({ theme }) => theme.color["white"]};
 `;
