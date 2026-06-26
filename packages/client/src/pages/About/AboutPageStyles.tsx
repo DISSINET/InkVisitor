658351812c28782ca134f15be5db0a03e7529800
Ptrhnk <petr.nahak@gmail.com>
2026-06-26 17:20:19 +0100

    refactor AboutPage styles and structure: add StyledContentSection, improve layout and responsiveness

--- packages/client/src/pages/About/AboutPageStyles.tsx
+++ packages/client/src/pages/About/AboutPageStyles.tsx
@@ -1,24 +1,50 @@
-import { space5, space7 } from "Theme/theme-space-shortcut";
 import styled from "styled-components";
 
 export const StyledContentWrapper = styled.div`
   display: flex;
-  align-items: center;
   flex-direction: column;
+  align-items: stretch;
   overflow: auto;
   width: 100%;
+  background-color: ${({ theme }) => theme.color["gray"][150]};
 `;
+
 export const StyledContent = styled.div`
   display: flex;
   flex-direction: column;
-  height: 100%;
-  padding: ${space5} ${space7};
+  width: 75%;
+  max-width: 96rem;
+  margin: ${({ theme }) => `${theme.space[8]} auto`};
+  background-color: ${({ theme }) => theme.color["white"]};
+  border-radius: ${({ theme }) => theme.borderRadius["lg"]};
+  box-shadow: ${({ theme }) => theme.boxShadow["subtle"]};
   font-size: ${({ theme }) => theme.fontSize["sm"]};
+
+  @media (max-width: 700px) {
+    padding: ${({ theme }) => `${theme.space[6]} ${theme.space[5]}`};
+    margin: ${({ theme }) => `${theme.space[4]} ${theme.space[3]}`};
+  }
 `;
 
 export const StyledLogo = styled.div`
+  display: flex;
+  justify-content: center;
   background-color: ${({ theme }) => theme.color.muni};
-  padding: ${({ theme }) => theme.space[8]};
+  padding: ${({ theme }) => `${theme.space[9]} ${theme.space[8]}`};
+  border-radius: ${({ theme }) => theme.borderRadius["md"]};
+  border-bottom-left-radius: 0;
+  border-bottom-right-radius: 0;
+  overflow: hidden;
+  margin-bottom: ${({ theme }) => theme.space[4]};
+
+  img {
+    max-width: 48rem;
+  }
+`;
+export const StyledContentSection = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: ${({ theme }) => `${theme.space[9]} ${theme.space[8]}`};
 `;
 export const StyledTitle = styled.div`
   font-size: ${({ theme }) => theme.fontSize["3xl"]};
@@ -32,37 +58,79 @@ export const StyledHeader = styled.div`
   font-family: Muni;
   font-size: ${({ theme }) => theme.fontSize["xl"]};
   color: ${({ theme }) => theme.color["primary"]};
-  padding-top: ${({ theme }) => theme.space[4]};
+  padding: ${({ theme }) => `0 0 ${theme.space[2]} ${theme.space[3]}`};
+  margin-top: ${({ theme }) => theme.space[8]};
+  border-left: ${({ theme }) => `3px solid ${theme.color["success"]}`};
+  border-bottom: ${({ theme }) => `1px solid ${theme.color["gray"][300]}`};
+  line-height: 1.2;
 `;
 
 export const StyledTextList = styled.ul`
-  padding-left: ${({ theme }) => theme.space[10]};
-  color: ${({ theme }) => theme.color["black"]};
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  color: ${({ theme }) => theme.color["text"]};
   padding-bottom: ${({ theme }) => theme.space[2]};
 `;
 
 export const StyledTextListItem = styled.li`
-  padding-top: ${({ theme }) => theme.space[2]};
+  position: relative;
+  padding-left: ${({ theme }) => theme.space[5]};
+  padding-top: ${({ theme }) => theme.space[3]};
+  line-height: 1.6;
+
+  &::before {
+    content: "";
+    position: absolute;
+    left: 0;
+    top: ${({ theme }) => theme.space[4]};
+    width: ${({ theme }) => theme.space[2]};
+    height: ${({ theme }) => theme.borderWidth[2]};
+    background-color: ${({ theme }) => theme.color["success"]};
+    border-radius: ${({ theme }) => theme.borderRadius["full"]};
+  }
 `;
 
 export const StyledPerson = styled.div``;
 export const StyledLink = styled.span`
   display: inline;
   margin-left: ${({ theme }) => theme.space[1]};
-  margin-right: ${({ theme }) => theme.space[0]};
   a {
     color: ${({ theme }) => theme.color["success"]};
     font-weight: ${({ theme }) => theme.fontWeight["bold"]};
     text-decoration: none;
     svg {
       margin-right: ${({ theme }) => theme.space[1]};
+      vertical-align: -0.1em;
+    }
+    &:hover {
+      text-decoration: underline;
     }
   }
 `;
 
-export const StyledAcknowledgement = styled.div``;
+export const StyledAcknowledgement = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+  gap: ${({ theme }) => theme.space[6]};
+  margin-top: ${({ theme }) => theme.space[10]};
+  padding-top: ${({ theme }) => theme.space[8]};
+  border-top: ${({ theme }) => `1px solid ${theme.color["gray"][300]}`};
+`;
 
 export const StyledAcknowledgementLogo = styled.img`
-  margin: ${({ theme }) => theme.space[4]};
-  padding: ${({ theme }) => theme.space[2]};
+  height: 5rem;
+  width: auto;
+  filter: grayscale(1);
+  opacity: 0.65;
+  transition:
+    filter 0.2s ease,
+    opacity 0.2s ease;
+
+  &:hover {
+    filter: grayscale(0);
+    opacity: 1;
+  }
 `;
