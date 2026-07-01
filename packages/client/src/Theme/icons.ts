77568ec4c2724c8370cea8360522339f7af4cdac
Ptrhnk <petr.nahak@gmail.com>
2026-07-01 20:14:44 +0100

    update react-icons and add VscEditCompact to icon file

--- packages/client/src/Theme/icons.ts
+++ packages/client/src/Theme/icons.ts
@@ -67,7 +67,7 @@ export { FaClipboard as IcoClipboardFilled } from "react-icons/fa";
 export { FaClone as IcoClone } from "react-icons/fa";
 export { FaDotCircle as IcoDotCircle } from "react-icons/fa";
 export { FaDownload as IcoDownload } from "react-icons/fa";
-export { FaEdit as IcoEdit } from "react-icons/fa";
+// export { FaEdit as IcoEdit } from "react-icons/fa";
 export { FaExchangeAlt as IcoExchange } from "react-icons/fa";
 export { FaExclamationTriangle as IcoExclamation } from "react-icons/fa";
 export { FaExternalLinkAlt as IcoExternalLink } from "react-icons/fa";
@@ -228,6 +228,9 @@ export { TiWarning as IcoWarning } from "react-icons/ti";
 export { TiWarningOutline as IcoWarningTiOutline } from "react-icons/ti";
 
 // vsc
+export { VscEditCompact as IcoEdit } from "react-icons/vsc";
+// export { VscEditCompact as IcoEditMd } from "react-icons/vsc";
+// export { VscEditCompact as IcoEditOutline } from "react-icons/vsc";
 export { VscClose as IcoCloseVsc } from "react-icons/vsc";
 export { VscCloseAll as IcoCloseAll } from "react-icons/vsc";
 export { VscCommentDiscussion as IcoCommentDiscussion } from "react-icons/vsc";
