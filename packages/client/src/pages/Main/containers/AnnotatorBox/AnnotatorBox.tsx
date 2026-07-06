e7eca3bf235893bb7d1ae4669650e18490c8414e
Ptrhnk <petr.nahak@gmail.com>
2026-07-06 19:42:39 +0100

    fix statement blinking after anchor add

--- packages/client/src/pages/Main/containers/AnnotatorBox/AnnotatorBox.tsx
+++ packages/client/src/pages/Main/containers/AnnotatorBox/AnnotatorBox.tsx
@@ -220,11 +220,6 @@ export const AnnotatorBox: React.FC<AnnotatorBox> = ({ height, width }) => {
       queryClient.invalidateQueries({
         queryKey: ["territory", "statement-list", territoryId],
       });
-      if (selectedDocumentId) {
-        queryClient.invalidateQueries({
-          queryKey: ["document", selectedDocumentId],
-        });
-      }
       queryClient.invalidateQueries({ queryKey: ["anchorEntities"] });
       queryClient.invalidateQueries({ queryKey: ["tree"] });
     },
