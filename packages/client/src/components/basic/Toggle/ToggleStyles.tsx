b4c436c769fc879eefd6f99f77db9f5f981b518b
Ptrhnk <petr.nahak@gmail.com>
2026-07-07 17:04:34 +0100

    style fixes, clean up (#3182)

--- packages/client/src/components/basic/Toggle/ToggleStyles.tsx
+++ packages/client/src/components/basic/Toggle/ToggleStyles.tsx
@@ -7,7 +7,11 @@ export const StyledToggle = styled.div<StyledToggle>`
   display: inline-flex;
   align-items: center;
   gap: 0.5rem;
-  color: ${({ theme, $active }) =>
-    $active ? theme.color["info"] : theme.color["danger"]};
+  color: ${({ theme, $active }) => ($active ? theme.color["info"] : theme.color["danger"])};
   cursor: pointer;
+`;
+
+export const StyledToggleLabel = styled.span`
+  font-size: ${({ theme }) => theme.fontSize["xs"]};
+  font-weight: 400;
 `;
