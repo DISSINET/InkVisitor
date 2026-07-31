8414cc0c30a2cdfc24aa1670422df5f1ae1683e4
Ptrhnk <petr.nahak@gmail.com>
2026-07-31 12:35:01 +0100

    feat(MultiInput): allow drag-and-drop reordering of multi-note rows (#1334)

--- packages/client/src/components/basic/MultiInput/MultiInputStyles.tsx
+++ packages/client/src/components/basic/MultiInput/MultiInputStyles.tsx
@@ -1,22 +1,37 @@
 import styled from "styled-components";
 
-export const StyledRow = styled.div`
+interface StyledRow {
+  $isDragging: boolean;
+}
+export const StyledRow = styled.div<StyledRow>`
   position: relative;
   display: flex;
   flex-direction: column;
   margin-bottom: 1rem;
+  opacity: ${({ $isDragging }) => ($isDragging ? 0.2 : 1)};
 `;
 
-// trash icon floats in the note's top-right corner, revealed on hover
-export const StyledDeleteButton = styled.div`
+// action icons stack in the note's top-right corner, revealed on hover
+export const StyledRowActions = styled.div`
   position: absolute;
   top: ${({ theme }) => theme.space[1]};
   right: ${({ theme }) => theme.space[1]};
   z-index: 1;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
   opacity: 0;
   transition: opacity 0.15s ease;
 
   ${StyledRow}:hover & {
     opacity: 1;
   }
+`;
+
+export const StyledDragHandle = styled.div`
+  display: flex;
+  align-items: center;
+  cursor: move;
+  font-size: ${({ theme }) => theme.fontSize["xxs"]};
+  color: ${({ theme }) => theme.color["black"]};
 `;
